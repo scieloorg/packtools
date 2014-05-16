497e24d8747c15194c56b2e797980474239a7659
Gustavo Fonseca <gustavo@gfonseca.net>
2014-05-16 18:15:01 -0300

    Regexp que pega o nome do elemento no local errado foi afinada; Adicionado suporte a anotação no elemento root.

--- packtools/stylechecker.py
+++ packtools/stylechecker.py
@@ -1,10 +1,22 @@
+"""
+Mensagens de error_log (linha, coluna - mensagem):
+
+4,0 - Element 'article', attribute 'dtd-version': [facet 'enumeration'] The value '3.0' is not an element of the set {'1.0'}.
+4,0 - Element 'article', attribute 'dtd-version': '3.0' is not a valid value of the local atomic type.
+824,0 - Element 'author-notes': This element is not expected. Expected is one of ( label, title, ack, app-group, bio, fn-group, glossary, ref-list, notes, sec ).
+6,0 - Element 'journal-title-group': This element is not expected. Expected is ( journal-id ).
+15,0 - Element 'contrib-group': This element is not expected. Expected is one of ( article-id, article-categories, title-group ).
+"""
 import re
 import copy
 from lxml import etree
+import logging
 
 
 SCHEMAS = {'http://static.scielo.org/sps/schema/SciELO-journalpublishing1.xsd': '/Users/gustavofonseca/prj/github/packtools/packtools/sps_xsd/sps.xsd'}
-EXPOSE_ELEMENTNAME_PATTERN = re.compile(r"'.*'")
+EXPOSE_ELEMENTNAME_PATTERN = re.compile(r"(?<=Element )'.*?'")
+
+logger = logging.getLogger(__name__)
 
 
 def XMLSchema(href):
@@ -32,7 +44,14 @@ class XML(object):
     def find(self, tagname, lineno):
         for elem in self.xml.findall('//' + tagname):
             if elem.sourceline == lineno:
+                logger.debug('method *find*: hit a regular element: %s.' % tagname)
                 return elem
+        else:
+            root = self.xml.getroot()
+            if root.tag == tagname:
+                logger.debug('method *find*: hit a root element.')
+                return root
+
 
     def validate(self):
         result = setdefault(self, '_validation_result', lambda: self.xmlschema.validate(self.xml))
@@ -51,11 +70,15 @@ class XML(object):
 
             err_element = self.find(element_name, error.line)
             if err_element is None:
-                raise ValueError('Could not locate the erratic element %s at line %s.' % (element_name, error.line))
+                raise ValueError('Could not locate the erratic element %s at line %s to annotate: %s.' % (element_name, error.line, error.message))
 
             notice_element = etree.Element('SPS-ERROR')
             notice_element.text = error.message
-            err_element.addprevious(notice_element)
+            try:
+                err_element.addprevious(notice_element)
+            except TypeError:
+                # In case of a root element, a comment if added.
+                err_element.addprevious(etree.Comment('SPS-ERROR: %s' % error.message))
 
     def __str__(self):
         return etree.tostring(self.xml, pretty_print=True,
