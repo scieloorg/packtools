1597ae43dd62f4390a2743378b4c2dc9c87bbc83
Juan Funez <juan.funez@gmail.com>
2015-07-31 17:58:20 -0300

    Limpeza de Tests e descartando xslt legado

--- packtools/catalogs/__init__.py
+++ packtools/catalogs/__init__.py
@@ -17,6 +17,5 @@ SCHEMAS = {
 XML_CATALOG = os.path.join(_CWD, 'scielo-publishing-schema.xml')
 
 XSLTS = {
-    'default-html.xslt': os.path.join(_CWD, 'default-html.xslt'),
     'root-html-1.2.xslt': os.path.join(_CWD, 'htmlgenerator/root-html-1.2.xslt'),
 }
---
+++ tests/tests_generated_html_tags.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+from __future__ import unicode_literals
+import unittest
+import io
+from lxml import etree
+
+from packtools import domain
+
+
+class GeneratedTagsTests(unittest.TestCase):
+
+    """ Tags gerais do html: <head>/<title>, <head>/<meta>, <html lang=?>, etc. """
+    def test_mandatory_meta_tag_charset_for_encoding(self):
+        """
+        verifica que o tag meta seja o correto no html gerado. deve ser: <meta charset="utf-8"/>
+        """
+        sample = u"""<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.0 20120330//EN" "JATS-journalpublishing1.dtd">
+                    <article xml:lang="pt">
+                       <sub-article xml:lang="en" article-type="translation" id="S01">
+                       </sub-article>
+                       <sub-article xml:lang="es" article-type="translation" id="S02">
+                       </sub-article>
+                    </article>
+                 """
+        fp = io.BytesIO(sample.encode('utf-8'))
+        et = etree.parse(fp)
+        for lang, html_output in domain.HTMLGenerator(et, valid_only=False):
+            meta_tag = html_output.xpath('/html/head/meta[@charset="utf-8"]')
+            self.assertEqual(1, len(meta_tag))
+
+    def test_html_tag_lang_attrib(self):
+        """
+        verifica que aparece o atributo "lang" no tag <html>, com o language certo.
+        """
+        sample = u"""<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.0 20120330//EN" "JATS-journalpublishing1.dtd">
+                    <article xml:lang="pt">
+                       <sub-article xml:lang="en" article-type="translation" id="S01">
+                       </sub-article>
+                       <sub-article xml:lang="es" article-type="translation" id="S02">
+                       </sub-article>
+                    </article>
+                 """
+        fp = io.BytesIO(sample.encode('utf-8'))
+        et = etree.parse(fp)
+        for lang, html_output in domain.HTMLGenerator(et, valid_only=False):
+            html_lang_attribute = html_output.xpath('/html')[0].attrib['lang']
+            self.assertEqual(lang, html_lang_attribute)
+
+    def test_bibliographic_legend(self):
+        """
+        verifica que aparece o conteudo processado como "bibliographic_legend" no html
+        """
+        expected_legend_text = '[#BIBLIOGRAPHIC LEGEND#]'
+        sample = u"""<?xml version="1.0" encoding="UTF-8"?>
+                    <!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.0 20120330//EN" "JATS-journalpublishing1.dtd">
+                    <article xml:lang="pt">
+                       <sub-article xml:lang="en" article-type="translation" id="S01">
+                       </sub-article>
+                       <sub-article xml:lang="es" article-type="translation" id="S02">
+                       </sub-article>
+                    </article>
+                 """
+        fp = io.BytesIO(sample.encode('utf-8'))
+        et = etree.parse(fp)
+        for lang, html_output in domain.HTMLGenerator(et, valid_only=False):
+            legend_span_tag = html_output.xpath('//span[@id="bibliographic_legend"]')
+            self.assertEqual(1, len(legend_span_tag))
+            self.assertEqual(expected_legend_text, legend_span_tag[0].text.strip())
