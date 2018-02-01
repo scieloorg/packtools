9d3882dc065bd8f82e29dc199b7fd4db15e894f4
Gustavo Fonseca <gustavo@gfonseca.net>
2018-02-01 10:54:05 -0200

    Inclui a nova XSLT no manifesto da pacote `catalog`

--- packtools/catalogs/__init__.py
+++ packtools/catalogs/__init__.py
@@ -36,6 +36,7 @@ XML_CATALOG = os.path.join(_CWD, 'scielo-publishing-schema.xml')
 
 XSLTS = {
     'root-html-1.2.xslt': os.path.join(_CWD, 'htmlgenerator/root-html-1.2.xslt'),
+    'root-html-2.0.xslt': os.path.join(_CWD, 'htmlgenerator/root-html-2.0.xslt'),
 }
 
 ISO3166_CODES = os.path.join(_CWD, 'iso3166-codes.json')
