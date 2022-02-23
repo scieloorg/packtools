909ec2ee9d3927b25e1af64447341ea1fbd66475
Rafael JPD <rafael.pezzuto@gmail.com>
2022-02-23 13:41:24 -0300

    Adiciona método para gerar pacote a partir de arquivos no disco

--- packtools/sps/sps_maker.py
+++ packtools/sps/sps_maker.py
@@ -10,3 +10,19 @@ FILE_PATHS_REQUIRED_KEYS = ['xml', 'assets', 'renditions']
 def get_names_and_packages(path):
     return packages.explore_source(path)
 
+
+def make_package_from_paths(paths, zip_folder=None):
+    _check_keys_and_files(paths)
+
+    package_metadata = {}
+    xml_sps = _get_xml_sps_from_path(paths['xml'])
+
+    package_metadata['xml'] = _get_xml_uri_and_name(xml_sps, paths['xml'])
+    package_metadata['renditions'] = paths['renditions']
+    package_metadata['assets'] = paths['assets']
+
+    zip_filename = _get_zip_filename(xml_sps)
+    package_metadata['temp-zipfile'] = _zip_files_from_paths(zip_filename, xml_sps, paths, zip_folder)
+
+    return package_metadata
+
