413516f4f6329317b7db66711b1b85c47253bdf8
Rafael JPD <rafael.pezzuto@gmail.com>
2022-02-23 12:58:47 -0300

    Altera métodos para retornar dados apenas se houver status_code 200

--- packtools/sps/libs/reqs.py
+++ packtools/sps/libs/reqs.py
@@ -3,9 +3,11 @@ import requests
 
 def requests_get_content(uri, timeout=10):
     response = requests.get(uri, timeout=timeout)
-    return response.content.decode("utf-8")
+    if response.status_code == 200:
+        return response.content.decode("utf-8")
 
 
 def requests_get(uri, timeout=10):
     response = requests.get(uri, timeout=timeout)
-    return response.content
+    if response.status_code == 200:    
+        return response.content
