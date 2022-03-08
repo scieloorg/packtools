18241f6468331b8567a26c8e2856b262578b0b22
Rafael JPD <rafael.pezzuto@gmail.com>
2022-03-07 21:36:55 -0300

    Adiciona função auxiliar para lidar com classes de erro novas

--- packtools/sps/libs/reqs.py
+++ packtools/sps/libs/reqs.py
@@ -25,3 +25,18 @@ def requests_get(uri, timeout=10):
         raise exceptions.SPSConnectionError()
     else:
         return response.content
+
+
+def _handle_http_error(status_code):
+    if status_code == 403:
+        raise exceptions.SPSHTTPForbiddenError()
+    elif status_code == 404:
+        raise exceptions.SPSHTTPResourceNotFoundError()
+    elif status_code == 500:
+        raise exceptions.SPSHTTPInternalServerError()
+    elif status_code == 502:
+        raise exceptions.SPSHTTPBadGatewayError()
+    elif status_code == 503:
+        raise exceptions.SPSHTTPServiceUnavailableError()
+    else:
+        raise exceptions.SPSHTTPError(f'{status_code}')
