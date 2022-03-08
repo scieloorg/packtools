360763b40d7b2ce65af1e693d4445e04dfa1dcb2
Rafael JPD <rafael.pezzuto@gmail.com>
2022-03-07 21:36:38 -0300

    Adequa métodos para usar função auxiliar que lida com classes de erros adicionais

--- packtools/sps/libs/reqs.py
+++ packtools/sps/libs/reqs.py
@@ -8,7 +8,9 @@ def requests_get_content(uri, timeout=10):
         response = requests.get(uri, timeout=timeout)
         response.raise_for_status()
     except requests.exceptions.HTTPError:
-        raise exceptions.SPSHTTPError(f'{response.status_code}')
+        _handle_http_error(response.status_code)
+    except requests.exceptions.ConnectionError:
+        raise exceptions.SPSConnectionError()
     else:
         return response.content.decode("utf-8")
 
@@ -18,6 +20,8 @@ def requests_get(uri, timeout=10):
         response = requests.get(uri, timeout=timeout)
         response.raise_for_status()
     except requests.exceptions.HTTPError:
-        raise exceptions.SPSHTTPError(f'{response.status_code}')
+        _handle_http_error(response.status_code)
+    except requests.exceptions.ConnectionError:
+        raise exceptions.SPSConnectionError()
     else:
         return response.content
