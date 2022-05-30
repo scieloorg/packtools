04a282e8d0fbdea797f44a889e2da2e1e281f053
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-05-30 09:17:50 -0300

    Ajusta a obtenção de número e suplemento

--- packtools/sps/models/front_articlemeta_issue.py
+++ packtools/sps/models/front_articlemeta_issue.py
@@ -15,6 +15,52 @@
 from packtools.sps.models.dates import ArticleDates
 
 
+def _extract_number_and_supplment_from_issue_element(issue):
+    """
+    Extrai do conteúdo de <issue>xxxx</issue>, os valores number e suppl.
+    Valores possíveis
+    5 (suppl), 5 Suppl, 5 Suppl 1, 5 spe, 5 suppl, 5 suppl 1, 5 suppl. 1,
+    25 Suppl 1, 2-5 suppl 1, 2spe, Spe, Supl. 1, Suppl, Suppl 12,
+    s2, spe, spe 1, spe pr, spe2, spe.2, spepr, supp 1, supp5 1, suppl,
+    suppl 1, suppl 5 pr, suppl 12, suppl 1-2, suppl. 1
+    """
+    if not issue:
+        return None, None
+    issue = issue.strip().replace(".", "")
+    splitted = [s for s in issue.split() if s]
+
+    splitted = ["spe"
+                if "spe" in s.lower() and s.isalpha() else s
+                for s in splitted
+                ]
+    if len(splitted) == 1:
+        issue = splitted[0]
+        if issue.isdigit():
+            return issue, None
+        if "sup" in issue.lower():
+            # match como sup*
+            return None, "0"
+        if issue.startswith("s"):
+            if issue[1:].isdigit():
+                return None, issue[1:]
+        # match com spe, 2-5, 3B
+        return issue, None
+
+    if len(splitted) == 2:
+        if "sup" in splitted[0].lower():
+            return None, splitted[1]
+        if "sup" in splitted[1].lower():
+            return splitted[0], "0"
+        # match spe 4 -> spe4
+        return "".join(splitted), None
+
+    if len(splitted) == 3:
+        if "sup" in splitted[1].lower():
+            return splitted[0], splitted[2]
+    # match ????
+    return "".join(splitted), None
+
+
 class ArticleMetaIssue:
 
     def __init__(self, xmltree):
@@ -57,13 +103,20 @@ class ArticleMetaIssue:
 
     @property
     def number(self):
-        # FIXME fazer o parser de issue para extrair number e/ou suppl
-        return self.xmltree.findtext(".//front/article-meta/issue")
+        _issue = self.issue
+        if _issue:
+            n, s = _extract_number_and_supplment_from_issue_element(_issue)
+            return n
 
     @property
     def suppl(self):
-        # FIXME verificar a grafia do elemento suppl
-        return self.xmltree.findtext(".//front/article-meta/suppl")
+        _suppl = self.xmltree.findtext(".//front/article-meta/supplement")
+        if _suppl:
+            return _suppl
+        _issue = self.issue
+        if _issue:
+            n, s = _extract_number_and_supplment_from_issue_element(_issue)
+            return s
 
     @property
     def elocation_id(self):
--- tests/sps/test_front_articlemeta_issue.py
+++ tests/sps/test_front_articlemeta_issue.py
@@ -19,11 +19,14 @@
 """
 
 
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from lxml import etree
 
-from packtools.sps.models.front_articlemeta_issue import ArticleMetaIssue
+from packtools.sps.models.front_articlemeta_issue import (
+    ArticleMetaIssue,
+    _extract_number_and_supplment_from_issue_element,
+)
 
 
 def _get_xml(articlemeta_content=None):
@@ -40,7 +43,7 @@ def _get_xml(articlemeta_content=None):
             </pub-date>
             <volume>4</volume>
             <issue>1</issue>
-            <suppl>B</suppl>
+            <supplement>B</supplement>
             <fpage seq="A">108</fpage>
             <lpage>123</lpage>
             <elocation-id>123</elocation-id>
@@ -143,3 +146,167 @@ class ArticleMetaIssueWithAbsentDataTest(TestCase):
     def test_data(self):
         expected = {}
         self.assertEqual(expected, self.issue.data)
+
+
+class TestExtractNumberAndSupplmentFromIssueElement(TestCase):
+    """
+    Extrai do conteúdo de <issue>xxxx</issue>, os valores number e suppl.
+    Valores possíveis
+    5 (suppl), 5 Suppl, 5 Suppl 1, 5 spe, 5 suppl, 5 suppl 1, 5 suppl. 1,
+    25 Suppl 1, 2-5 suppl 1, 2spe, Spe, Supl. 1, Suppl, Suppl 12,
+    s2, spe, spe 1, spe pr, spe2, spe.2, spepr, supp 1, supp5 1, suppl,
+    suppl 1, suppl 5 pr, suppl 12, suppl 1-2, suppl. 1
+
+    """
+    def test_number_and_suppl_for_5_parenteses_suppl(self):
+        expected = "5", "0"
+        result = _extract_number_and_supplment_from_issue_element("5 (suppl)")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_Suppl(self):
+        expected = "5", "0"
+        result = _extract_number_and_supplment_from_issue_element("5 Suppl")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_Suppl_1(self):
+        expected = "5", "1"
+        result = _extract_number_and_supplment_from_issue_element("5 Suppl 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_spe(self):
+        expected = "5spe", None
+        result = _extract_number_and_supplment_from_issue_element("5 spe")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_suppl(self):
+        expected = "5", "0"
+        result = _extract_number_and_supplment_from_issue_element("5 suppl")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_suppl_1(self):
+        expected = "5", "1"
+        result = _extract_number_and_supplment_from_issue_element("5 suppl 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_5_suppl_dot_1(self):
+        expected = "5", "1"
+        result = _extract_number_and_supplment_from_issue_element("5 suppl. 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_25_Suppl_1(self):
+        expected = "25", "1"
+        result = _extract_number_and_supplment_from_issue_element("25 Suppl 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_2_hyphen_5_suppl_1(self):
+        expected = "2-5", "1"
+        result = _extract_number_and_supplment_from_issue_element("2-5 suppl 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_2spe(self):
+        expected = "2spe", None
+        result = _extract_number_and_supplment_from_issue_element("2spe")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_Spe(self):
+        expected = "spe", None
+        result = _extract_number_and_supplment_from_issue_element("Spe")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_Supldot_1(self):
+        expected = None, "1"
+        result = _extract_number_and_supplment_from_issue_element("Supl. 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_Suppl(self):
+        expected = None, "0"
+        result = _extract_number_and_supplment_from_issue_element("Suppl")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_Suppl_12(self):
+        expected = None, "12"
+        result = _extract_number_and_supplment_from_issue_element("Suppl 12")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_s2(self):
+        expected = None, "2"
+        result = _extract_number_and_supplment_from_issue_element("s2")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_spe(self):
+        expected = "spe", None
+        result = _extract_number_and_supplment_from_issue_element("spe")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_special(self):
+        expected = "spe", None
+        result = _extract_number_and_supplment_from_issue_element("Especial")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_spe_1(self):
+        expected = "spe1", None
+        result = _extract_number_and_supplment_from_issue_element("spe 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_spe_pr(self):
+        expected = "spepr", None
+        result = _extract_number_and_supplment_from_issue_element("spe pr")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_spe2(self):
+        expected = "spe2", None
+        result = _extract_number_and_supplment_from_issue_element("spe2")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_spedot2(self):
+        expected = "spe2", None
+        result = _extract_number_and_supplment_from_issue_element("spe.2")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_supp_1(self):
+        expected = None, "1"
+        result = _extract_number_and_supplment_from_issue_element("supp 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_suppl(self):
+        expected = None, "0"
+        result = _extract_number_and_supplment_from_issue_element("suppl")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_suppl_1(self):
+        expected = None, "1"
+        result = _extract_number_and_supplment_from_issue_element("suppl 1")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_suppl_12(self):
+        expected = None, "12"
+        result = _extract_number_and_supplment_from_issue_element("suppl 12")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_suppl_1hyphen2(self):
+        expected = None, "1-2"
+        result = _extract_number_and_supplment_from_issue_element("suppl 1-2")
+        self.assertEqual(expected, result)
+
+    def test_number_and_suppl_for_suppldot_1(self):
+        expected = None, "1"
+        result = _extract_number_and_supplment_from_issue_element("suppl. 1")
+        self.assertEqual(expected, result)
+
+    @skip("Encontrado no sistema, porém fora do padrão aceitável")
+    def test_number_and_suppl_for_spepr(self):
+        expected = "spepr", None
+        result = _extract_number_and_supplment_from_issue_element("spepr")
+        self.assertEqual(expected, result)
+
+    @skip("Encontrado no sistema, porém fora do padrão aceitável")
+    def test_number_and_suppl_for_supp5_1(self):
+        expected = "supp5", "1"
+        result = _extract_number_and_supplment_from_issue_element("supp5 1")
+        self.assertEqual(expected, result)
+
+    @skip("Encontrado no sistema, porém fora do padrão aceitável")
+    def test_number_and_suppl_for_suppl_5_pr(self):
+        expected = None, "5pr"
+        result = _extract_number_and_supplment_from_issue_element("suppl 5 pr")
+        self.assertEqual(expected, result)
