e4ddc59ad54904224e9ed81817307649456c79fd
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-11 20:26:41 -0300

    Cria classe ArticleAssets

---
+++ packtools/sps/models/article_assets.py
@@ -0,0 +1,8 @@
+class ArticleAssets:
+    """
+    './/graphic[@xlink:href]',
+    './/media[@xlink:href]',
+    './/inline-graphic[@xlink:href]',
+    './/supplementary-material[@xlink:href]',
+    './/inline-supplementary-material[@xlink:href]',
+    """
