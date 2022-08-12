fbc9435ff20183b623262dbc0f2bcf3d34844ffe
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-11 21:17:55 -0300

    Adiciona teste para verificar comportamento quando XML contém media e figs com ids

--- tests/sps/test_article_assets.py
+++ tests/sps/test_article_assets.py
@@ -169,3 +169,51 @@ class ArticleAssetsTest(TestCase):
       self.assertDictEqual(expected, obtained)
   
 
+    def test_article_assets_with_media_and_graphic(self):
+      data = """
+      <article xmlns:xlink="http://www.w3.org/1999/xlink">
+        <body>
+          <p><media mimetype="video" mime-subtype="mp4" xlink:href="1234-5678-rctb-45-05-0110-m01.mp4"/></p>
+          <div>
+            <fig id="f01">
+              <label>Figura 1</label>
+              <caption>
+                  <title>Caption Figura 1</title>
+              </caption>
+              <disp-formula>
+              <alternatives>
+                  <graphic xlink:href="original.tif" />
+                  <graphic xlink:href="ampliada.png" specific-use="scielo-web" />
+                  <graphic xlink:href="miniatura.jpg" specific-use="scielo-web" content-type="scielo-20x20" />
+              </alternatives>
+              </disp-formula>
+              <attrib>Fonte: Dados originais da pesquisa</attrib>
+            </fig>
+          </div>
+        </body>
+      </article>
+      """
+      xmltree = xml_utils.get_xml_tree(data)
+
+      expected = {
+        None: [
+          '1234-5678-rctb-45-05-0110-m01.mp4',
+        ],
+        'f01': [
+          'original.tif',
+          'ampliada.png',
+          'miniatura.jpg',
+        ]
+      }
+      obtained = {}
+
+      for asset in ArticleAssets(xmltree).article_assets:
+        a_id = asset.id
+        a_name = asset.name
+
+        if a_id not in obtained:
+          obtained[a_id] = []
+
+        obtained[a_id].append(a_name)
+
+      self.assertDictEqual(expected, obtained)
