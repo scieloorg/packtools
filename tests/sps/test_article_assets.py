c066ce1e3743b732c1ce353dbed8e2cc5c5d2aeb
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-11 21:16:57 -0300

    Adiciona teste para lidar com vários asset em XML com um mesmo ID

--- tests/sps/test_article_assets.py
+++ tests/sps/test_article_assets.py
@@ -59,3 +59,31 @@ class ArticleAssetsTest(TestCase):
         obtained[a_id].append(a_name)
 
       self.assertDictEqual(expected, obtained)
+
+
+    def test_article_assets_with_multiple_figures(self):
+      data = open('tests/sps/fixtures/document2.xml').read()
+      xmltree = xml_utils.get_xml_tree(data)
+
+      expected = {
+        'f01': [
+          'https://minio.scielo.br/documentstore/1414-431X/ywDM7t6mxHzCRWp7kGF9rXQ/fd89fb6a2a0f973016f2de7ee2b64b51ca573999.jpg',
+          'https://minio.scielo.br/documentstore/1414-431X/ywDM7t6mxHzCRWp7kGF9rXQ/0c10c88b56f3f9b4f4eccfe9ddbca3fd581aac1b.jpg'
+        ],
+        'f02': [
+          'https://minio.scielo.br/documentstore/1414-431X/ywDM7t6mxHzCRWp7kGF9rXQ/afd520e3ff23a23f2c973bbbaa26094e9e50f487.jpg',
+          'https://minio.scielo.br/documentstore/1414-431X/ywDM7t6mxHzCRWp7kGF9rXQ/c2e5f2b77881866ef9820b03e99b3fedbb14cb69.jpg'
+        ]
+      }
+      obtained = {}
+
+      for asset in ArticleAssets(xmltree).article_assets:
+        a_id = asset.id
+        a_name = asset.name
+
+        if a_id not in obtained:
+          obtained[a_id] = []
+
+        obtained[a_id].append(a_name)
+
+      self.assertDictEqual(expected, obtained)
