7d1dc6640f2dd43d47bedf7246725dc9f4833f41
Rafael JPD <rafael.pezzuto@gmail.com>
2022-08-19 15:11:49 -0300

    Adiciona property responsável por rastrear principal idioma e demais

--- packtools/sps/models/article_renditions.py
+++ packtools/sps/models/article_renditions.py
@@ -1,3 +1,19 @@
 class ArticleRenditions:
     def __init__(self, xmltree):
         self.xmltree = xmltree
+
+    @property
+    def article_renditions(self):
+        _renditions = []
+
+        main_rendition = Rendition(
+            self.xmltree.find("."),
+            True,
+        )
+        _renditions.append(main_rendition)
+
+        for sub_article in self.xmltree.xpath(".//sub-article[@article-type='translation']"):
+            _renditions.append(Rendition(sub_article))
+
+        return _renditions
+
