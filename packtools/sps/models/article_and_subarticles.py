fd9c955b439c742cc169d0991b15749e7b0bb079
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-19 19:29:53 -0300

    Adiciona propriedade main_article_type

--- packtools/sps/models/article_and_subarticles.py
+++ packtools/sps/models/article_and_subarticles.py
@@ -1,3 +1,7 @@
 class ArticleAndSubArticles:
     def __init__(self, xmltree):
         self.xmltree = xmltree
+
+    @property
+    def main_article_type(self):
+        return self.xmltree.find(".").get("article-type")
