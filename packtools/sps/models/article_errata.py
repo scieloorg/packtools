c969bdef26e24556612b66cd79c6af34c70393d7
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-19 12:32:59 -0300

    Cria módulo para lidar com conteúdo de errata em artigo

---
+++ packtools/sps/models/article_errata.py
@@ -0,0 +1,16 @@
+"""
+<fn-group>
+    <fn fn-type="other">
+        <label>Additions and Corrections</label>
+        <p>On page 10, where it was read:</p>
+        <p>“Joao da Silva”</p>
+        <p>Now reads:</p>
+        <p>“João da Silva Santos”</p>
+    </fn>
+</fn-group>
+"""
+
+
+class ArticleErrata:
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
