469ee438f256b4379c1925774769572c25928766
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-22 18:00:05 -0300

    Obtém códigos DOI de XML de documento corrigido

--- packtools/sps/validation/erratum.py
+++ packtools/sps/validation/erratum.py
@@ -9,6 +9,12 @@ def has_compatible_errata_and_document(xml_errata, xml_article, articles_types=[
     if not has_errata_notes(xml_article):
         return False
 
+    article_doi_list = set()
+    for lang_doi in DoiWithLang(xml_article).data:
+        doi = lang_doi['value']
+        doi_std = standardizer.document_doi(doi)
+        if doi_std:
+            article_doi_list.add(doi_std)    
 def has_errata_notes(xml_article):
     errata_notes = ArticleWithErrataNotes(xml_article).footnotes()
     if len(errata_notes) == 0:
