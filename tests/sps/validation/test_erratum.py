a3fd93a799d8f7b9a65018bfa7489d388f427713
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-25 14:22:28 -0300

    Testa arquivos XML de errata e artigo incompatíveis

--- tests/sps/validation/test_erratum.py
+++ tests/sps/validation/test_erratum.py
@@ -68,3 +68,67 @@ class ErratumTest(TestCase):
         """)
 
         self.assertTrue(has_compatible_errata_and_document(xml_errata, xml_article))
+
+    def test_erratum_has_not_compatibile_errata_and_document(self):
+        xml_errata = get_xml_tree("""
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="correction" xml:lang="en">
+            <front>
+                <article-meta>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v3">Z6mnK3PjKhDZtHJQJYPzxpw</article-id>
+                    <article-id pub-id-type="doi">10.21577/0103-5053.20170069</article-id>
+                    <related-article ext-link-type="doi" id="ra2" related-article-type="corrected-article" xlink:href="10.1590/S0103-50532008000700007"/>
+                </article-meta>
+            </front>
+            <body>
+                <sec>
+                    <title>Enantioselective Synthesis of (<italic>R</italic>)-Isocarvone from (<italic>S</italic>)-Perillaldehyde</title>
+                    <p>
+                        <italic>
+                        <bold>Douglas Gamba, Diego S. Pisoni, Jessé S. da Costa, Cesar L. Petzhold, Antonio C. A. Borges and Marco A. Ceschi*</bold>
+                        </italic>
+                    </p>
+                    <p>
+                        <italic>Instituto de Química, Universidade Federal do Rio Grande do Sul, Av. Bento Gonçalves, 9500, Campus do Vale, 91501-970 Porto Alegre-RS, Brazil</italic>
+                    </p>
+                    <p>
+                        <italic>Vol. 19, No. 7, 1270-1276, 2008.</italic>
+                    </p>
+                    <p>
+                        <italic>Vol. 19, No. 7, S1-S11, 2008.</italic>
+                    </p>
+                    <p>
+                        <ext-link ext-link-type="uri" xlink:href="http://dx.doi.org/10.1590/S0103-50532008000700007">
+                        <italic>http://dx.doi.org/10.1590/S0103-50532008000700007</italic>
+                        </ext-link>
+                    </p>
+                    <p>Pages 1270 and S1</p>
+                    <p>The co-author's name "Jessé S. da Costa" will change to "Jessie S. da Costa"</p>
+                </sec>
+            </body>
+        </article>
+        """)
+
+        xml_article = get_xml_tree("""
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en">
+            <front>
+                <article-meta>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v3">zTn4sYXBrfSTMNVPF5Dm7jr</article-id>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v2">S0103-50532014001202258</article-id>
+                    <article-id pub-id-type="doi">10.5935/0103-5053.20140192</article-id>
+                </article-meta>
+            </front>
+            <back>
+                <fn-group>
+                    <fn fn-type="other">
+                        <label>Additions and Corrections</label>
+                        <p>On page 2258, where it was read:</p>
+                        <p>“Jessé S. Costa”</p>
+                        <p>Now reads:</p>
+                        <p>“Jessie S. Costa”</p>
+                    </fn>
+                </fn-group>
+            </back>
+        </article>
+        """)
+        
+        self.assertFalse(has_compatible_errata_and_document(xml_errata, xml_article))
