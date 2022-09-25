13f3bc2cc6910a535eab44bf16148ca76869d4c2
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-25 14:25:06 -0300

    Testa pacote de errata com XMLs incompatíveis

--- tests/sps/test_package.py
+++ tests/sps/test_package.py
@@ -14,3 +14,8 @@ class PackageTest(TestCase):
         obtained = PackageWithErrata(zip_path)
         self.assertTrue(obtained.is_valid())
 
+    def test_package_with_errata_xml_files_are_incompatible(self):
+        zip_path = './tests/sps/fixtures/errata_packages/with_incompatible_xml_files.zip'
+        obtained = PackageWithErrata(zip_path)
+        self.assertFalse(obtained.is_valid())
+
