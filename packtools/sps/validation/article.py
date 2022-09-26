d6bd5f9e6e6e85796bef38a9249b3fdbc18df676
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-25 22:11:08 -0300

    Identifica se dois XMLs possuem os mesmos ISSNs

--- packtools/sps/validation/article.py
+++ packtools/sps/validation/article.py
@@ -6,3 +6,14 @@ from packtools.sps.models.front_journal_meta import ISSN
 class InvalidXMLTreeError(Exception):
     ...
 
+def have_similar_issn_codes(xml1, xml2):
+    a1_issn = ISSN(xml1)
+    a2_issn = ISSN(xml2)
+    
+    if a1_issn.epub != a2_issn.epub:
+        return False
+
+    if a1_issn.ppub != a2_issn.ppub:
+        return False
+
+    return True
