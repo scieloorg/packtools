e87188317d4d8fd3ebdddd49d2bac2a29664048e
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-25 22:11:44 -0300

    Identifica se dois XMLs possuem os mesmos códigos DOI

--- packtools/sps/validation/article.py
+++ packtools/sps/validation/article.py
@@ -6,6 +6,16 @@ from packtools.sps.models.front_journal_meta import ISSN
 class InvalidXMLTreeError(Exception):
     ...
 
+def have_similar_doi_codes(xml1, xml2):
+    a1_doi_list = [d['value'] for d in DoiWithLang(xml1).data]
+    a2_doi_list = [d['value'] for d in DoiWithLang(xml2).data]
+
+    if set(a1_doi_list) ^ set(a2_doi_list):
+        return False
+
+    return True
+
+
 def have_similar_issn_codes(xml1, xml2):
     a1_issn = ISSN(xml1)
     a2_issn = ISSN(xml2)
