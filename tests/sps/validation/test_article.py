a6668f872d9a2c0f8093518d8d917410136ac46f
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-25 22:12:32 -0300

    Adiciona teste para lidar com xmls que são similares

--- tests/sps/validation/test_article.py
+++ tests/sps/validation/test_article.py
@@ -34,3 +34,16 @@ class ArticleTest(TestCase):
         """)
         self.assertFalse(are_similar_articles(self.xml_a1, xml_a2))
 
+    def test_article_are_similar_articles_results_true(self):
+        xml_a2 = get_xml_tree("""
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en">
+            <front>
+                <article-meta>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v3">zTn4sYXBrfSTMNVPF5Dm7jr</article-id>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v2">S0103-50532014001202258</article-id>
+                    <article-id pub-id-type="doi">10.5935/0103-5053.20140192</article-id>
+                </article-meta>
+            </front>
+        </article>
+        """)
+        self.assertTrue(are_similar_articles(self.xml_a1, xml_a2))
