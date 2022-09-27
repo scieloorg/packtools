c862f40295e84b3889a912b5d8ffe197ff018a1c
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-27 18:15:22 -0300

    Cria método que verifica se xmltree suporta lista de issns informada

--- packtools/sps/validation/journal.py
+++ packtools/sps/validation/journal.py
@@ -3,3 +3,16 @@ from packtools.sps import exceptions
 from packtools.sps.models.front_journal_meta import Acronym, ISSN, Title
 
 
+def are_journal_issns_compatible(xml_article, issns):
+    """
+    Params
+    ------
+    xml_article: ElementTree
+    issns: list
+    """
+    obj_journal_issn = ISSN(xml_article)
+    for i in [j for j in [obj_journal_issn.epub, obj_journal_issn.ppub] if j != '']:
+        if i not in issns:
+            raise exceptions.ArticleHasIncompatibleJournalISSNError(data={'xml': i, 'issns': issns})
+    return True
+
