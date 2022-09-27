76badb8f1d000f187e1645ace911685fe4148854
Rafael JPD <rafael.pezzuto@gmail.com>
2022-09-27 18:13:09 -0300

    Adiciona trecho de XML com dados de periódico

--- packtools/sps/models/front_journal_meta.py
+++ packtools/sps/models/front_journal_meta.py
@@ -1,3 +1,18 @@
+"""
+<journal-meta>
+    <journal-id journal-id-type="publisher-id">tinf</journal-id>
+    <journal-title-group>
+        <journal-title>Transinformação</journal-title>
+        <abbrev-journal-title abbrev-type="publisher">Transinformação</abbrev-journal-title>
+    </journal-title-group>
+    <issn pub-type="ppub">0103-3786</issn>
+    <issn pub-type="epub">2318-0889</issn>
+    <publisher>
+        <publisher-name>Pontifícia Universidade Católica de Campinas</publisher-name>
+    </publisher>
+</journal-meta>
+"""
+
 
 class ISSN:
     def __init__(self, xmltree):
