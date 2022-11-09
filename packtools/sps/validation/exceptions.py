bc1c79d2415faa07d8e536a0d0949c96d585a8fb
Rafael JPD <rafael.pezzuto@gmail.com>
2022-11-09 11:55:26 -0300

    Cria exceções relacionadas à validação de xml-lang

---
+++ packtools/sps/validation/exceptions.py
@@ -0,0 +1,15 @@
+class ValidationArticleDataError(Exception):
+    """ The root of Article data exceptions.
+    """
+    def __init__(self, *args: object, message, line):
+        super().__init__(*args)
+        self.message = message
+        self.line = line
+
+
+class ValidationArticleAndSubArticlesUnavailableLanguage(ValidationArticleDataError):
+    ...
+
+
+class ValidationArticleAndSubArticlesHasInvalidLanguage(ValidationArticleDataError):
+    ...
