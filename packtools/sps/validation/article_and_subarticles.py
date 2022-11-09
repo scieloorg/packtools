ddef35edb56ef8c0ab6b2a2990ae4df246cea39f
Rafael JPD <rafael.pezzuto@gmail.com>
2022-11-09 11:53:44 -0300

    Cria módulo e método para validar xml_lang de article_and_subarticles

---
+++ packtools/sps/validation/article_and_subarticles.py
@@ -0,0 +1,38 @@
+from packtools.sps.validation import exceptions
+from packtools.sps.models.article_and_subarticles import ArticleAndSubArticles
+from langcodes import tag_is_valid
+
+
+def validate_language(xml):
+    """
+    Params
+    ------
+    xml: ElementTree
+
+    Returns
+    -------
+        A tuple comprising the validation status and the errors list.
+    """
+    errors = []
+
+    art_and_subarts = ArticleAndSubArticles(xml)
+
+    for i in art_and_subarts.data:
+        _lang = i.get('lang')
+        _article_type = i.get('article_type')
+
+        if _lang is None:
+            _message = f'XML {_article_type} has no language.'
+            errors.append(exceptions.ValidationArticleAndSubArticlesUnavailableLanguage(
+                message=_message,
+                line=i.get('line_number'),
+            ))
+
+        if not tag_is_valid(_lang):
+            _message = f'XML {_article_type} has an invalid language: {_lang}'
+            errors.append(exceptions.ValidationArticleAndSubArticlesHasInvalidLanguage(
+                message=_message,
+                line=i.get('line_number'),
+            ))
+   
+    return len(errors) == 0, errors
