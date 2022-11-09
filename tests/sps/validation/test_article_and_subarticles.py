be54ce7ca89ca9c4fc15992a96985d43316a8c84
Rafael JPD <rafael.pezzuto@gmail.com>
2022-11-09 11:56:03 -0300

    Adiciona teste para validar xml com um idioma inválido

--- tests/sps/validation/test_article_and_subarticles.py
+++ tests/sps/validation/test_article_and_subarticles.py
@@ -22,3 +22,34 @@ class ArticleTest(TestCase):
         result, _ = validate_language(xml_tree)
         self.assertTrue(result)
 
+
+    def test_article_has_invalid_language(self):
+        xml_str = """
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="e">
+            <front>
+                <article-meta>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v3">zTn4sYXBrfSTMNVPF5Dm7jr</article-id>
+                    <article-id pub-id-type="publisher-id" specific-use="scielo-v2">S0103-50532014001202258</article-id>
+                    <article-id pub-id-type="doi">10.5935/0103-5053.20140192</article-id>
+                </article-meta>
+            </front>
+        </article>
+        """
+        xml_tree = get_xml_tree(xml_str)
+
+        result, errors = validate_language(xml_tree)
+
+        # Assegura que um problema de idioma foi identificado
+        self.assertFalse(result)
+
+        # Assegura que a mensagem relacionada ao problema identicado é a esperada
+        self.assertListEqual(
+            ['XML research-article has an invalid language: e'],
+            [e.message for e in errors]
+        )
+
+        # Assegura que a linha em que o problema foi identicado é a esperada
+        self.assertListEqual(
+            [2],
+            [e.line for e in errors]
+        )
