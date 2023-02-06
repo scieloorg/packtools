4265d908c1038acec658b85f1606ba28baa24b5e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-06 17:51:03 -0300

    Adiciona teste para verificar uma data com o valor de ano fora do intervalo

--- tests/sps/validation/test_dates.py
+++ tests/sps/validation/test_dates.py
@@ -126,3 +126,19 @@ class IsCompleteDateTest(TestCase):
         obtained = dates.is_complete(_date, 'pub-date')
         self.assertDictEqual(expected, obtained)
 
+    def test_is_complete_a_date_with_invalid_year_number(self):
+        _date = dict(
+            year='-10',
+            month='10',
+            day='15',
+        )
+        expected = dict(
+            input=_date,
+            result='error',
+            message="pub-date must contain valid values, year -10 is out of range, "
+                    "enter valid values for day, month and year",
+            element='pub-date',
+        )
+        obtained = dates.is_complete(_date, 'pub-date')
+        self.assertDictEqual(expected, obtained)
+
