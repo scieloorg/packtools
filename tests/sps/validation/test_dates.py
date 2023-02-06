e691ea654d76ec1a0a212764c64b1cf6bd53a993
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-06 17:47:23 -0300

    Adiciona teste para verificar uma data com o valor de ano inválido

--- tests/sps/validation/test_dates.py
+++ tests/sps/validation/test_dates.py
@@ -49,3 +49,18 @@ class IsCompleteDateTest(TestCase):
         obtained = dates.is_complete(_date, 'pub-date')
         self.assertDictEqual(expected, obtained)
 
+    def test_is_complete_a_date_with_invalid_year(self):
+        _date = dict(
+            year='',
+            month='2',
+            day='5',
+        )
+        expected = dict(
+            input=_date,
+            result='error',
+            message="pub-date must contain valid values, enter valid values for day, month and year",
+            element='pub-date',
+        )
+        obtained = dates.is_complete(_date, 'pub-date')
+        self.assertDictEqual(expected, obtained)
+
