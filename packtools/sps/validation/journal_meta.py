5a7ce874eb160fd389898bae9ae56ff1d6d157e6
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-09 14:21:45 -0300

    Adiciona classe 'ISSNValidation'

---
+++ packtools/sps/validation/journal_meta.py
@@ -0,0 +1,29 @@
+from ..models.journal_meta import ISSN, Acronym, Title, Publisher
+
+
+class ISSNValidation:
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
+        self.journal_issns = ISSN(xmltree)
+
+    def validate_epub(self, expected_value):
+        resp_epub = dict(
+            attrib='issn_epub',
+            input=self.xmltree,
+            output_expected=expected_value,
+            output_obteined=self.journal_issns.epub,
+            match=True if expected_value == self.journal_issns.epub else False
+        )
+        return resp_epub
+
+    def validate_ppub(self, expected_value):
+        resp_ppub = dict(
+            attrib='issn_ppub',
+            input=self.xmltree,
+            output_expected=expected_value,
+            output_obteined=self.journal_issns.ppub,
+            match=True if expected_value == self.journal_issns.ppub else False
+        )
+        return resp_ppub
+
+
