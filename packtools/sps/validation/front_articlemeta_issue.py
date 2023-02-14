cc803c92248cc1793aebed536901a5d3bd85f411
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-14 12:45:42 -0300

    Adiciona classe para validação de atributos de 'issue'

---
+++ packtools/sps/validation/front_articlemeta_issue.py
@@ -0,0 +1,106 @@
+from ..models.front_articlemeta_issue import ArticleMetaIssue
+
+
+class IssueValidation:
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
+        self.article_issue = ArticleMetaIssue(xmltree)
+
+    def validate_volume(self, expected_value):
+        """
+        Checks the correctness of a volume.
+
+        Parameters
+        ----------
+        expected_value : str
+            Correct value for volume.
+
+        Returns
+        -------
+        dict
+            A dictionary as described in the example.
+
+        Examples
+        --------
+        >>> validate_volume('23')
+
+        {
+            'object': 'volume',
+            'output_expected': '23',
+            'output_obteined': '23',
+            'match': True
+        }
+        """
+        resp_vol = dict(
+            object='volume',
+            output_expected=expected_value,
+            output_obteined=self.article_issue.volume,
+            match=True if expected_value == self.article_issue.volume else False
+        )
+        return resp_vol
+
+    def validate_issue(self, expected_value):
+        """
+        Checks the correctness of a issue.
+
+        Parameters
+        ----------
+        expected_value : str
+            Correct value for issue.
+
+        Returns
+        -------
+        dict
+            A dictionary as described in the example.
+
+        Examples
+        --------
+        >>> validate_issue('4')
+
+        {
+            'object': 'issue',
+            'output_expected': '4',
+            'output_obteined': '4',
+            'match': True
+        }
+        """
+        resp_issue = dict(
+            object='issue',
+            output_expected=expected_value,
+            output_obteined=self.article_issue.issue,
+            match=True if expected_value == self.article_issue.issue else False
+        )
+        return resp_issue
+
+    def validate_supplement(self, expected_value):
+        """
+        Checks the correctness of a supplement.
+
+        Parameters
+        ----------
+        expected_value : str
+            Correct value for supplement.
+
+        Returns
+        -------
+        dict
+            A dictionary as described in the example.
+
+        Examples
+        --------
+        >>> validate_supplement('5')
+
+        {
+            'object': 'supplement',
+            'output_expected': '5',
+            'output_obteined': '5b',
+            'match': False
+        }
+        """
+        resp_suppl = dict(
+            object='supplement',
+            output_expected=expected_value,
+            output_obteined=self.article_issue.suppl,
+            match=True if expected_value == self.article_issue.suppl else False
+        )
+        return resp_suppl
