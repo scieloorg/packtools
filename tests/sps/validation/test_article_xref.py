2e0d4865ff7d31dee6d8d6437094a6f25fdf2938
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-16 11:44:18 -0300

    Adiciona testes para 'ArticleXrefValidation'

---
+++ tests/sps/validation/test_article_xref.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+from lxml import etree
+
+from packtools.sps.validation.article_xref import ArticleXrefValidation
+
+
+class ArticleXrefTest(TestCase):
+    def setUp(self):
+        self.xmltree = etree.fromstring(
+            """
+            <article>
+                <article-meta>
+                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <aff id="aff1">
+                        <p>cited affiliation</p>
+                    </aff>
+    
+                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <fig id="fig1">
+                        <p>cited figure</p>
+                    </fig>
+    
+                    <p>citing element<xref ref-type="table" rid="table1">1</xref></p>     
+                    <table id="table1">
+                        <p>cited tablet</p>
+                    </table>
+                </article-meta>
+            </article>
+            """
+        )
+        self.article_xref = ArticleXrefValidation(self.xmltree)
+
+    def test_validate_citing_elements_matches(self):
+        expected = dict(
+            output_expected={'aff1', 'fig1', 'table1'},
+            output_obteined={'aff1', 'fig1', 'table1'},
+            match=True
+        )
+        obtained = self.article_xref.validate_citing_elements({'aff1', 'fig1', 'table1'})
+        self.assertDictEqual(expected, obtained)
+
+    def test_validate_citing_elements_no_matches(self):
+        expected = dict(
+            output_expected={'aff1', 'fig1'},
+            output_obteined={'aff1', 'fig1', 'table1'},
+            match=False
+        )
+        obtained = self.article_xref.validate_citing_elements({'aff1', 'fig1'})
+        self.assertDictEqual(expected, obtained)
+
+    def test_validate_cited_elements_matches(self):
+        expected = dict(
+            output_expected={'aff1', 'fig1', 'table1'},
+            output_obteined={'aff1', 'fig1', 'table1'},
+            match=True
+        )
+        obtained = self.article_xref.validate_cited_elements({'aff1', 'fig1', 'table1'})
+        self.assertDictEqual(expected, obtained)
+
+    def test_validate_cited_elements_no_matches(self):
+        expected = dict(
+            output_expected={'aff1', 'fig1'},
+            output_obteined={'aff1', 'fig1', 'table1'},
+            match=False
+        )
+        obtained = self.article_xref.validate_cited_elements({'aff1', 'fig1'})
+        self.assertDictEqual(expected, obtained)
+
+    def test_validate_parity_between_citing_and_cited_elements(self):
+        expected = dict(
+            output_expected=set(),
+            output_obteined=set(),
+            match=True
+        )
+        obtained = self.article_xref.validate_parity_between_citing_and_cited_elements(set())
+        self.assertDictEqual(expected, obtained)
+
+
