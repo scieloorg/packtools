c440f4883047e4479245eb561483d4ee4483779e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-24 09:54:44 -0300

    Adiciona testes para elementos citantes não pareados

--- tests/sps/validation/test_article_xref.py
+++ tests/sps/validation/test_article_xref.py
@@ -40,12 +40,33 @@ class ArticleXrefValidationTest(TestCase):
         self.assertDictEqual(expected, obtained)
 
     def test_validate_citing_elements_no_matches(self):
-        expected = dict(
-            expected_value={'aff1', 'fig1'},
-            obteined_value={'aff1', 'fig1', 'table1'},
-            match=False
+        self.xmltree = etree.fromstring(
+            """
+            <article>
+                <article-meta>
+                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <aff id="aff1">
+                        <p>cited affiliation</p>
+                    </aff>
+
+                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <fig id="fig1">
+                        <p>cited figure</p>
+                    </fig>
+
+                    <p>citing element<xref ref-type="table" rid="table1">1</xref></p>     
+                </article-meta>
+            </article>
+            """
         )
-        obtained = self.article_xref.validate_citing_elements({'aff1', 'fig1'})
+        self.article_xref = ArticleXrefValidation(self.xmltree)
+        expected = dict(
+            citing_elements={'aff1', 'fig1', 'table1'},
+            cited_elements={'aff1', 'fig1'},
+            diff={'table1'},
+            msg="ERROR: the citing elements {'table1'} do not have the respective cited elements"
+        )
+        obtained = self.article_xref.validate_citing_elements()
         self.assertDictEqual(expected, obtained)
 
     def test_validate_cited_elements_matches(self):
