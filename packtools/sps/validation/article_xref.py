1a761755a8e352371109c40a765c7dbd2959ec3e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-24 09:53:00 -0300

    Remove método redundante

--- packtools/sps/validation/article_xref.py
+++ packtools/sps/validation/article_xref.py
@@ -79,12 +79,3 @@ class ArticleXrefValidation:
     @property
     def reference_without_destiny(self):
         return self.article_xref.all_xref_rids - self.article_xref.all_ids
-
-    def validate_parity_between_citing_and_cited_elements(self, expected_value):
-        union = self.reference_without_destiny.union(self.reference_without_origin)
-        resp = dict(
-            expected_value=expected_value,
-            obteined_value=union,
-            match=(expected_value == union)
-        )
-        return resp
