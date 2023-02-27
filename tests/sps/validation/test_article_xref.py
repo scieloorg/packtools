d99194a06e9f4db3c053875c0451ff12fc8a3a36
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-27 09:24:56 -0300

    Adequa os testes

--- tests/sps/validation/test_article_xref.py
+++ tests/sps/validation/test_article_xref.py
@@ -6,24 +6,24 @@ from packtools.sps.validation.article_xref import ArticleXrefValidation
 
 class ArticleXrefValidationTest(TestCase):
 
-    def test_validate_rid_elements_matches(self):
+    def test_validate_rids_matches(self):
         self.xmltree = etree.fromstring(
             """
             <article>
                 <article-meta>
-                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <p><xref ref-type="aff" rid="aff1">1</xref></p>     
                     <aff id="aff1">
-                        <p>cited affiliation</p>
+                        <p>affiliation</p>
                     </aff>
 
-                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <p><xref ref-type="fig" rid="fig1">1</xref></p>     
                     <fig id="fig1">
-                        <p>cited figure</p>
+                        <p>figure</p>
                     </fig>
 
-                    <p>citing element<xref ref-type="table" rid="table1">1</xref></p>     
+                    <p><xref ref-type="table" rid="table1">1</xref></p>     
                     <table id="table1">
-                        <p>cited tablet</p>
+                        <p>table</p>
                     </table>
                 </article-meta>
             </article>
@@ -31,62 +31,64 @@ class ArticleXrefValidationTest(TestCase):
         )
         self.article_xref = ArticleXrefValidation(self.xmltree)
         expected = dict(
-            rid_elements={'aff1', 'fig1', 'table1'},
-            id_elements={'aff1', 'fig1', 'table1'},
-            diff=set(),
-            msg="OK: all rid elements have the respective id elements"
+            expected_value=['aff1', 'fig1', 'table1'],
+            obtained_value=['aff1', 'fig1', 'table1'],
+            result=[],
+            message="OK: all rids have the respective ids"
         )
-        obtained = self.article_xref.validate_rid_elements()
+        obtained = self.article_xref.validate_rid()
         self.assertDictEqual(expected, obtained)
 
-    def test_validate_rid_elements_no_matches(self):
+    def test_validate_rids_no_matches(self):
         self.xmltree = etree.fromstring(
             """
             <article>
                 <article-meta>
-                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <p><xref ref-type="aff" rid="aff1">1</xref></p>     
                     <aff id="aff1">
-                        <p>cited affiliation</p>
+                        <p>affiliation</p>
                     </aff>
 
-                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <p><xref ref-type="fig" rid="fig1">1</xref></p>     
                     <fig id="fig1">
-                        <p>cited figure</p>
+                        <p>figure</p>
                     </fig>
 
-                    <p>citing element<xref ref-type="table" rid="table1">1</xref></p>     
+                    <p><xref ref-type="table" rid="table1">1</xref></p>     
                 </article-meta>
             </article>
             """
         )
         self.article_xref = ArticleXrefValidation(self.xmltree)
         expected = dict(
-            rid_elements={'aff1', 'fig1', 'table1'},
-            id_elements={'aff1', 'fig1'},
-            diff={'table1'},
-            msg="ERROR: the rid elements {'table1'} do not have the respective id elements"
+            expected_value=['aff1', 'fig1', 'table1'],
+            obtained_value=['aff1', 'fig1'],
+            result=['table1'],
+            message="ERROR: rids were found with the values "
+                f"{self.article_xref.validate_rid()['expected_value']} but there were "
+                "no ids with the corresponding values"
         )
-        obtained = self.article_xref.validate_rid_elements()
+        obtained = self.article_xref.validate_rid()
         self.assertDictEqual(expected, obtained)
 
-    def test_validate_id_elements_matches(self):
+    def test_validate_ids_matches(self):
         self.xmltree = etree.fromstring(
             """
             <article>
                 <article-meta>
-                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <p><xref ref-type="aff" rid="aff1">1</xref></p>     
                     <aff id="aff1">
-                        <p>cited affiliation</p>
+                        <p>affiliation</p>
                     </aff>
 
-                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <p><xref ref-type="fig" rid="fig1">1</xref></p>     
                     <fig id="fig1">
-                        <p>cited figure</p>
+                        <p>figure</p>
                     </fig>
 
-                    <p>citing element<xref ref-type="table" rid="table1">1</xref></p>     
+                    <p><xref ref-type="table" rid="table1">1</xref></p>     
                     <table id="table1">
-                        <p>cited tablet</p>
+                        <p>table</p>
                     </table>
                 </article-meta>
             </article>
@@ -94,31 +96,31 @@ class ArticleXrefValidationTest(TestCase):
         )
         self.article_xref = ArticleXrefValidation(self.xmltree)
         expected = dict(
-            rid_elements={'aff1', 'fig1', 'table1'},
-            id_elements={'aff1', 'fig1', 'table1'},
-            diff=set(),
-            msg="OK: all id elements have the respective rid elements"
+            obtained_value=['aff1', 'fig1', 'table1'],
+            expected_value=['aff1', 'fig1', 'table1'],
+            result=[],
+            message="OK: all ids have the respective rids"
         )
-        obtained = self.article_xref.validate_id_elements()
+        obtained = self.article_xref.validate_id()
         self.assertDictEqual(expected, obtained)
 
-    def test_validate_id_elements_no_matches(self):
+    def test_validate_ids_no_matches(self):
         self.xmltree = etree.fromstring(
             """
             <article>
                 <article-meta>
-                    <p>citing element<xref ref-type="aff" rid="aff1">1</xref></p>     
+                    <p><xref ref-type="aff" rid="aff1">1</xref></p>     
                     <aff id="aff1">
-                        <p>cited affiliation</p>
+                        <p>affiliation</p>
                     </aff>
 
-                    <p>citing element<xref ref-type="fig" rid="fig1">1</xref></p>     
+                    <p><xref ref-type="fig" rid="fig1">1</xref></p>     
                     <fig id="fig1">
-                        <p>cited figure</p>
+                        <p>figure</p>
                     </fig>
     
                     <table id="table1">
-                        <p>cited tablet</p>
+                        <p>table</p>
                     </table>
                 </article-meta>
             </article>
@@ -126,10 +128,12 @@ class ArticleXrefValidationTest(TestCase):
         )
         self.article_xref = ArticleXrefValidation(self.xmltree)
         expected = dict(
-            rid_elements={'aff1', 'fig1'},
-            id_elements={'aff1', 'fig1', 'table1'},
-            diff={'table1'},
-            msg="ERROR: the id elements {'table1'} do not have the respective rid elements"
+            obtained_value=['aff1', 'fig1'],
+            expected_value=['aff1', 'fig1', 'table1'],
+            result=['table1'],
+            message="ERROR: ids were found with the values "
+                f"{self.article_xref.validate_id()['expected_value']} but there were "
+                "no rids with the corresponding values"
         )
-        obtained = self.article_xref.validate_id_elements()
+        obtained = self.article_xref.validate_id()
         self.assertDictEqual(expected, obtained)
