2a528cc43478aaa5e3e3fa694188a8d303be06e2
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-28 13:21:02 -0300

    Corrige classe para extração de dados do XML

--- packtools/sps/models/article_toc_sections.py
+++ packtools/sps/models/article_toc_sections.py
@@ -1,16 +1,72 @@
+from packtools.sps.utils import xml_utils
+
+
 class ArticleTocSections:
 
     def __init__(self, xmltree):
         self.xmltree = xmltree
 
     @property
-    def subj_group_type(self):
-        return self.xmltree.findtext('.//article-meta//'
-                                     'subj-group[@subj-group-type="heading"]/'
-                                     'subject') or ''
+    def article_section(self):
+        _sections = []
+        for node_with_lang in xml_utils.get_nodes_with_lang(
+                self.xmltree,
+                ".",
+                ".//article-meta//subj-group[@subj-group-type='heading']/subject"
+        ):
+            _section = {
+                "lang": node_with_lang["lang"],
+                "text": xml_utils.node_text_without_xref(node_with_lang["node"]),
+            }
+            _sections.append(_section)
+        return _sections
 
     @property
-    def subj_group(self):
-        return self.xmltree.findtext('.//article-meta//'
-                                     'subj-group[@subj-group-type="heading"]/'
-                                     'subj-group/subject') or ''
+    def article_section_dict(self):
+        return {
+            item['lang']: item['text']
+            for item in self.article_section
+        }
+
+    @property
+    def sub_article_section(self):
+        _sections = []
+        for node_with_lang in xml_utils.get_nodes_with_lang(
+                self.xmltree,
+                ".//sub-article[@article-type='translation']",
+                ".//front-stub//subj-group[@subj-group-type='heading']/subject"
+        ):
+
+            _section = {
+                "lang": node_with_lang["lang"],
+                "text": xml_utils.node_text_without_xref(node_with_lang["node"]),
+            }
+            _sections.append(_section)
+        return _sections
+
+    @property
+    def sub_article_section_dict(self):
+        return {
+            item['lang']: item['text']
+            for item in self.sub_article_section
+        }
+
+    @property
+    def sub_section(self):
+        _sub_sections = []
+        for node_with_lang in xml_utils.get_nodes_with_lang(
+                self.xmltree,
+                ".", ".//article-meta//subj-group[@subj-group-type='heading']/subj-group/subject"):
+            _sub_section = {
+                "lang": node_with_lang["lang"],
+                "text": xml_utils.node_text_without_xref(node_with_lang["node"]),
+            }
+            _sub_sections.append(_sub_section)
+        return _sub_sections
+
+    @property
+    def sub_section_dict(self):
+        return {
+            item['lang']: item['text']
+            for item in self.sub_section
+        }
