9f5133ef50504caa50fed206e12d1d55ab517196
Samuel Veiga <samuelveigarangel@hotmail.com>
2023-03-05 18:07:36 -0300

    Inserido atributo para extrair kwd com subtags

--- packtools/sps/models/kwd_group.py
+++ packtools/sps/models/kwd_group.py
@@ -1,37 +1,49 @@
-"""
-    <kwd-group xml:lang="en">
-        <kwd>Triatoma sordida</kwd>
-        <kwd>Triatoma guasayana</kwd>
-        <kwd>Trypanosoma cruzi</kwd>
-        <kwd>dispersive flight</kwd>
-        <kwd>Argentina</kwd>
-    </kwd-group>
-"""
+from packtools.sps.utils import xml_utils
+
+
+def get_node_without_subtag(node):
+    """
+        Função que retorna nó sem subtags. 
+    """
+    return "".join(node.xpath(".//text()"))
+
 
 class KwdGroup:
-    def __init__(self, xmltree):
+    def __init__(self, xmltree, subtag=False):
         self._xmltree = xmltree
+        self._subtag = subtag
+
+    def illegal_argument_error(self):
+        if not isinstance(self._subtag, bool):
+            raise TypeError(
+                f"Argument subtag must be True or False, not {type(self._subtag)}")
 
     @property
     def extract_kwd_data_with_lang_text(self):
+        
+        self.illegal_argument_error()
+        
         _data = []
+        kwd_text = xml_utils.node_text_without_xref if self._subtag else get_node_without_subtag
+
         for kwd_group in self._xmltree.xpath('.//kwd-group'):
             _lang = kwd_group.get("{http://www.w3.org/XML/1998/namespace}lang")
             for kwd in kwd_group.xpath("kwd"):
-                _data.append({"lang": _lang, "text": kwd.text})
-        
+                kwd = kwd_text(kwd)
+                _data.append({"lang": _lang, "text": kwd})
         return _data
 
     @property
     def extract_kwd_extract_data_by_lang(self):
+        
+        self.illegal_argument_error()
+        
         _data_dict = {}
         _data = self.extract_kwd_data_with_lang_text
-        
+
         for d in _data:
             if d['lang'] not in _data_dict:
-                _data_dict[d['lang']] = []    
+                _data_dict[d['lang']] = []
             _data_dict[d['lang']].append(d['text'])
         return _data_dict
-       
-
-
+ 
--- tests/sps/test_kwd_group.py
+++ tests/sps/test_kwd_group.py
@@ -1,23 +1,3 @@
-"""
-    <kwd-group xml:lang="en">
-        <title>KEYWORDS</title>
-        <kwd>Primary health care</kwd>
-        <kwd>Ambulatory care facilities</kwd>
-        <kwd>Chronic pain</kwd>
-        <kwd>Analgesia</kwd>
-        <kwd>Pain management</kwd>
-    </kwd-group>
-
-    <kwd-group xml:lang="pt">
-        <title>PALAVRAS-CHAVE</title>
-        <kwd>Atenção primária à saúde</kwd>
-        <kwd>Instituições de assistência ambulatorial</kwd>
-        <kwd>Dor crônica</kwd>
-        <kwd>Analgesia</kwd>
-        <kwd>Tratamento da dor</kwd>
-    </kwd-group>
-"""
-
 from unittest import TestCase
 
 from packtools.sps.utils import xml_utils
@@ -25,15 +5,24 @@ from packtools.sps.utils import xml_utils
 from packtools.sps.models.kwd_group import KwdGroup 
 
 
+def get_kwd_data_with_lang(data, subtag=False):
+    xmltree = xml_utils.get_xml_tree(data)
+    return KwdGroup(xmltree, subtag=subtag).extract_kwd_data_with_lang_text
+
+def get_kwd_data_without_key_text(data, subtag=False):
+    xmltree = xml_utils.get_xml_tree(data)
+    return KwdGroup(xmltree, subtag=subtag).extract_kwd_extract_data_by_lang
+
+
 class KwdGroupTest(TestCase):
 
     def test_extract_kwd_data_with_lang(self):
-        
+
+        # xml sem subtags em kwd
         with open('tests/samples/0034-7094-rba-69-03-0227.xml', 'r', encoding='utf-8') as f:
             data = f.read()
-
-        xmltree = xml_utils.get_xml_tree(data)
-        kwd_extract_data_with_lang_text = KwdGroup(xmltree).extract_kwd_data_with_lang_text
+        
+        kwd_extract_data_with_lang_text = get_kwd_data_with_lang(data)
 
         expected_output = [
             {'lang': 'en', 'text': 'Primary health care'}, 
@@ -45,18 +34,18 @@ class KwdGroupTest(TestCase):
             {'lang': 'pt', 'text': 'Instituições de assistência ambulatorial'}, 
             {'lang': 'pt', 'text': 'Dor crônica'}, 
             {'lang': 'pt', 'text': 'Analgesia'}, 
-            {'lang': 'pt', 'text': 'Tratamento da dor'}]
+            {'lang': 'pt', 'text': 'Tratamento da dor'}
+        ]
         
         self.assertEqual(kwd_extract_data_with_lang_text, expected_output)
 
+    def test_extract_kwd_data_without_key_text(self):
 
-    def test_data_without_key(self):
-
+        # xml sem subtags em kwd
         with open('tests/samples/0034-7094-rba-69-03-0227.xml', 'r', encoding='utf-8') as f:
             data = f.read()
-       
-        xmltree = xml_utils.get_xml_tree(data)
-        kwd_extract_by_lang = KwdGroup(xmltree).extract_kwd_extract_data_by_lang
+
+        kwd_extract_by_lang = get_kwd_data_without_key_text(data)
         
         expected_output = {
             'en': [
@@ -76,3 +65,87 @@ class KwdGroupTest(TestCase):
             }
 
         self.assertEqual(kwd_extract_by_lang, expected_output)
+
+    def test_extract_kwd_data_with_lang_subtag(self):
+       
+        # xml com subtags em kwd
+        with open('tests/samples/0034-8910-rsp-48-2-0296.xml', 'r', encoding='utf-8') as f:
+            data = f.read()
+
+        kwd_extract_kwd_with_lang_subtag = get_kwd_data_with_lang(data, subtag=True)
+
+        expected_output = [
+            {'lang': 'en', 'text': 'Chagas Disease, transmission'}, 
+            {'lang': 'en', 'text': 'Triatominae, <italic>Trypanosoma cruzi</italic>, isolation'}, 
+            {'lang': 'en', 'text': 'Communicable Diseases, epidemiology'}, 
+            {'lang': 'pt', 'text': 'Doença de Chagas, transmissão'}, 
+            {'lang': 'pt', 'text': 'Triatominae, <italic>Trypanosoma cruzi</italic>, isolamento'}, 
+            {'lang': 'pt', 'text': 'Doenças Transmissíveis, epidemiologia'}
+        ]
+
+        self.assertEqual(kwd_extract_kwd_with_lang_subtag, expected_output)
+
+    def test_extract_kwd_data_without_lang_subtag(self):
+        
+        # xml com subtags em kwd
+        with open('tests/samples/0034-8910-rsp-48-2-0296.xml', 'r', encoding='utf-8') as f:
+            data = f.read()
+
+        kwd_extract_kwd_without_subtag = get_kwd_data_with_lang(data, subtag=False)
+
+        expected_output = [
+            {'lang': 'en', 'text': 'Chagas Disease, transmission'}, 
+            {'lang': 'en', 'text': 'Triatominae, Trypanosoma cruzi, isolation'}, 
+            {'lang': 'en', 'text': 'Communicable Diseases, epidemiology'}, 
+            {'lang': 'pt', 'text': 'Doença de Chagas, transmissão'}, 
+            {'lang': 'pt', 'text': 'Triatominae, Trypanosoma cruzi, isolamento'}, 
+            {'lang': 'pt', 'text': 'Doenças Transmissíveis, epidemiologia'}
+        ]
+
+        self.assertEqual(kwd_extract_kwd_without_subtag, expected_output)
+
+    def test_extract_kwd_data_with_subtag(self):
+                
+        # xml com subtags em kwd
+        with open('tests/samples/0034-8910-rsp-48-2-0296.xml', 'r', encoding='utf-8') as f:
+            data = f.read()
+
+        kwd_extract_kwd_with_subtag = get_kwd_data_without_key_text(data, subtag=True)
+        
+        expected_output = {
+            'en': [
+                'Chagas Disease, transmission', 
+                'Triatominae, <italic>Trypanosoma cruzi</italic>, isolation', 
+                'Communicable Diseases, epidemiology'
+            ], 
+            'pt': [
+                'Doença de Chagas, transmissão', 
+                'Triatominae, <italic>Trypanosoma cruzi</italic>, isolamento', 
+                'Doenças Transmissíveis, epidemiologia'
+                ]
+            }
+        
+        self.assertEqual(kwd_extract_kwd_with_subtag, expected_output)
+    
+    def test_extract_kwd_data_without_subtag(self):
+                
+        # xml com subtags em kwd
+        with open('tests/samples/0034-8910-rsp-48-2-0296.xml', 'r', encoding='utf-8') as f:
+            data = f.read()
+
+        kwd_extract_kwd_with_subtag = get_kwd_data_without_key_text(data, subtag=False)
+
+        expected_output = {
+            'en': [
+                'Chagas Disease, transmission', 
+                'Triatominae, Trypanosoma cruzi, isolation', 
+                'Communicable Diseases, epidemiology'
+            ], 
+            'pt': [
+                'Doença de Chagas, transmissão', 
+                'Triatominae, Trypanosoma cruzi, isolamento', 
+                'Doenças Transmissíveis, epidemiologia'
+                ]
+            }
+        
+        self.assertEqual(kwd_extract_kwd_with_subtag, expected_output)
