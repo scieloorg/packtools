d128ec3ac57aa19e65d48d6fc715a1d0bf1fdd1f
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-03-06 16:38:48 -0300

    Adiciona a classe de testes 'ArticleLicenseTest'

---
+++ tests/sps/test_article_license.py
@@ -0,0 +1,72 @@
+from unittest import TestCase
+
+from lxml import etree
+
+from packtools.sps.models.article_license import ArticleLicense
+
+
+class ArticleLicenseTest(TestCase):
+    def setUp(self):
+        xml = ("""
+                <article xmlns:xlink="http://www.w3.org/1999/xlink">
+                <front>
+                <article-meta>
+                    <permissions>
+                        <license license-type="open-access" 
+                        xlink:href="http://creativecommons.org/licenses/by/4.0/" 
+                        xml:lang="en">
+                            <license-p>This is an article published in open access under a Creative Commons license.</license-p>
+                        </license>
+                        <license license-type="open-access" 
+                        xlink:href="http://creativecommons.org/licenses/by/4.0/" 
+                        xml:lang="pt">
+                            <license-p>Este é um artigo publicado em acesso aberto sob uma licença Creative Commons.</license-p>
+                        </license>
+                        <license license-type="open-access" 
+                        xlink:href="http://creativecommons.org/licenses/by/4.0/" 
+                        xml:lang="es">
+                            <license-p>Este es un artículo publicado en acceso abierto bajo una licencia Creative Commons.</license-p>
+                        </license>
+                    </permissions>
+                </article-meta>
+                </front>
+                </article>
+                """)
+        xmltree = etree.fromstring(xml)
+        self.article_license = ArticleLicense(xmltree)
+
+    def test_get_link(self):
+        expected = [
+            {
+                'text': 'http://creativecommons.org/licenses/by/4.0/',
+                'lang': 'en'
+            },
+            {
+                'text': 'http://creativecommons.org/licenses/by/4.0/',
+                'lang': 'pt'
+            },
+            {
+                'text': 'http://creativecommons.org/licenses/by/4.0/',
+                'lang': 'es'
+            }
+        ]
+
+        self.assertEqual(expected, self.article_license.link)
+
+    def test_get_license_p(self):
+        expected = [
+            {
+                'text': 'This is an article published in open access under a Creative Commons license.',
+                'lang': 'en'
+            },
+            {
+                'text': 'Este é um artigo publicado em acesso aberto sob uma licença Creative Commons.',
+                'lang': 'pt'
+            },
+            {
+                'text': 'Este es un artículo publicado en acceso abierto bajo una licencia Creative Commons.',
+                'lang': 'es'
+            }
+        ]
+
+        self.assertEqual(expected, self.article_license.license_p)
