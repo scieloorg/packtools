00f94a7cd233a219ff5da1e1f8813c28a6d7cff1
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-04-23 18:26:22 -0300

    Corrige a obtenção de 'xml_crossref' em 'test_xml_depositor_pipe'

--- tests/sps/formats/test_crossref.py
+++ tests/sps/formats/test_crossref.py
@@ -132,8 +132,13 @@ class PipelineCrossref(TestCase):
             "depositor_email_address": "name@domain.com"
         }
 
-        xml_crossref = setupdoibatch_pipe()
-        xml_head_pipe(xml_crossref)
+        xml_crossref = ET.fromstring(
+            '<doi_batch>'
+            '<head>'
+            '</head>'
+            '</doi_batch>'
+        )
+
         xml_depositor_pipe(xml_crossref, data)
 
         self.obtained = ET.tostring(xml_crossref, encoding="utf-8").decode("utf-8")
