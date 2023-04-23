4e77adbe83624fa29fc2f71964848bf83ed7779d
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-04-23 18:31:56 -0300

    Corrige a obtenção de 'xml_crossref' em 'test_xml_pubdate_pipe'

--- tests/sps/formats/test_crossref.py
+++ tests/sps/formats/test_crossref.py
@@ -373,11 +373,16 @@ class PipelineCrossref(TestCase):
             '</journal>'
             '</body>'
         )
-
-        xml_crossref = setupdoibatch_pipe()
-        xml_body_pipe(xml_crossref)
-        xml_journal_pipe(xml_crossref)
-        xml_journalissue_pipe(xml_crossref)
+        xml_crossref = ET.fromstring(
+            '<doi_batch>'
+            '<body>'
+            '<journal>'
+            '<journal_issue>'
+            '</journal_issue>'
+            '</journal>'
+            '</body>'
+            '</doi_batch>'
+        )
 
         xml_pubdate_pipe(xml_tree, xml_crossref)
 
