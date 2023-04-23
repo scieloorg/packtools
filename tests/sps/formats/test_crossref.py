68d2690c1bd03e28cbd768fd8b4bdfab563a472c
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-04-23 18:27:50 -0300

    Corrige a obtenção de 'xml_crossref' em 'test_xml_journal_pipe'

--- tests/sps/formats/test_crossref.py
+++ tests/sps/formats/test_crossref.py
@@ -175,8 +175,14 @@ class PipelineCrossref(TestCase):
         self.assertIsNotNone(xml_crossref.find('body'))
 
     def test_xml_journal_pipe(self):
-        xml_crossref = setupdoibatch_pipe()
-        xml_body_pipe(xml_crossref)
+        xml_crossref = ET.fromstring(
+            '<doi_batch>'
+            '<head>'
+            '</head>'
+            '<body>'
+            '</body>'
+            '</doi_batch>'
+        )
         xml_journal_pipe(xml_crossref)
 
         self.assertIsNotNone(xml_crossref.find('./body/journal'))
