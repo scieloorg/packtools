8824cf6df53e392710bf8e04d744167e0e49cd8d
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-04-28 15:32:57 -0300

    Refatora 'article_citations'

--- packtools/sps/models/article_citations.py
+++ packtools/sps/models/article_citations.py
@@ -8,15 +8,32 @@ class ArticleCitations:
     def article_citations(self):
         _citations = []
         for node in self.xmltree.xpath("./back/ref-list//ref"):
-            d = dict(
-                label=node.xpath('./label')[0].text[:-1],
-                journal_title=node.xpath('./element-citation/source')[0].text,
-                author=" ".join([node.xpath('./element-citation/person-group/name/surname')[0].text,
-                                node.xpath('./element-citation/person-group/name/given-names')[0].text]),
-                volume=node.xpath('./element-citation/volume')[0].text,
-                first_page=node.xpath('./element-citation/fpage')[0].text,
-                cYear=node.xpath('./element-citation/year')[0].text,
-                article_title=node.xpath('./element-citation/article-title')[0].text
-            )
+            label = get_label(node)
+            source = get_source(node)
+            author = get_author(node)
+            volume = get_volume(node)
+            issue = get_issue(node)
+            fpage = get_fpage(node)
+            year = get_year(node)
+            article_title = get_article_title(node)
+
+            tags = [
+                ('label', label),
+                ('source', source),
+                ('author', author),
+                ('volume', volume),
+                ('issue', issue),
+                ('fpage', fpage),
+                ('year', year),
+                ('article_title', article_title)
+            ]
+
+            d = dict()
+            for name, value in tags:
+                if value is not None:
+                    try:
+                        d[name] = value.text
+                    except AttributeError:
+                        d[name] = value
             _citations.append(d)
         return _citations
