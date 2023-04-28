c1162e93901f13a6bc08b1db0da12d3f293864c0
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-04-28 15:33:18 -0300

    Adiciona funções auxiliares em 'article_citations'

--- packtools/sps/models/article_citations.py
+++ packtools/sps/models/article_citations.py
@@ -1,3 +1,39 @@
+def get_label(node):
+    return node.find('./label').text[:-1]
+
+
+def get_source(node):
+    return node.find('./element-citation/source')
+
+
+def get_author(node):
+    surname = node.find('./element-citation/person-group/name/surname')
+    given_name = node.find('./element-citation/person-group/name/given-names')
+    if surname is not None and given_name is not None:
+        return " ".join([surname.text, given_name.text])
+    else:
+        return None
+
+
+def get_volume(node):
+    return node.find('./element-citation/volume')
+
+
+def get_issue(node):
+    return node.find('./element-citation/issue')
+
+
+def get_fpage(node):
+    return node.find('./element-citation/fpage')
+
+
+def get_year(node):
+    return node.find('./element-citation/year')
+
+
+def get_article_title(node):
+    return node.find('./element-citation/article-title')
+
 
 class ArticleCitations:
 
