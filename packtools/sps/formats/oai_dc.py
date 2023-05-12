fae78b33a3219c9548c4ca5b3848a78a6d02d191
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-05-11 23:42:30 -0300

    Complementa descrição em 'xml_oai_dc_description'

--- packtools/sps/formats/oai_dc.py
+++ packtools/sps/formats/oai_dc.py
@@ -314,21 +314,37 @@ def xml_oai_dc_subject(xml_oai_dc, xml_tree):
 
 def xml_oai_dc_description(xml_oai_dc, xml_tree):
     """
-    <dc:description>
-        <![CDATA[ Resumen En el siglo xx, la música popular fue el lenguaje artístico más
-        contundente y una de las grandes fuerzas estéticas de Brasil. Desde los años 30, ella
-        ocupó una posición importante en debates culturales y proyectos nacionalistas y, a lo
-        largo de los años, expandió su alcance con el avance de los medios de comunicación.
-        Dicha inversión todavía resultó, más allá de lo esperado, en la producción de una
-        modalidad crítica de canción. Así que en los 60, al igual que el arte moderno, la canción
-        se tornó reflexiva, operando de manera crítica en relación consigo misma y con el contexto
-        cultural y político. El componente reflexivo de la canción parece haber ampliado
-        considerablemente su alcance cultural. Tanto es así que no parece irrazonable señalar
-        una conexión profunda entre la canción y un cierto horizonte de construcción nacional.
-        En este trabajo, intentamos hacer un panorama del desarrollo de la música popular brasileña
-        de modo de aclarar ese proceso y, tal vez, posibilitar nuevos planteamientos para pensar
-        sus posibilidades críticas desde su dimensión estética. ]]>
-    </dc:description>
+    Schema (http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#dublincore):
+        <complexType name="oai_dcType">
+            <choice minOccurs="0" maxOccurs="unbounded">
+                <element ref="dc:description"/>
+            </choice>
+        </complexType>
+
+    Element definition (https://www.dublincore.org/specifications/dublin-core/dces/):
+        Term Name:  description
+        URI:	    http://purl.org/dc/elements/1.1/description
+        Label:	    Description
+        Definition:	An account of the resource.
+        Comment:	Description may include but is not limited to: an abstract, a table of contents, a graphical
+                    representation, or a free-text account of the resource.
+
+    Example:
+        <dc:description>
+            <![CDATA[ Resumen En el siglo xx, la música popular fue el lenguaje artístico más
+            contundente y una de las grandes fuerzas estéticas de Brasil. Desde los años 30, ella
+            ocupó una posición importante en debates culturales y proyectos nacionalistas y, a lo
+            largo de los años, expandió su alcance con el avance de los medios de comunicación.
+            Dicha inversión todavía resultó, más allá de lo esperado, en la producción de una
+            modalidad crítica de canción. Así que en los 60, al igual que el arte moderno, la canción
+            se tornó reflexiva, operando de manera crítica en relación consigo misma y con el contexto
+            cultural y político. El componente reflexivo de la canción parece haber ampliado
+            considerablemente su alcance cultural. Tanto es así que no parece irrazonable señalar
+            una conexión profunda entre la canción y un cierto horizonte de construcción nacional.
+            En este trabajo, intentamos hacer un panorama del desarrollo de la música popular brasileña
+            de modo de aclarar ese proceso y, tal vez, posibilitar nuevos planteamientos para pensar
+            sus posibilidades críticas desde su dimensión estética. ]]>
+        </dc:description>
     """
     abstract = article_abstract.Abstract(xml_tree)
 
