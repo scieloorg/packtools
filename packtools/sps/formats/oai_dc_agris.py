61e0abddd8a585c235cb4a16a1bf3eaaa5007f90
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-05-25 14:17:56 -0300

    Adiciona 'get_identifier'

---
+++ packtools/sps/formats/oai_dc_agris.py
@@ -0,0 +1,35 @@
+from lxml import etree as ET
+from packtools.sps.models import (
+    article_ids,
+    journal_meta,
+    article_titles,
+    article_authors,
+    kwd_group,
+    article_and_subarticles,
+    article_abstract,
+    dates,
+    front_articlemeta_issue,
+)
+
+
+def get_identifier(xml_tree):
+    """
+    Schema (https://agris.fao.org/agris_ods/dlio.dtd.txt):
+        <!-- ELEMENT identifier -->
+        <!ELEMENT dc:identifier (#PCDATA)>
+        <!ATTLIST dc:identifier
+            xml:lang CDATA #IMPLIED
+            scheme (ags:IPC | ags:RN | ags:PN | ags:ISBN | ags:JN | dcterms:URI | ags:DOI | ags:PC) #IMPLIED
+        >
+
+    Example:
+        <identifier>oai:agris.scielo:XS2021000111</identifier>
+    """
+    try:
+        identifier = article_ids.ArticleIds(xml_tree).v2
+        if len(identifier) == 23:
+            return ''.join([identifier[10:18], identifier[21:]])
+    except TypeError:
+        pass
+
+
