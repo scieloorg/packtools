a8a805ce8599dee7f13bb6cf2b0f61cdbacd83a9
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-05-25 14:36:44 -0300

    Adiciona 'test_xml_oai_dc_agris_title_pipe_without_title'

--- tests/sps/formats/test_oai_dc_agris.py
+++ tests/sps/formats/test_oai_dc_agris.py
@@ -350,3 +350,51 @@ class TestPipelineOaiDcAgris(unittest.TestCase):
 
         self.assertIn(expected, self.obtained)
 
+    def test_xml_oai_dc_agris_title_pipe_without_title(self):
+        xml_tree = ET.fromstring(
+            '<article '
+            'xmlns:mml="http://www.w3.org/1998/Math/MathML" '
+            'xmlns:xlink="http://www.w3.org/1999/xlink" '
+            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xml:lang="es">'
+            '<front>'
+            '<article-meta>'
+            '<article-id>S0718-71812021000100011</article-id>'
+            '<article-id pub-id-type="doi">10.7764/69.1</article-id>'
+            '</article-meta>'
+            '</front>'
+            '</article>'
+        )
+
+        expected = (
+            '<metadata>'
+            '<ags:resources '
+            'xmlns:xsl="http://www.w3.org/1999/XSL/Transform" '
+            'xmlns:ags="http://purl.org/agmes/1.1/" '
+            'xmlns:dc="http://purl.org/dc/elements/1.1/" '
+            'xmlns:agls="http://www.naa.gov.au/recordkeeping/gov_online/agls/1.2" '
+            'xmlns:dcterms="http://purl.org/dc/terms/">'
+            '<ags:resource ags:ARN="XS2021000111"/>'
+            '</ags:resources>'
+            '</metadata>'
+        )
+
+        xml_oai_dc_agris = ET.fromstring(
+            '<metadata>'
+            '<ags:resources '
+            'xmlns:xsl="http://www.w3.org/1999/XSL/Transform" '
+            'xmlns:ags="http://purl.org/agmes/1.1/" '
+            'xmlns:dc="http://purl.org/dc/elements/1.1/" '
+            'xmlns:agls="http://www.naa.gov.au/recordkeeping/gov_online/agls/1.2" '
+            'xmlns:dcterms="http://purl.org/dc/terms/">'
+            '<ags:resource ags:ARN="XS2021000111">'
+            '</ags:resource>'
+            '</ags:resources>'
+            '</metadata>'
+        )
+
+        xml_oai_dc_agris_title_pipe(xml_oai_dc_agris, xml_tree)
+
+        self.obtained = ET.tostring(xml_oai_dc_agris, encoding="utf-8").decode("utf-8")
+
+        self.assertEqual(expected, self.obtained)
+
