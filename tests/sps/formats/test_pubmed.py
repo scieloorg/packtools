4e29278a46af32a6a56a6cb21a7a5ebe6d749d83
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-06-06 14:07:26 -0300

    Adiciona 'test_xml_pubmed_journal_title_pipe_without_title'

--- tests/sps/formats/test_pubmed.py
+++ tests/sps/formats/test_pubmed.py
@@ -138,3 +138,31 @@ class PipelinePubmed(unittest.TestCase):
 
         self.assertEqual(obtained, expected)
 
+    def test_xml_pubmed_journal_title_pipe_without_title(self):
+        expected = (
+            '<Article>'
+            '<Journal/>'
+            '</Article>'
+        )
+        xml_pubmed = ET.fromstring(
+            '<Article>'
+            '<Journal/>'
+            '</Article>'
+        )
+        xml_tree = ET.fromstring(
+            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
+            'xmlns:xlink="http://www.w3.org/1999/xlink" '
+            'article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">'
+            '<front>'
+            '<journal-meta>'
+            '</journal-meta>'
+            '</front>'
+            '</article>'
+        )
+
+        xml_pubmed_journal_title_pipe(xml_pubmed, xml_tree)
+
+        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
+
+        self.assertEqual(obtained, expected)
+
