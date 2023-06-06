9b50048affc85ef569dec9b6d8a83bf4159c78c2
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-06-06 13:59:17 -0300

    Adiciona 'xml_pubmed_article_pipe'

---
+++ packtools/sps/formats/pubmed.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from lxml import etree as ET
+
+from packtools.sps.models import (
+    journal_meta,
+    front_articlemeta_issue,
+    dates,
+    article_titles,
+    article_ids,
+    article_and_subarticles,
+)
+
+
+def xml_pubmed_article_pipe():
+    root = ET.Element("Article")
+    tree = ET.ElementTree(root)
+
+    return tree
+
+
