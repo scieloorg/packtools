d7f5972c9f0aa2e896cc59e2daf98ae5f7f33ae8
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-06-27 22:47:13 -0300

    Adiciona 'test_xml_pubmed_without_title_reference_list'

--- tests/sps/formats/test_pubmed.py
+++ tests/sps/formats/test_pubmed.py
@@ -1332,6 +1332,33 @@ class PipelinePubmed(unittest.TestCase):
 
         self.assertEqual(obtained, expected)
 
+    def test_xml_pubmed_without_title_reference_list(self):
+        expected = (
+            '<Article>'
+            '<ReferenceList/>'
+            '</Article>'
+        )
+        xml_pubmed = ET.fromstring(
+            '<Article>'
+            '<ReferenceList/>'
+            '</Article>'
+        )
+        xml_tree = ET.fromstring(
+            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
+            'xmlns:xlink="http://www.w3.org/1999/xlink" '
+            'article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">'
+            '<back>'
+            '<ref-list/>'
+            '</back>'
+            '</article>'
+        )
+
+        xml_pubmed_title_reference_list(xml_pubmed, xml_tree)
+
+        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
+
+        self.assertEqual(obtained, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
