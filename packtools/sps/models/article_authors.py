155cda68297a643517170212df0f9994d1e3b5f4
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-19 14:25:58 -0300

    Adiciona os dados de afiliaçõe junto aos dados dos autores

--- packtools/sps/models/article_authors.py
+++ packtools/sps/models/article_authors.py
@@ -1,3 +1,6 @@
+from packtools.sps.models.aff import Affiliation
+
+
 class Authors:
     def __init__(self, xmltree):
         self.xmltree = xmltree
@@ -44,3 +47,14 @@ class Authors:
             _author['contrib-type'] = node.attrib['contrib-type']
             _data.append(_author)
         return _data
+
+    @property
+    def contribs_with_affs(self):
+        affs = Affiliation(self.xmltree)
+        affs_by_id = affs.affiliation_by_id
+
+        for item in self.contribs:
+            for rid in item["aff_rids"]:
+                item.setdefault("affs", [])
+                item["affs"].append(affs_by_id.get(rid))
+            yield item
