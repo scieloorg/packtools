8b9cf6ea702e3dacfee62a41ee6792f3d4b04eca
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-07-19 23:40:23 -0300

    Adiciona 'xml_pmc_authors'

--- packtools/sps/formats/pmc.py
+++ packtools/sps/formats/pmc.py
@@ -10,3 +10,13 @@ def pipeline_pmc(xml_tree):
     return xml_tree
 
 
+def xml_pmc_authors(xml_tree):
+    authors = xml_tree.findall(".//contrib")
+    for author in authors:
+        contrib_id = author.find("./contrib-id")
+        author.remove(contrib_id)
+        label = author.find("./xref/sup")
+        author.find("./xref").text = label.text
+        author.find("./xref").remove(label)
+
+
