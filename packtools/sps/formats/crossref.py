7d923cfa5c448e678ffd98e265ae96da55ac9a92
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-07-27 17:41:53 -0300

    Adiciona 'Docstring' em 'xml_crossref_articlecitations_pipe'

--- packtools/sps/formats/crossref.py
+++ packtools/sps/formats/crossref.py
@@ -1769,136 +1769,68 @@ def xml_crossref_close_pipe(xml_crossref):
 
 def xml_crossref_articlecitations_pipe(xml_crossref, xml_tree):
     """
-    IN (SciELO format) ->
-    <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
-    <back>
-    <ref-list>
-    <title>REFERENCES</title>
-    <ref id="B1">
-    <label>1.</label>
-    <mixed-citation>
-    1. Tran B, Falster MO, Douglas K, Blyth F, Jorm LR. Smoking and potentially preventable hospitalisation: the benefit of smoking cessation in older ages. Drug Alcohol Depend. 2015;150:85-91. DOI:
-    <ext-link ext-link-type="uri" xlink:href="https://doi.org/10.1016/j.drugalcdep.2015.02.028">https://doi.org/10.1016/j.drugalcdep.2015.02.028</ext-link>
-    </mixed-citation>
-    <element-citation publication-type="journal">
-    <person-group person-group-type="author">
-    <name>
-    <surname>Tran</surname>
-    <given-names>B</given-names>
-    </name>
-    <name>
-    <surname>Falster</surname>
-    <given-names>MO</given-names>
-    </name>
-    <name>
-    <surname>Douglas</surname>
-    <given-names>K</given-names>
-    </name>
-    <name>
-    <surname>Blyth</surname>
-    <given-names>F</given-names>
-    </name>
-    <name>
-    <surname>Jorm</surname>
-    <given-names>LR</given-names>
-    </name>
-    </person-group>
-    <article-title>Smoking and potentially preventable hospitalisation: the benefit of smoking cessation in older ages</article-title>
-    <source>Drug Alcohol Depend.</source>
-    <year>2015</year>
-    <volume>150</volume>
-    <fpage>85</fpage>
-    <lpage>91</lpage>
-    <comment>
-    DOI:
-    <ext-link ext-link-type="uri" xlink:href="https://doi.org/10.1016/j.drugalcdep.2015.02.028">https://doi.org/10.1016/j.drugalcdep.2015.02.028</ext-link>
-    </comment>
-    </element-citation>
-    </ref>
-    <ref id="B2">
-    <label>2.</label>
-    <mixed-citation>
-    2. Kwon JA, Jeon W, Park EC, Kim JH, Kim SJ, Yoo KB, et al. Effects of disease detection on changes in smoking behavior. Yonsei Med J. 2015;56(4): 1143-9. DOI:
-    <ext-link ext-link-type="uri" xlink:href="https://doi.org/10.3349/ymj.2015.56.4.1143">https://doi.org/10.3349/ymj.2015.56.4.1143</ext-link>
-    </mixed-citation>
-    <element-citation publication-type="journal">
-    <person-group person-group-type="author">
-    <name>
-    <surname>Kwon</surname>
-    <given-names>JA</given-names>
-    </name>
-    <name>
-    <surname>Jeon</surname>
-    <given-names>W</given-names>
-    </name>
-    <name>
-    <surname>Park</surname>
-    <given-names>EC</given-names>
-    </name>
-    <name>
-    <surname>Kim</surname>
-    <given-names>JH</given-names>
-    </name>
-    <name>
-    <surname>Kim</surname>
-    <given-names>SJ</given-names>
-    </name>
-    <name>
-    <surname>Yoo</surname>
-    <given-names>KB</given-names>
-    </name>
-    <etal/>
-    </person-group>
-    <article-title>Effects of disease detection on changes in smoking behavior</article-title>
-    <source>Yonsei Med J.</source>
-    <year>2015</year>
-    <volume>56</volume>
-    <issue>4</issue>
-    <fpage>1143</fpage>
-    <lpage>9</lpage>
-    <comment>
-    DOI:
-    <ext-link ext-link-type="uri" xlink:href="https://doi.org/10.3349/ymj.2015.56.4.1143">https://doi.org/10.3349/ymj.2015.56.4.1143</ext-link>
-    </comment>
-    </element-citation>
-    </ref>
-    </ref-list>
-    </back>
-    </article>
-    """
+    Adiciona o elemento 'citation_list' ao xml_crossref.
 
-    """
-    OUT (CrossRef format) ->
-    <doi_batch xmlns:ai="http://www.crossref.org/AccessIndicators.xsd" xmlns:jats="http://www.ncbi.nlm.nih.gov/JATS1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.crossref.org/schema/4.4.0" version="4.4.0" xsi:schemaLocation="http://www.crossref.org/schema/4.4.0 http://www.crossref.org/schemas/crossref4.4.0.xsd">
-    <body>
-    <journal>
-    <journal_article language="en" publication_type="full_text" reference_distribution_opts="any">
-    <citation_list>
-    <citation key="ref1">
-    <journal_title>Drug Alcohol Depend.</journal_title>
-    <author>Tran B</author>
-    <volume>150</volume>
-    <first_page>85</first_page>
-    <cYear>2015</cYear>
-    <article_title>Smoking and potentially preventable hospitalisation: the benefit of smoking cessation in older ages</article_title>
-    </citation>
-    </citation_list>
-    </journal_article>
-    
-    <journal_article language="pt" publication_type="full_text" reference_distribution_opts="any">
-    <citation_list>
-    <citation key="ref1">
-    <journal_title>Drug Alcohol Depend.</journal_title>
-    <author>Tran B</author>
-    <volume>150</volume>
-    <first_page>85</first_page>
-    <cYear>2015</cYear>
-    <article_title>Smoking and potentially preventable hospitalisation: the benefit of smoking cessation in older ages</article_title>
-    </citation>
-    </citation_list>
-    </journal_article>
-    </journal>
-    </body>
+    Parameters
+    ----------
+    xml_crossref : lxml.etree._Element
+        Elemento XML no padrão CrossRef em construção
+
+    xml_tree : lxml.etree._Element
+        Elemento XML no padrão SciELO com os dados de origem
+
+    Returns
+    -------
+    <?xml version="1.0" encoding="UTF-8"?>
+    <doi_batch ...>
+       <body>
+          <journal>
+             <journal_article language="pt" publication_type="research-article" reference_distribution_opts="any">
+                <citation_list>
+                   <citation key="ref">
+                      <journal_title>Circulation</journal_title>
+                      <author>Go AS</author>
+                      <volume>129</volume>
+                      <issue>3</issue>
+                      <first_page>e28</first_page>
+                      <cYear>2014</cYear>
+                      <article_title>Heart disease and stroke statistics-2014 update: a report from the American Heart Association</article_title>
+                   </citation>
+                   <citation key="ref">
+                      <journal_title>Arq Bras Cardiol</journal_title>
+                      <author>Albuquerque DC</author>
+                      <volume>104</volume>
+                      <issue>6</issue>
+                      <first_page>433</first_page>
+                      <cYear>2015</cYear>
+                      <article_title>I Brazilian Registry of Heart Failure - clinical aspects, care quality and hospitalization outcomes</article_title>
+                   </citation>
+                </citation_list>
+             </journal_article>
+             <journal_article language="en" publication_type="translation" reference_distribution_opts="any">
+                <citation_list>
+                   <citation key="ref">
+                      <journal_title>Circulation</journal_title>
+                      <author>Go AS</author>
+                      <volume>129</volume>
+                      <issue>3</issue>
+                      <first_page>e28</first_page>
+                      <cYear>2014</cYear>
+                      <article_title>Heart disease and stroke statistics-2014 update: a report from the American Heart Association</article_title>
+                   </citation>
+                   <citation key="ref">
+                      <journal_title>Arq Bras Cardiol</journal_title>
+                      <author>Albuquerque DC</author>
+                      <volume>104</volume>
+                      <issue>6</issue>
+                      <first_page>433</first_page>
+                      <cYear>2015</cYear>
+                      <article_title>I Brazilian Registry of Heart Failure - clinical aspects, care quality and hospitalization outcomes</article_title>
+                   </citation>
+                </citation_list>
+             </journal_article>
+          </journal>
+       </body>
     </doi_batch>
     """
     citations = article_citations.ArticleCitations(xml_tree).article_citations
