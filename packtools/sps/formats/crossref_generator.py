bb14022d29b3f1e5c664dd13f4208e73cc335a5b
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-07-28 15:35:19 -0300

    Adiciona mensagem em 'crossref_generator.py'

--- packtools/sps/formats/crossref_generator.py
+++ packtools/sps/formats/crossref_generator.py
@@ -33,6 +33,7 @@ def main():
 
     with open(arguments.path_to_write, "w", encoding="utf-8") as file:
         file.write(xml_crossref_formated)
+        print(f"Arquivo criado em: {arguments.path_to_write}")
 
 
 if __name__ == '__main__':
