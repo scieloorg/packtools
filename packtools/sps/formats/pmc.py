0f6e4013cf5114cad9b1bc0aec4f6b290fd1fff8
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-07-28 15:38:11 -0300

    Adiciona 'docstring' em 'xml_pmc_aff'

--- packtools/sps/formats/pmc.py
+++ packtools/sps/formats/pmc.py
@@ -10,6 +10,35 @@ def pipeline_pmc(xml_tree):
 
 
 def xml_pmc_aff(xml_tree):
+    """
+    Remove os elementos 'institution', 'addr-line' e 'country' do XML SciELO.
+    Adiciona o conteúdo de 'institution content-type="original"' no XML PMC.
+
+    Parameters
+    ----------
+    xml_tree : lxml.etree._Element
+        Elemento XML no padrão SciELO com os dados de origem, por exemplo:
+        <aff id="aff1">
+            <label>1</label>
+            <institution content-type="original">Universidade Federal do Rio Grande do Sul, Escola de Enfermagem, Programa de Pós-Graduação em Enfermagem, Porto Alegre, RS, Brazil.</institution>
+            <institution content-type="orgname">Universidade Federal do Rio Grande do Sul</institution>
+            <institution content-type="orgdiv1">Escola de Enfermagem</institution>
+            <institution content-type="orgdiv2">Programa de Pós-Graduação em Enfermagem</institution>
+            <addr-line>
+               <named-content content-type="city">Porto Alegre</named-content>
+               <named-content content-type="state">RS</named-content>
+            </addr-line>
+            <country country="BR">Brazil</country>
+         </aff>
+
+    Returns
+    -------
+    lxml.etree._Element :
+    <aff id="aff1">
+        <label>1</label>
+        Universidade Federal do Rio Grande do Sul, Escola de Enfermagem, Programa de Pós-Graduação em Enfermagem, Porto Alegre, RS, Brazil.
+    </aff>
+    """
     affs = xml_tree.findall(".//aff")
     for aff in affs:
         aff_institution = aff.find("./institution[@content-type='original']").text
