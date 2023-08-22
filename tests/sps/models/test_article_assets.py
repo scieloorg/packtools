0081118bde69ba4dd073fbe8eadd58c97a18d69e
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-08-22 08:28:44 -0300

    Aplica isort

--- tests/sps/models/test_article_assets.py
+++ tests/sps/models/test_article_assets.py
@@ -1,11 +1,8 @@
 from unittest import TestCase
 
+from packtools.sps.models.article_assets import (ArticleAssets,
+                                                 SupplementaryMaterials)
 from packtools.sps.utils import xml_utils
-
-from packtools.sps.models.article_assets import (
-    ArticleAssets,
-    SupplementaryMaterials,
-)
 
 
 def generate_xmltree(snippet):
