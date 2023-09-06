5897c0edfcfd1225d19d12d9519a2bb30ac35352
Luciano <41302084+Rossi-Luciano@users.noreply.github.com>
2023-09-06 08:54:01 -0300

    Completa classe funding (#462)
    * Adiciona 'financial_disclosure' e teste
    
    * Adiciona 'test_funding_sources'
    
    * Adiciona 'award_groups' e teste
    
    * Corrige 'financial_disclosure'
    
    * Adapta os testes.
    
    * Adiciona 'funding_statement'
    
    * Adiciona 'test_funding_statement'
    
    * Corrige 'financial_disclosure'
    
    * Corrige 'test_financial_disclosure'

--- packtools/sps/models/funding_group.py
+++ packtools/sps/models/funding_group.py
@@ -1,5 +1,5 @@
 import logging
-
+from lxml import etree
 
 logger = logging.getLogger(__name__)
 
@@ -25,14 +25,19 @@ class FundingGroup:
         self._xmltree = xmltree
 
     @property
+    def financial_disclosure(self):
+        # TODO separar os valores entre 'funding-source' e 'award-id'
+        for node in self._xmltree.xpath(".//fn-group/fn[@fn-type='financial-disclosure']"):
+            yield " ".join([item.strip() for item in node.xpath(".//text()") if item.strip()])
+
+    @property
     def award_groups(self):
         items = []
         for node in self._xmltree.xpath(".//funding-group/award-group"):
-            funding_sources = node.xpath("funding-source")
-            award_ids = node.xpath("award-id")
-            d = {}
-            d["funding-source"] = [item.text for item in funding_sources]
-            d["award-id"] = [item.text for item in award_ids]
+            d = {
+                "funding-source": [source.text for source in node.xpath("funding-source")],
+                "award-id": [id.text for id in node.xpath("award-id")]
+            }
             items.append(d)
         return items
 
@@ -42,3 +47,7 @@ class FundingGroup:
         for node in self._xmltree.xpath(".//funding-group/award-group/funding-source"):
             items.append(node.text)
         return items
+
+    @property
+    def funding_statement(self):
+        return self._xmltree.xpath(".//funding-group/funding-statement")[0].text
