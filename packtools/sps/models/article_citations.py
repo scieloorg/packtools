ca0de85366c6228a08c7499430b0fa15aaa6578a
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-10-10 11:59:10 -0300

    Corrige 'article_citations'

--- packtools/sps/models/article_citations.py
+++ packtools/sps/models/article_citations.py
@@ -91,9 +91,9 @@ class ArticleCitations:
 
     @property
     def article_citations(self):
-        _citations = []
         for node in self.xmltree.xpath("./back/ref-list//ref"):
             tags = [
+                ('ref_id', get_ref_id(node)),
                 ('label', get_label(node)),
                 ('source', get_source(node)),
                 ('main_author', get_main_author(node)),
@@ -102,10 +102,11 @@ class ArticleCitations:
                 ('issue', get_issue(node)),
                 ('fpage', get_fpage(node)),
                 ('lpage', get_lpage(node)),
+                ('elocation_id', get_elocation_id(node)),
                 ('year', get_year(node)),
                 ('article_title', get_article_title(node)),
                 ('mixed_citation', ET.tostring(get_mixed_citation(node), encoding=str, method='text').strip()),
-                ('citation_ids', get_citation_ids(node)),
+                ('citation_ids', get_citation_ids(node))
             ]
 
             d = dict()
@@ -115,5 +116,4 @@ class ArticleCitations:
                         d[name] = value.text
                     except AttributeError:
                         d[name] = value
-            _citations.append(d)
-        return _citations
+            yield d
