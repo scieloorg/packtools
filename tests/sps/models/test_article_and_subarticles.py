f7ee0fde9c69c52fc9aadef1aa952eac2fcf3aea
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-07 14:16:14 -0300

    Atualiza 'test_elements_order'

--- tests/sps/models/test_article_and_subarticles.py
+++ tests/sps/models/test_article_and_subarticles.py
@@ -43,6 +43,7 @@ class ArticleAndSubarticlesTest(TestCase):
         self.assertListEqual(expected, obtained)
 
     def test_elements_order(self):
+        self.maxDiff = None
         with open('tests/samples/artigo-com-traducao-e-pareceres-traduzidos.xml', 'r') as data:
             xmltree = xml_utils.get_xml_tree(data.read())
 
@@ -51,39 +52,46 @@ class ArticleAndSubarticlesTest(TestCase):
                 'article_id': 'main',
                 'article_type': 'research-article',
                 'lang': 'pt',
-                'line_number': 2
+                'line_number': 2,
+                'subject': 'ARTIGOS'
             },
             {
                 'article_id': 's2',
                 'article_type': 'reviewer-report',
                 'lang': 'pt',
-                'line_number': 93
+                'line_number': 93,
+                'subject': 'Pareceres'
             },
             {
                 'article_id': 's3',
                 'article_type': 'reviewer-report',
                 'lang': 'pt',
-                'line_number': 141
+                'line_number': 141,
+                'subject': 'Pareceres'
             },
             {
                 'article_id': 's1',
                 'article_type': 'translation',
                 'lang': 'en',
-                'line_number': 189
+                'line_number': 189,
+                'subject': 'ARTICLES'
             },
             {
                 'article_id': 's5',
                 'article_type': 'reviewer-report',
                 'lang': 'en',
-                'line_number': 233
+                'line_number': 233,
+                'subject': 'Reviews'
             },
             {
                 'article_id': 's6',
                 'article_type': 'reviewer-report',
                 'lang': 'en',
-                'line_number': 271
+                'line_number': 271,
+                'subject': 'Reviews'
             }
         ]
         obtained = [d for d in ArticleAndSubArticles(xmltree).data]
 
         self.assertListEqual(expected, obtained)
+
