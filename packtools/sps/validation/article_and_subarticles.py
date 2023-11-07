a1727bab59f70093fb9bfb1e589e6c10b72988ba
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-07 14:15:17 -0300

    Adiciona 'validate_dtd_version'

--- packtools/sps/validation/article_and_subarticles.py
+++ packtools/sps/validation/article_and_subarticles.py
@@ -103,6 +103,47 @@ class ArticleValidation:
             .format(self.articles.main_article_type, article_specific_use, " | ".join(specific_use_list))
         }
 
+    def validate_dtd_version(self, dtd_version_list=None):
+        """
+        Params
+        ------
+            xml: ElementTree
+
+        Returns
+        -------
+        list: dicts as:
+        {
+            'title': 'Article element dtd-version attribute validation',
+            'xpath': './article/dtd-version',
+            'validation_type': 'value in list',
+            'response': 'OK',
+            'expected_value': ['1.1', '1.2', '1.3'],
+            'got_value': '1.1',
+            'message': 'Got 1.1 expected one item of this list: 1.1 | 1.2 | 1.3',
+            'advice': 'XML research-article has 1.1 as dtd-version expected one item of this list: 1.1 | 1.2 | 1.3'
+        }
+        """
+
+        dtd_version_list = dtd_version_list or self.dtd_version_list
+        if not dtd_version_list:
+            raise ArticleValidationValidateDtdVersionException("Function requires list of dtd versions")
+
+        article_dtd_version = self.articles.main_dtd_version
+        validated = article_dtd_version in dtd_version_list
+
+        return {
+            'title': 'Article element dtd-version attribute validation',
+            'xpath': './article/dtd-version',
+            'validation_type': 'value in list',
+            'response': 'OK' if validated else 'ERROR',
+            'expected_value': dtd_version_list,
+            'got_value': article_dtd_version,
+            'message': 'Got {} expected one item of this list: {}'.format(article_dtd_version, " | "
+                                                                          .join(dtd_version_list)),
+            'advice': None if validated else 'XML {} has {} as dtd-version, expected one item of this list: {}'
+            .format(self.articles.main_article_type, article_dtd_version, " | ".join(dtd_version_list))
+        }
+
     def validate(self, data):
         """
         Função que executa as validações da classe ArticleLangValidation.
