00f4a1b34193bd14ffcd4dad37ced07b85b7c1da
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-15 22:37:01 -0300

    Adiciona 'most_similar'

--- packtools/sps/validation/similarity_utils.py
+++ packtools/sps/validation/similarity_utils.py
@@ -9,3 +9,16 @@ def how_similar(this, that):
     return difflib.SequenceMatcher(None, this.lower(), that.lower()).ratio()
 
 
+def most_similar(similarity):
+    items = []
+    highiest_rate = 0
+    ratio_list = similarity.keys()
+    if len(ratio_list) > 0:
+        ratio_list = sorted(ratio_list)
+        ratio_list.reverse()
+        highiest_rate = ratio_list[0]
+        items = similarity[highiest_rate]
+
+    return highiest_rate, items
+
+
