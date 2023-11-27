c0f9c3c87665ed3f42ed5dc1300d7391d6d30767
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-27 13:26:48 -0300

    Adiciona 'test_validate_article_has_no_doi'

--- tests/sps/validation/test_article_doi.py
+++ tests/sps/validation/test_article_doi.py
@@ -33,3 +33,29 @@ class ArticleDoiTest(unittest.TestCase):
         }
         self.assertDictEqual(obtained, expected)
 
+    def test_validate_article_has_no_doi(self):
+        self.maxDiff = None
+        xml_str = """
+            <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
+            article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
+            <front>
+            <article-id pub-id-type="publisher-id" specific-use="scielo-v3">TPg77CCrGj4wcbLCh9vG8bS</article-id>
+            <article-id pub-id-type="publisher-id" specific-use="scielo-v2">S0104-11692020000100303</article-id>
+            <article-id pub-id-type="other">00303</article-id>
+            </front>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = ArticleDoiValidation(xml_tree).validate_main_article_doi_exists()
+        expected = {
+            'title': 'Article DOI element',
+            'xpath': './article-id[@pub-id-type="doi"]',
+            'validation_type': 'exist',
+            'response': 'ERROR',
+            'expected_value': 'article DOI',
+            'got_value': None,
+            'message': 'Got None expected a DOI',
+            'advice': 'XML research-article does not present a DOI'
+        }
+        self.assertDictEqual(obtained, expected)
+
