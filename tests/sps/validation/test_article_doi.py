3e5b04ec024b49700ed0c045bcc84eb329a38370
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-27 13:28:06 -0300

    Adiciona 'test_validate_all_dois_are_unique'

--- tests/sps/validation/test_article_doi.py
+++ tests/sps/validation/test_article_doi.py
@@ -168,3 +168,38 @@ class ArticleDoiTest(unittest.TestCase):
         }
         self.assertDictEqual(obtained, expected)
 
+    def test_validate_all_dois_are_unique(self):
+        self.maxDiff = None
+        xml_str = """
+            <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
+            article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="pt">
+            <front>
+                <article-id specific-use="previous-pid" pub-id-type="publisher-id">S2176-45732023005002205</article-id>
+                <article-id specific-use="scielo-v3" pub-id-type="publisher-id">PqQCH4JjQTWmwYF97s4YGKv</article-id>
+                <article-id specific-use="scielo-v2" pub-id-type="publisher-id">S2176-45732023000200226</article-id>
+                <article-id pub-id-type="doi">10.1590/2176-4573p59270</article-id>
+            </front>
+            <sub-article article-type="reviewer-report" id="s2" xml:lang="pt" />
+            <sub-article article-type="reviewer-report" id="s3" xml:lang="pt" />
+            <sub-article article-type="translation" id="s1" xml:lang="en">
+                <front-stub>
+                    <article-id pub-id-type="doi">10.1590/2176-4573e59270</article-id>
+                </front-stub>
+            </sub-article>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = ArticleDoiValidation(xml_tree).validate_all_dois_are_unique()
+
+        expected = {
+            'title': 'Article DOI element is unique',
+            'xpath': './article-id[@pub-id-type="doi"]',
+            'validation_type': 'exist/verification',
+            'response': 'OK',
+            'expected_value': 'Unique DOI values',
+            'got_value': 'DOIs identified: 10.1590/2176-4573p59270 | 10.1590/2176-4573e59270',
+            'message': "Got DOIs and frequencies ('10.1590/2176-4573p59270', 1) | ('10.1590/2176-4573e59270', 1)",
+            'advice': None
+        }
+        self.assertDictEqual(obtained, expected)
+
