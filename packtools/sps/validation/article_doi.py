9928bb7e7a9dd0286754fdced83836b214b13522
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-11-27 13:25:34 -0300

    Adiciona 'validate_translations_doi_exists'

--- packtools/sps/validation/article_doi.py
+++ packtools/sps/validation/article_doi.py
@@ -53,3 +53,61 @@ class ArticleDoiValidation:
                 self.articles.main_article_type)
         }
 
+    def validate_translations_doi_exists(self):
+        """
+        Checks for the existence of DOI.
+
+        XML input
+        ---------
+        <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
+        article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="pt">
+        <front>
+            <article-id specific-use="previous-pid" pub-id-type="publisher-id">S2176-45732023005002205</article-id>
+            <article-id specific-use="scielo-v3" pub-id-type="publisher-id">PqQCH4JjQTWmwYF97s4YGKv</article-id>
+            <article-id specific-use="scielo-v2" pub-id-type="publisher-id">S2176-45732023000200226</article-id>
+            <article-id pub-id-type="doi">10.1590/2176-4573p59270</article-id>
+        </front>
+        <sub-article article-type="reviewer-report" id="s2" xml:lang="pt" />
+        <sub-article article-type="reviewer-report" id="s3" xml:lang="pt" />
+        <sub-article article-type="translation" id="s1" xml:lang="en">
+            <front-stub>
+                <article-id pub-id-type="doi">10.1590/2176-4573e59270</article-id>
+            </front-stub>
+        </sub-article>
+        </article>
+
+        Returns
+        -------
+        dict
+            Such as:
+            {
+                'title': 'Sub-article translation DOI element',
+                'xpath': './sub-article[@article-type="translation"]',
+                'validation_type': 'exist',
+                'response': 'OK',
+                'expected_value': 'sub-article translation DOI for languages en',
+                'got_value': 'Languages with identified DOI: en',
+                'message': "Got ['en'] expected ['en']",
+                'advice': None
+            }
+        """
+
+        translations_lang = [item['lang'] for item in self.articles.data if item['article_type'] == 'translation']
+        translations_lang_with_doi = [item['lang'] for item in self.dois if item['lang'] in translations_lang]
+
+        validated = len(translations_lang) == len(translations_lang_with_doi)
+
+        if not validated:
+            diff = list(set(translations_lang) - set(translations_lang_with_doi))
+
+        return {
+            'title': 'Sub-article translation DOI element',
+            'xpath': './sub-article[@article-type="translation"]',
+            'validation_type': 'exist',
+            'response': 'OK' if validated else 'ERROR',
+            'expected_value': 'sub-article translation DOI for languages {}'.format(" | ".join(translations_lang)),
+            'got_value': 'Languages with identified DOI: {}'.format(" | ".join(translations_lang_with_doi)),
+            'message': 'Got {} expected {}'.format(translations_lang_with_doi, translations_lang),
+            'advice': None if validated else 'The translation sub-article for {} languages does not present a DOI'.format(diff)
+        }
+
