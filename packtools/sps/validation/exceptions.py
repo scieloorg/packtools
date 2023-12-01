7292fa9c5519bd85daae96d2b5f07486cbb178f3
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-12-01 09:34:45 -0300

    Remove exceção não utilizada

--- packtools/sps/validation/exceptions.py
+++ packtools/sps/validation/exceptions.py
@@ -37,7 +37,3 @@ class ValidationArticleAndSubArticlesArticleTypeException(Exception):
 
 class ValidationArticleAndSubArticlesSubjectsException(Exception):
     ...
-
-
-class ValidationDateException(Exception):
-    ...
