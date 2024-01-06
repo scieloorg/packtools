6ad02fe43805dc033e91201423c18f3ab09e8567
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-01-06 18:56:14 -0300

    Torna 'publisher_name_list' obrigatório

--- packtools/sps/validation/journal_meta.py
+++ packtools/sps/validation/journal_meta.py
@@ -72,7 +72,7 @@ class PublisherNameValidation:
         self.publisher = Publisher(self.xmltree)
         self.publisher_name_list = publisher_name_list
 
-    def validate_publishers_names(self, publisher_name_list=None):
+    def validate_publishers_names(self, publisher_name_list):
         """
         Checks whether the publisher name is as expected.
 
