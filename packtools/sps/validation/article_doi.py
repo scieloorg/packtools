e96daace2065651b24f84b0c16f44165ae419cfa
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-01-05 23:42:17 -0300

    Atualiza 'docstring'

--- packtools/sps/validation/article_doi.py
+++ packtools/sps/validation/article_doi.py
@@ -202,19 +202,81 @@ class ArticleDoiValidation:
         <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
             article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
             <front>
-            <article-id pub-id-type="publisher-id" specific-use="scielo-v3">TPg77CCrGj4wcbLCh9vG8bS</article-id>
-            <article-id pub-id-type="publisher-id" specific-use="scielo-v2">S0104-11692020000100303</article-id>
-            <article-id pub-id-type="doi">10.1590/1518-8345.2927.3231</article-id>
-            <article-id pub-id-type="other">00303</article-id>
+                <article-meta>
+                <article-id specific-use="previous-pid" pub-id-type="publisher-id">S2176-45732023005002205</article-id>
+                <article-id specific-use="scielo-v3" pub-id-type="publisher-id">PqQCH4JjQTWmwYF97s4YGKv</article-id>
+                <article-id specific-use="scielo-v2" pub-id-type="publisher-id">S2176-45732023000200226</article-id>
+                <article-id pub-id-type="doi">10.1590/2176-4573p59270</article-id>
+                <title-group>
+                    <article-title>Analysis of the evolution of competences in the clinical practice of the nursing degree</article-title>
+                </title-group>
+                <contrib-group>
+                    <contrib contrib-type="author">
+                      <contrib-id contrib-id-type="orcid">0000-0002-5364-5270</contrib-id>
+                      <name>
+                        <surname>Martínez-Momblán</surname>
+                        <given-names>Maria Antonia</given-names>
+                      </name>
+                      <xref ref-type="aff" rid="aff1">1</xref>
+                    </contrib>
+                    <contrib contrib-type="author">
+                      <contrib-id contrib-id-type="orcid">0000-0002-6406-0120</contrib-id>
+                      <name>
+                        <surname>Colina-Torralva</surname>
+                        <given-names>Javier</given-names>
+                      </name>
+                      <xref ref-type="aff" rid="aff1">1</xref>
+                    </contrib>
+                </contrib-group>
+                </article-meta>
             </front>
-        </article>
+            <sub-article article-type="reviewer-report" id="s2" xml:lang="pt" />
+            <sub-article article-type="reviewer-report" id="s3" xml:lang="pt" />
+            <sub-article article-type="translation" id="s1" xml:lang="pt">
+                <front-stub>
+                    <article-id pub-id-type="doi">10.1590/2176-4573e59270</article-id>
+                        <title-group>
+                            <article-title>Análise da evolução de competências da prática clínica no curso de enfermagem</article-title>
+                        </title-group>
+                        <contrib-group>
+                            <contrib contrib-type="author">
+                            <contrib-id contrib-id-type="orcid">0000-0002-5364-5270</contrib-id>
+                                <name>
+                                <surname>Martínez-Momblán</surname>
+                                <given-names>Maria Antonia</given-names>
+                                </name>
+                            <xref ref-type="aff" rid="aff2">1</xref>
+                            </contrib>
+                            <contrib contrib-type="author">
+                            <contrib-id contrib-id-type="orcid">0000-0002-6406-0120</contrib-id>
+                                <name>
+                                <surname>Colina-Torralva</surname>
+                                <given-names>Javier</given-names>
+                                </name>
+                            <xref ref-type="aff" rid="aff2">1</xref>
+                            </contrib>
+                        </contrib-group>
+                </front-stub>
+            </sub-article>
+            </article>
 
         Params
 
         ------
         callable_get_validation : function
             A function that will be passed as an argument.
-            This function must have the signature 'def callable_get_validate(doi_name):' and returns True if the doi_name is valid or False otherwise
+            This function must have the signature 'def callable_get_validate(doi_name):' and returns a dict, such as:
+            {
+                'en': {
+                        'title': 'Analysis of the evolution of competences in the clinical practice of the nursing degree',
+                        'doi': '10.1590/2176-4573p59270'
+                    },
+                'pt': {
+                        'title': 'Análise da evolução de competências da prática clínica no curso de enfermagem',
+                        'doi': '10.1590/2176-4573e59270'
+                    },
+                'authors': ['Martínez-Momblán, Maria Antonia', 'Colina-Torralva, Javier']
+            }
 
         Returns
         -------
@@ -226,11 +288,27 @@ class ArticleDoiValidation:
                     'xpath': './article-id[@pub-id-type="doi"]',
                     'validation_type': 'exist',
                     'response': 'OK',
-                    'expected_value': 10.1590/1518-8345.2927.3231,
-                    'got_value': 10.1590/1518-8345.2927.3231,
-                    'message': 'Got 10.1590/1518-8345.2927.3231 expected 10.1590/1518-8345.2927.3231',
+                    'expected_value': [
+                        'en',
+                        '10.1590/2176-4573p59270',
+                        'Analysis of the evolution of competences in the clinical practice of the nursing degree',
+                        ['Martínez-Momblán, Maria Antonia', 'Colina-Torralva, Javier']
+                    ],
+                    'got_value': [
+                        'en',
+                        '10.1590/2176-4573p59270',
+                        'Analysis of the evolution of competences in the clinical practice of the nursing degree',
+                        ['Martínez-Momblán, Maria Antonia', 'Colina-Torralva, Javier']
+                    ],
+                    'message': "Got "
+                               "['en', '10.1590/2176-4573p59270', "
+                               "'Analysis of the evolution of competences in the clinical practice of the nursing degree', "
+                               "['Martínez-Momblán, Maria Antonia', 'Colina-Torralva, Javier']] expected "
+                               "['en', '10.1590/2176-4573p59270', "
+                               "'Analysis of the evolution of competences in the clinical practice of the nursing degree', "
+                               "['Martínez-Momblán, Maria Antonia', 'Colina-Torralva, Javier']]",
                     'advice': None
-                }
+                },...
             ]
         """
         callable_get_validate = callable_get_validate or _callable_extern_validate_default
