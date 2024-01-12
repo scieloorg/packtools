28733ba611bcf258c184e8893f641c8e392e3950
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-01-12 16:24:53 -0300

    Adiciona teste para quando o valor de 'issue' não corresponde a nenhum padrão esperado

--- tests/sps/validation/test_front_articlemeta_issue.py
+++ tests/sps/validation/test_front_articlemeta_issue.py
@@ -108,6 +108,38 @@ class IssueTest(TestCase):
             with self.subTest(i):
                 self.assertDictEqual(expected[i], item)
 
+    def test_validate_article_issue_out_of_pattern_value(self):
+        self.maxDiff = None
+        xml_str = """
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en">
+            <front>
+                <article-meta>
+                    <issue>vol 4</issue>
+                    <supplement>2</supplement>
+                </article-meta>
+            </front>
+        </article>
+        """
+        xml_tree = etree.fromstring(xml_str)
+        obtained = IssueValidation(xml_tree).validate_article_issue()
+
+        expected = [
+            {
+                'title': 'Article-meta issue element validation',
+                'xpath': './/front/article-meta/issue',
+                'validation_type': 'format',
+                'response': 'ERROR',
+                'expected_value': 'a valid value to article issue',
+                'got_value': 'vol 4',
+                'message': 'Got vol 4 expected a valid value to article issue',
+                'advice': 'Provide values according to the following examples: 4, spe1, 4 suppl 1 or suppl 1',
+
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
 
     def test_suppl_matches(self):
         xml = (
