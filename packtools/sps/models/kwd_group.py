95ab4daaf7ddbb3e56dbfcf7d66cbc364cd7a3b6
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-01-19 14:26:00 -0300

    Corrige função para considerar 'kwd-group' sem 'lang'

--- packtools/sps/models/kwd_group.py
+++ packtools/sps/models/kwd_group.py
@@ -11,17 +11,23 @@ def get_node_without_subtag(node):
 class KwdGroup:
     def __init__(self, xmltree):
         self._xmltree = xmltree
-    
+
     def extract_kwd_data_with_lang_text(self, subtag):
-        
         _data = []
         kwd_text = xml_utils.node_text_without_xref if subtag else get_node_without_subtag
 
-        for kwd_group in self._xmltree.xpath('.//kwd-group'):
-            _lang = kwd_group.get("{http://www.w3.org/XML/1998/namespace}lang")
-            for kwd in kwd_group.xpath("kwd"):
-                kwd = kwd_text(kwd)
-                _data.append({"lang": _lang, "text": kwd})
+        nodes = self._xmltree.xpath('.//article-meta | .//sub-article')
+
+        for node in nodes:
+            node_lang = node.get("{http://www.w3.org/XML/1998/namespace}lang")
+
+            for kwd_group in node.xpath('.//kwd-group'):
+                kwd_group_lang = kwd_group.get("{http://www.w3.org/XML/1998/namespace}lang", node_lang)
+
+                for kwd in kwd_group.xpath("kwd"):
+                    keyword_text = kwd_text(kwd)
+                    _data.append({"lang": kwd_group_lang, "text": keyword_text})
+
         return _data
 
     def extract_kwd_extract_data_by_lang(self, subtag):
