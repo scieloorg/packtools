759f75114defb1d60d42fe18fa3973b8753d3edb
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-01-29 22:55:05 -0300

    Adiciona 'funding_sources_validation'

--- packtools/sps/validation/funding_group.py
+++ packtools/sps/validation/funding_group.py
@@ -10,3 +10,32 @@ def equalize_list_sizes(list1, list2):
     return list1, list2
 
 
+class FundingGroupValidation:
+    def __init__(self, xml_tree):
+        self.xml_tree = xml_tree
+        self.funding_sources = FundingGroup(xml_tree).award_groups
+        self.principal_award_recipients = FundingGroup(xml_tree).principal_award_recipients
+        self.principal_investigator = FundingGroup(xml_tree).principal_investigators
+
+    def funding_sources_validation(self):
+        for funding in self.funding_sources or [None]:
+            if funding:
+                is_valid = funding.get("funding-source") != [] and funding.get("award-id") != []
+                fundings, awards = equalize_list_sizes(funding.get("funding-source"), funding.get("award-id"))
+                obtained = ' | '.join([f'{fund} ({award})' for fund, award in zip(fundings, awards)])
+            else:
+                is_valid = False
+                obtained = None
+
+            yield {
+                'title': 'Funding source element validation',
+                'xpath': './/funding-group/award-group/funding-source',
+                'validation_type': 'exist',
+                'response': 'OK' if is_valid else 'ERROR',
+                'expected_value': obtained if is_valid else 'values to <funding-source> and <award-id>',
+                'got_value': obtained,
+                'message': 'Got {} expected {}'.format(
+                    obtained, obtained if is_valid else 'values to <funding-source> and <award-id>'),
+                'advice': None if is_valid else 'Provide values to <funding-source> and <award-id>'
+            }
+
