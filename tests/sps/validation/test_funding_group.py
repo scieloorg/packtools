8ed387f255ab64e7a0cdac417e99753acf1c7947
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-02-02 13:12:01 -0300

    Adiciona testes

--- tests/sps/validation/test_funding_group.py
+++ tests/sps/validation/test_funding_group.py
@@ -528,6 +528,182 @@ class FundingGroupValidationTest(unittest.TestCase):
             with self.subTest(i):
                 self.assertDictEqual(expected[i], item)
 
+    def test_ack_exist_validation_success_article_ack(self):
+        self.maxDiff = None
+        xml_str = """
+            <article>
+                <back>
+                    <ack>
+                        <title>Acknowledgments</title>
+                        <p>Paragraph 1.</p>
+                        <p>Paragraph 2.</p>
+                        <p>Paragraph 3.</p>
+                    </ack>
+                </back>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = FundingGroupValidation(xml_tree).ack_exist_validation()
+        expected = [
+            {
+                'title': 'Acknowledgment element validation',
+                'xpath': './/back//ack',
+                'validation_type': 'exist',
+                'response': 'OK',
+                'expected_value': {
+                    "title": 'Acknowledgments',
+                    "text": 'Paragraph 1. Paragraph 2. Paragraph 3.'
+                },
+                'got_value': {
+                    "title": 'Acknowledgments',
+                    "text": 'Paragraph 1. Paragraph 2. Paragraph 3.'
+                },
+                'message': "Got {'title': 'Acknowledgments', 'text': 'Paragraph 1. Paragraph 2. Paragraph 3.'} "
+                           "expected {'title': 'Acknowledgments', 'text': 'Paragraph 1. Paragraph 2. Paragraph 3.'}",
+                'advice': None
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
+    def test_ack_exist_validation_success_sub_article_ack(self):
+        self.maxDiff = None
+        xml_str = """
+            <article>
+                <sub-article>
+                    <back>
+                        <ack>
+                            <title>Acknowledgments</title>
+                            <p>Paragraph 1.</p>
+                            <p>Paragraph 2.</p>
+                            <p>Paragraph 3.</p>
+                        </ack>
+                    </back>
+                </sub-article>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = FundingGroupValidation(xml_tree).ack_exist_validation()
+        expected = [
+            {
+                'title': 'Acknowledgment element validation',
+                'xpath': './/back//ack',
+                'validation_type': 'exist',
+                'response': 'OK',
+                'expected_value': {
+                    "title": 'Acknowledgments',
+                    "text": 'Paragraph 1. Paragraph 2. Paragraph 3.'
+                },
+                'got_value': {
+                    "title": 'Acknowledgments',
+                    "text": 'Paragraph 1. Paragraph 2. Paragraph 3.'
+                },
+                'message': "Got {'title': 'Acknowledgments', 'text': 'Paragraph 1. Paragraph 2. Paragraph 3.'} "
+                           "expected {'title': 'Acknowledgments', 'text': 'Paragraph 1. Paragraph 2. Paragraph 3.'}",
+                'advice': None
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
+    def test_ack_exist_validation_fail_article_ack(self):
+        self.maxDiff = None
+        xml_str = """
+            <article>
+                <back>
+                    
+                </back>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = FundingGroupValidation(xml_tree).ack_exist_validation()
+        expected = [
+            {
+                'title': 'Acknowledgment element validation',
+                'xpath': './/back//ack',
+                'validation_type': 'exist',
+                'response': 'ERROR',
+                'expected_value': 'value to <ack>',
+                'got_value': None,
+                'message': 'Got None expected value to <ack>',
+                'advice': 'Provide value to <ack>'
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
+    def test_funding_statement_exist_validation_success(self):
+        self.maxDiff = None
+        xml_str = """
+            <article>
+                <front>
+                    <article-meta>
+                        <funding-group>
+                            <award-group>
+                                <funding-source>Natural Science Foundation of Hunan Province</funding-source>
+                                <award-id>2019JJ40269</award-id>
+                            </award-group>
+                            <funding-statement>declaração de financiamento</funding-statement>
+                        </funding-group>
+                    </article-meta>
+                </front>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = FundingGroupValidation(xml_tree).funding_statement_exist_validation()
+        expected = [
+            {
+                'title': 'Funding statement element validation',
+                'xpath': './/funding-group/funding-statement',
+                'validation_type': 'exist',
+                'response': 'OK',
+                'expected_value': 'declaração de financiamento',
+                'got_value': 'declaração de financiamento',
+                'message': 'Got declaração de financiamento expected declaração de financiamento',
+                'advice': None
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
+    def test_funding_statement_exist_validation_fail(self):
+        self.maxDiff = None
+        xml_str = """
+            <article>
+                <front>
+                    <article-meta>
+                        <funding-group>
+                            <award-group>
+                                <funding-source>Natural Science Foundation of Hunan Province</funding-source>
+                                <award-id>2019JJ40269</award-id>
+                            </award-group>
+                        </funding-group>
+                    </article-meta>
+                </front>
+            </article>
+            """
+        xml_tree = get_xml_tree(xml_str)
+        obtained = FundingGroupValidation(xml_tree).funding_statement_exist_validation()
+        expected = [
+            {
+                'title': 'Funding statement element validation',
+                'xpath': './/funding-group/funding-statement',
+                'validation_type': 'exist',
+                'response': 'ERROR',
+                'expected_value': 'value to <funding-statement>',
+                'got_value': None,
+                'message': 'Got None expected value to <funding-statement>',
+                'advice': 'Provide value to <funding-statement>',
+            }
+        ]
+        for i, item in enumerate(obtained):
+            with self.subTest(i):
+                self.assertDictEqual(expected[i], item)
+
 
 if __name__ == '__main__':
     unittest.main()
