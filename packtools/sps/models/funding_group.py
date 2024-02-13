85cdf6d68fdb05af789846793e96649351d6143c
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-02-12 21:50:11 -0300

    Adiciona '_strip_and_remove_final_dot'

--- packtools/sps/models/funding_group.py
+++ packtools/sps/models/funding_group.py
@@ -9,6 +9,13 @@ def _is_funding_source(text):
     return all(char.isalpha() or char.isspace() for char in text)
 
 
+def _strip_and_remove_final_dot(text):
+    text = text.strip()
+    if text.endswith('.'):
+        text = text[:-1]
+    return text
+
+
 class FundingGroup:
     """
     Class that performs the extraction of values for funding-source and award-id.
