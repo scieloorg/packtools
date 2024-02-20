db8cd5eabfe877a39b69ffa3703299bc1943c0bb
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-02-20 12:22:51 -0300

    Diferencia dados que não foram marcados no XML como 'funding-source' ou 'award-id'

--- packtools/sps/validation/funding_group.py
+++ packtools/sps/validation/funding_group.py
@@ -29,36 +29,45 @@ class FundingGroupValidation:
         title = 'Funding source element validation'
         validation_type = 'exist'
         for funding in self.funding_group + self.funding_fn:
+            fn_type = funding.get('fn-type')
             is_valid = False
             advice = None
-            funding_list = funding.get("funding-source")
-            award_list = funding.get("award-id")
+            funding_list = funding.get("funding-source") or funding.get("look-like-funding-source") or []
+            award_list = funding.get("award-id") or funding.get("look-like-award-id") or []
             has_funding = len(funding_list) > 0
             has_award = len(award_list) > 0
-            obtained = '{} values to funding source and {} values to award id'.format(len(funding_list), len(award_list))
-            message = 'Got {} as funding source and {} as award id'.format(funding_list, award_list)
+            obtained = '{} values {} and {} values {}'.format(
+                len(funding_list),
+                'that look like funding source' if fn_type else 'for funding source',
+                len(award_list),
+                'that look like award id' if fn_type else 'for award id'
+            )
+            message = 'Got {} {} and {} {}'.format(
+                funding_list,
+                'that look like funding source' if fn_type else 'as funding source',
+                award_list,
+                'that look like award id' if fn_type else 'as award id'
+            )
 
-            fn_type = funding.get('fn-type')
             xpath = f".//fn-group/fn[@fn-type='{fn_type}']//p" if fn_type else './/funding-group/award-group/funding-source'
             if fn_type == 'supported-by':
-                expected = 'at least 1 value to funding source'
+                expected = 'at least 1 value for funding source'
                 if not has_funding:
-                    advice = 'Provide value to funding source'
+                    advice = 'Provide value for funding source'
                 else:
                     is_valid = True
             else:
-                expected = 'at least 1 value to funding source and at least 1 value to award id'
+                expected = 'at least 1 value for funding source and at least 1 value for award id'
                 if not has_award and not has_funding:
-                    advice = 'Provide values to award id and funding source'
+                    advice = 'Provide values for award id and funding source'
                 elif not has_award and has_funding:
-                    advice = 'Provide value to award id or move funding source to <fn fn-type="supported-by">'
+                    advice = 'Provide value for award id or move funding source to <fn fn-type="supported-by">'
                 elif has_award and not has_funding:
-                    advice = 'Provide value to funding source'
+                    advice = 'Provide value for funding source'
                 else:
                     is_valid = True
 
             yield _create_response(title, xpath, validation_type, is_valid, expected, obtained, message, advice)
-
 
     def award_id_format_validation(self, callable_validation=None):
         callable_validation = callable_validation or _callable_extern_validate_default
