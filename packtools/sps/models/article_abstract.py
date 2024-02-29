c8f05750cee2668b0acee2b14f7e41ccf51a3fac
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-02-29 12:25:16 -0300

    Adapta classe 'Abstract' para retornar os valores em formato HTML

--- packtools/sps/models/article_abstract.py
+++ packtools/sps/models/article_abstract.py
@@ -72,7 +72,7 @@ article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lan
 </sub-article>
 </article>
 """
-from packtools.sps.utils.xml_utils import node_text, get_node_without_subtag
+from packtools.sps.utils.xml_utils import node_text, get_node_without_subtag, convert_xml_to_html
 
 
 class Abstract:
@@ -80,7 +80,7 @@ class Abstract:
     def __init__(self, xmltree):
         self.xmltree = xmltree
 
-    def _get_structured_abstract(self, abstract_node):
+    def _get_structured_abstract(self, abstract_node, html=False, allowed_tags=None, xml_to_html=None):
         """
         Retorna o resumo estruturado
 
@@ -104,7 +104,8 @@ class Abstract:
         """
         out = dict()
 
-        out["title"] = abstract_node.findtext("title")
+        out["title"] = abstract_node.findtext("title") if not html else \
+            convert_xml_to_html(abstract_node.find("title"), allowed_tags, xml_to_html)
         out["lang"] = abstract_node.get("{http://www.w3.org/XML/1998/namespace}lang")
 
         for node in abstract_node.xpath("sec"):
@@ -114,41 +115,49 @@ class Abstract:
             p = title = None
             node_title = node.find("title")
             if node_title is not None:
-                title = get_node_without_subtag(node_title)
+                title = get_node_without_subtag(node_title) if not html else \
+                    convert_xml_to_html(node_title, allowed_tags, xml_to_html)
 
             node_p = node.find("p")
             if node_p is not None:
-                p = get_node_without_subtag(node_p)
+                p = get_node_without_subtag(node_p) if not html else \
+                    convert_xml_to_html(node_p, allowed_tags, xml_to_html)
 
             out["sections"].append({"title": title, "p": p})
         else:
             # abstract/p
             node_p = abstract_node.find("p")
             if node_p is not None:
-                out["p"] = get_node_without_subtag(node_p).strip()
+                out["p"] = get_node_without_subtag(node_p).strip() if not html else \
+                    convert_xml_to_html(node_p, allowed_tags, xml_to_html)
         return out
 
-    def _format_abstract(self, abstract_node, style=None):
+    def _format_abstract(self, abstract_node, style=None, html=False, allowed_tags=None, xml_to_html=None):
         if style == "xml":
             # formato xml
             return node_text(abstract_node)
 
         if style == "inline":
             # retorna o conteúdo do nó abstract como str
+            if html:
+                return convert_xml_to_html(abstract_node, allowed_tags, xml_to_html)
             return get_node_without_subtag(abstract_node, remove_extra_spaces=True)
 
         if style == "only_p":
             # retorna somente o conteúdo dos nós abstract//p como str
             texts = []
             for node_p in abstract_node.xpath(".//p"):
-                p_text = get_node_without_subtag(node_p)
+                if html:
+                    p_text = convert_xml_to_html(node_p, allowed_tags, xml_to_html)
+                else:
+                    p_text = get_node_without_subtag(node_p)
                 texts.append(p_text.strip())
             return " ".join(texts)
 
         # retorna abstract em formato de dicionário
-        return self._get_structured_abstract(abstract_node)
+        return self._get_structured_abstract(abstract_node, html, allowed_tags, xml_to_html)
 
-    def get_main_abstract(self, style=None):
+    def get_main_abstract(self, style=None, html=False, allowed_tags=None, xml_to_html=None):
         """
         Retorna o resumo principal no formato indicado.
         Formato padrão: inline
@@ -159,6 +168,9 @@ class Abstract:
             abstract = self._format_abstract(
                 abstract_node=abstract_node,
                 style=style,
+                html=html,
+                allowed_tags=allowed_tags,
+                xml_to_html=xml_to_html
             )
             article_lang = self.xmltree.find(".").get("{http://www.w3.org/XML/1998/namespace}lang")
             abstract_lang = abstract_node.get("{http://www.w3.org/XML/1998/namespace}lang")
@@ -170,7 +182,7 @@ class Abstract:
                 "abstract": abstract
             }
 
-    def _get_sub_article_abstracts(self, style=None):
+    def _get_sub_article_abstracts(self, style=None, html=False, allowed_tags=None, xml_to_html=None):
         """
         Retorna gerador de resumos em sub-article
         """
@@ -184,14 +196,17 @@ class Abstract:
                 item["lang"] = abstract_lang or sub_article_lang
                 item["abstract"] = self._format_abstract(
                     abstract_node=abstract_node,
-                    style=style
+                    style=style,
+                    html=html,
+                    allowed_tags=allowed_tags,
+                    xml_to_html=xml_to_html
                 )
                 if not style:
                     item["abstract"]["lang"] = item["lang"]
                 item['id'] = sub_article.get("id")
                 yield item
 
-    def _get_trans_abstracts(self, style=None):
+    def _get_trans_abstracts(self, style=None, html=False, allowed_tags=None, xml_to_html=None):
         """
         Retorna gerador de resumos trans-abstract
         """
@@ -201,7 +216,10 @@ class Abstract:
             item["lang"] = trans_abstract.get("{http://www.w3.org/XML/1998/namespace}lang")
             item["abstract"] = self._format_abstract(
                 abstract_node=trans_abstract,
-                style=style
+                style=style,
+                html=html,
+                allowed_tags=allowed_tags,
+                xml_to_html=xml_to_html
             )
             yield item
 
