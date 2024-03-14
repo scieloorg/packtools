2831738beb5d0ae4f9ce88d476c998674568d94c
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-03-14 11:03:23 -0300

    Adiciona 'extract_kwd_data_with_lang_html_format'

--- packtools/sps/models/kwd_group.py
+++ packtools/sps/models/kwd_group.py
@@ -169,3 +169,65 @@ class KwdGroup:
                     resp["text"] = keyword_text
 
                     yield resp
+
+    def extract_kwd_data_with_lang_html_format(self,
+                                               tags_to_keep=None,
+                                               tags_to_keep_with_content=None,
+                                               tags_to_remove_with_content=None,
+                                               tags_to_convert_to_html=None
+                                               ):
+        """
+        Extract keyword data with language information by article type from XML tree nodes.
+
+        Params
+        ------
+        tags_to_keep (list, optional): Tags to be preserved. Eg.:
+            ['bold', 'p']
+        tags_to_keep_with_content (list, optional): Tags to be preserved with the respective content. Eg.:
+            ['bold', 'p']
+        tags_to_remove_with_content (list, optional): Tags to be removed with its content. Eg.:
+            ['bold', 'p']
+        tags_to_convert_to_html (dict, optional): Tags to be converted into HTML format. Eg.:
+            {'bold': 'b'}
+
+        Returns
+        -------
+        Iterator[dict]: A generator that yields dictionaries. Eg.:
+            {
+                'parent_name': 'article',
+                'lang': 'pt',
+                'plain_text': None,
+                'html_text': [
+                    '<bold>conteúdo de bold</bold> text',
+                    'text <bold>conteúdo de bold</bold> text',
+                    'text <bold>conteúdo de bold</bold>',
+                    'text <bold>conteúdo <i>de</i> bold</bold>'
+                ]
+            },...
+        """
+        dict_nodes = {
+            'article': self._xmltree.xpath('.//article-meta'),
+            'sub-article': self._xmltree.xpath('./sub-article')
+        }
+
+        for tp, nodes in dict_nodes.items():
+            for node in nodes:
+                node_lang = node.get("{http://www.w3.org/XML/1998/namespace}lang")
+                resp = {}
+                if node.get("id") is not None:
+                    resp['id'] = node.get("id")
+
+                for kwd_group in node.xpath('.//kwd-group'):
+                    kwd_group_lang = kwd_group.get("{http://www.w3.org/XML/1998/namespace}lang", node_lang)
+
+                    keyword_text = []
+                    for kwd in kwd_group.xpath("kwd"):
+                        keywords = xml_utils.process_subtags(kwd, tags_to_keep, tags_to_keep_with_content,
+                                                             tags_to_remove_with_content, tags_to_convert_to_html)
+                        keyword_text.append(keywords)
+                    resp["parent_name"] = tp
+                    resp["lang"] = kwd_group_lang
+                    resp["plain_text"] = None
+                    resp["html_text"] = keyword_text
+
+                    yield resp
