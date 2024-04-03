d2fc0a4a23cea8d79c206e58ff5436f80abd3d12
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-04-03 11:04:55 -0300

    Refatora a classe 'PeerReview'

--- packtools/sps/models/peer_review.py
+++ packtools/sps/models/peer_review.py
@@ -1,16 +1,36 @@
 from ..utils import xml_utils
+"""
+<custom-meta-group>
+<custom-meta>
+<meta-name>peer-review-recommendation</meta-name>
+<meta-value>accept</meta-value>
+</custom-meta>
+</custom-meta-group>
+"""
 
 
-class PeerReview:
-    def __init__(self, xmltree):
-        self.xmltree = xmltree
+class CustomMetaGroup:
+    def __init__(self, main_node):
+        self.main_node = main_node
 
     @property
-    def meta_names(self):
-        for item in self.xmltree.xpath('.//custom-meta-group//custom-meta//meta-name'):
-            yield item.text
+    def custom_meta(self):
+        for item in self.main_node.xpath('.//custom-meta-group//custom-meta'):
+            yield CustomMeta(item)
+
+
+class CustomMeta:
+    def __init__(self, custom_meta_node):
+        self.custom_meta_node = custom_meta_node
 
     @property
-    def meta_values(self):
-        for item in self.xmltree.xpath('.//custom-meta-group//custom-meta//meta-value'):
-            yield item.text
+    def meta_name(self):
+        return self.custom_meta_node.findtext('meta-name')
+
+    @property
+    def meta_value(self):
+        return self.custom_meta_node.findtext('meta-value')
+
+
+class PeerReview(CustomMetaGroup):
+    pass
