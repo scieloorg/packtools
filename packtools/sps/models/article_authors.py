545720b637b7c1fd58b02dd04374f80643063e63
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-04-18 10:30:17 -0300

    Adiciona a classe 'Contrib'

--- packtools/sps/models/article_authors.py
+++ packtools/sps/models/article_authors.py
@@ -9,73 +9,66 @@ def _get_collab(node):
         return {}
 
 
-class Authors:
-    def __init__(self, node):
-        # node é um nó que pode representar 'article' ou 'sub-article'
+class Contrib:
+    def __init__(self, xmltree, node):
+        self.xmltree = xmltree
         self.node = node
 
     @property
-    def collab(self):
+    def contrib(self):
+        _author = _get_collab(self.node)
+        for tag in ("surname", "prefix", "suffix"):
+            data = self.node.findtext(f".//{tag}")
+            if data:
+                _author[tag] = data
+        if data := self.node.findtext(".//given-names"):
+            _author["given_names"] = data
+
         try:
-            return self.node.xpath(".//collab")[0].text
+            _author["orcid"] = self.node.xpath("contrib-id[@contrib-id-type='orcid']")[
+                0
+            ].text
         except IndexError:
-            return None
+            pass
+
+        _author["role"] = []
+        for role in self.node.xpath(".//role"):
+            _author["role"].append({
+                "text": role.text,
+                "content-type": role.get("content-type"),
+                "specific-use": role.get("specific-use")
+            })
+        if not _author["role"]:
+            _author.pop("role")
+
+        for xref in self.node.findall('.//xref'):
+            rid = xref.get("rid")
+
+            if not rid:
+                continue
+            _author.setdefault("rid", [])
+            _author["rid"].append(rid)
+
+            reftype = xref.get("ref-type")
+            if not reftype == "aff":
+                continue
+            _author.setdefault("rid-aff", [])
+            _author["rid-aff"].append(rid)
+
+        _author["aff_rids"] = _author.get("rid-aff")
+        _author["contrib-type"] = self.node.attrib.get("contrib-type")
+        return _author
 
     @property
-    def contribs(self):
-        _data = []
-        for node in self.node.xpath(".//contrib"):
-            _author = _get_collab(node)
-            for tag in ("surname", "prefix", "suffix"):
-                data = node.findtext(f".//{tag}")
-                if data:
-                    _author[tag] = data
-            if data := node.findtext(".//given-names"):
-                _author["given_names"] = data
-
-            try:
-                _author["orcid"] = node.xpath("contrib-id[@contrib-id-type='orcid']")[
-                    0
-                ].text
-            except IndexError:
-                pass
-
-            _author["role"] = []
-            for role in node.xpath(".//role"):
-                _author["role"].append({
-                    "text": role.text,
-                    "content-type": role.get("content-type"),
-                    "specific-use": role.get("specific-use")
-                })
-            if not _author["role"]:
-                _author.pop("role")
-
-            for xref in node.findall('.//xref'):
-                rid = xref.get("rid")
-
-                if not rid:
-                    continue
-                _author.setdefault("rid", [])
-                _author["rid"].append(rid)
-
-                reftype = xref.get("ref-type")
-                if not reftype == "aff":
-                    continue
-                _author.setdefault("rid-aff", [])
-                _author["rid-aff"].append(rid)
-
-            _author["aff_rids"] = _author.get("rid-aff")
-            _author["contrib-type"] = node.attrib.get("contrib-type")
-            _data.append(_author)
-        return _data
-
-    @property
-    def contribs_with_affs(self):
-        affs = Affiliation(self.node)
+    def contrib_with_aff(self):
+        contrib = self.contrib
+        affs = Affiliation(self.xmltree)
         affs_by_id = affs.affiliation_by_id
+        for rid in contrib.get("aff_rids") or []:
+            aff = affs_by_id.get(rid)
+            if aff:
+                contrib.setdefault("affs", [])
+                contrib["affs"].append(aff)
+        return contrib
 
-        for item in self.contribs:
-            for rid in item.get("aff_rids") or []:
-                item.setdefault("affs", [])
-                item["affs"].append(affs_by_id.get(rid))
-            yield item
+
