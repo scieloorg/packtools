81035818a79874ba4a55f6307dd39f9871c47626
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-05-08 12:02:59 -0300

    Adiciona 'docstring'

--- packtools/sps/validation/alternatives.py
+++ packtools/sps/validation/alternatives.py
@@ -9,7 +9,70 @@ class AlternativeValidation:
         self.obtained_children = alternative.get("alternative_children")
         self.children_list = children_list
 
+        """
+            Check whether the alternatives match the tag that contains them.
 
+            XML input
+            ---------
+            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"
+            dtd-version="1.0" article-type="research-article" xml:lang="pt">
+                <body>
+                    <table-wrap>
+                        <alternatives>
+                            <p />
+                        </alternatives>
+                    </table-wrap>
+                </body>
+                <sub-article article-type="translation" xml:lang="en" id="TRen">
+                    <body>
+                        <fig>
+                            <alternatives>
+                                <title />
+                                <abstract />
+                            </alternatives>
+                        </fig>
+                    </body>
+                </sub-article>
+            </article>
+
+            Params
+            ------
+            alternative : dict, such as:
+                {
+                    'alternative_children': ['graphic', 'table'],
+                    'alternative_parent': 'table-wrap',
+                    'parent': 'article',
+                    'parent_id': None
+                }
+
+            children_list : list, such as:
+                ["graphic", "table"]
+
+            Returns
+            -------
+            list[dict], such as:
+                [
+                    {
+                        'title': 'Alternatives validation',
+                        'parent': 'article',
+                        'parent_id': None,
+                        'item': 'table-wrap',
+                        'sub_item': 'alternatives',
+                        'validation_type': 'value in list',
+                        'expected_value': ['graphic', 'table'],
+                        'got_value': ['p'],
+                        'response': 'ERROR',
+                        'message': "Got ['p'], expected ['graphic', 'table']",
+                        'advice': "Provide child tags according to the list: ['graphic', 'table']",
+                        'data': {
+                            'alternative_children': ['p'],
+                            'alternative_parent': 'table-wrap',
+                            'parent': 'article',
+                            'parent_id': None
+                        }
+                    },
+                ]
+        """
         for tag in self.obtained_children:
             if tag not in (self.parent_children_dict.get(self.obtained_parent) or []):
                 yield self.create_validation_response(
