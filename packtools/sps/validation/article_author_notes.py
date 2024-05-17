60507bc6c0f07f47dda839105be672c1112d0b2e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-05-17 10:05:23 -0300

    Corrige a validação do atributo <fn-type>
    - Altera para um nome mais significativo;
    - Adiciona documentação;
    - Renomeia 'fn_numbers' para 'fn_count';
    - Altera o tipo de validação para 'exist';
    - Corrige 'advice'.

--- packtools/sps/validation/article_author_notes.py
+++ packtools/sps/validation/article_author_notes.py
@@ -92,22 +92,84 @@ class AuthorNotesValidation:
                 data=author_note
             )
 
-    def fn_type_validation(self):
+    def validate_fn_type_attribute_presence(self):
+        """
+            Checks if every fn tag has the fn-type attribute.
+
+            XML input
+            ---------
+            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" dtd-version="1.0" article-type="research-article" xml:lang="pt">
+                <front>
+                    <article-meta>
+                        <author-notes>
+                            <corresp>
+                                <label>Correspondência</label>:  Karine de Lima Sírio Boclin  Sousa Lima, 257 apto. 902 Copacabana  22081-010 Rio de Janeiro, RJ, Brasil  E-mail: <email>karine.boclin@gmail.com</email>
+                            </corresp>
+                            <fn fn-type="conflict">
+                                <p>Os autores declaram não haver conflito de interesses.</p>
+                            </fn>
+                        </author-notes>
+                    </article-meta>
+                </front>
+                <sub-article article-type="translation" id="TRen" xml:lang="en">
+                    <front-stub>
+                        <author-notes>
+                            <corresp>
+                                <label>Correspondence</label>:  Karine de Lima Sírio Boclin  Sousa Lima, 257 apto. 902 Copacabana  22081-010 Rio de Janeiro, RJ, Brasil  E-mail: <email>karine.boclin@gmail.com</email>
+                            </corresp>
+                            <fn fn-type="conflict">
+                                <p>The authors declare that there is no conflict of interest.</p>
+                            </fn>
+                        </author-notes>
+                    </front-stub>
+                </sub-article>
+            </article>
+
+            Returns
+            -------
+            list of dict
+                A list of dictionaries, such as:
+                [
+                    {
+                        'title': 'Author notes validation',
+                        'parent': 'article',
+                        'parent_id': None,
+                        'item': 'fn',
+                        'sub_item': '@fn-type',
+                        'validation_type': 'exist',
+                        'response': 'OK',
+                        'expected_value': '1 fn-types',
+                        'got_value': '1 fn-types',
+                        'message': "Got 1 fn-types, expected 1 fn-types",
+                        'advice': None,
+                        'data': {
+                            'corresp': ['Correspondência: Karine de Lima Sírio Boclin Sousa Lima, 257 apto. 902 Copacabana'
+                                             ' 22081-010 Rio de Janeiro, RJ, Brasil E-mail: karine.boclin@gmail.com'],
+                            'fn_count': 1,
+                            'fn_types': ['conflict'],
+                            'parent': 'article',
+                            'parent_id': None,
+                            'parent_article_type': 'research-article',
+                            'parent_lang': 'pt'
+                            }
+                    },...
+                ]
+            """
         for author_note in self.author_notes:
             fn_types = author_note.get("fn_types")
-            fn_numbers = author_note.get("fn_count")
-            is_valid = len(fn_types) == fn_numbers
+            fn_count = author_note.get("fn_count")
+            is_valid = len(fn_types) == fn_count
             yield format_response(
                 title="Author notes validation",
                 parent=author_note.get("parent"),
                 parent_id=author_note.get("parent_id"),
                 item="fn",
                 sub_item="@fn-type",
-                validation_type="match",
+                validation_type="exist",
                 is_valid=is_valid,
-                expected=f"{fn_numbers} fn-types",
+                expected=f"{fn_count} fn-types",
                 obtained=f"{len(fn_types)} fn-types",
-                advice="provide one <@fn-type> for each <fn> tag",
+                advice="provide one @fn-type for each <fn> tag",
                 data=author_note
             )
 
