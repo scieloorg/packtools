3c3b2d25be9ddd0ced0a3eb565b5f977c57f304e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-05-21 11:37:48 -0300

    Adiciona testes

--- tests/sps/models/test_article_contribs.py
+++ tests/sps/models/test_article_contribs.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 
 from lxml import etree
 
-from packtools.sps.models.article_contribs import Contrib
+from packtools.sps.models.article_contribs import Contrib, ContribGroup, ArticleContribs
+from packtools.sps.utils import xml_utils
 
 
 class ContribTest(TestCase):
@@ -553,3 +554,209 @@ class ContribWithoutRoleTest(TestCase):
         }
 
         self.assertDictEqual(obtained, expected)
+
+
+class ContribGroupTest(TestCase):
+    def setUp(self):
+        xml = """
+            <article>
+                <front>
+                    <article-meta>
+                        <contrib-group>
+                            <contrib contrib-type="author">
+                                <collab collab-type="committee">Technical Committee ISO/TC 108, Subcommittee SC 2</collab>
+                                <xref ref-type="aff" rid="aff1"/>
+                            </contrib>
+                            <contrib contrib-type="author">
+                                <collab>
+                                    <named-content content-type="program">Joint United
+                                    Nations Program on HIV/AIDS (UNAIDS)</named-content>,
+                                    <institution>World Health Organization</institution>,
+                                    Geneva, <country>Switzerland</country>
+                                </collab>
+                            </contrib>
+                            <contrib contrib-type="author">
+                                <collab>
+                                    <named-content content-type="program">Nonoccupational HIV
+                                    PEP Task Force, Brown University AIDS Program</named-content>
+                                    and the <institution>Rhode Island Department of
+                                    Health</institution>, Providence, Rhode Island
+                                </collab>
+                            </contrib>
+                            <contrib contrib-type="author">
+                                <name>
+                                    <surname>VENEGAS-MARTÍNEZ</surname>
+                                    <given-names>FRANCISCO</given-names>
+                                    <prefix>Prof</prefix>
+                                    <suffix>Nieto</suffix>
+                                </name>
+                                <xref ref-type="aff" rid="aff1"/>
+                                <xref ref-type="aff" rid="aff2"/>
+                            </contrib>
+                        </contrib-group>
+                    </article-meta>
+                </front>
+            </article>
+            """
+        self.xmltree = etree.fromstring(xml)
+
+    def test_data(self):
+        self.maxDiff = None
+        contrib_group = self.xmltree.xpath(".//contrib-group")[0]
+        obtained = list(ContribGroup(contrib_group).contribs)
+        expected = [
+            {
+                'collab': 'Technical Committee ISO/TC 108, Subcommittee SC 2',
+                'contrib_type': 'author',
+                'contrib_xref': [{'ref_type': 'aff', 'rid': 'aff1', 'text': None}]
+            },
+            {
+                'collab': 'Joint United Nations Program on HIV/AIDS (UNAIDS) , World Health Organization , Geneva, '
+                          'Switzerland',
+                'contrib_type': 'author'
+            },
+            {
+                'collab': 'Nonoccupational HIV PEP Task Force, Brown University AIDS Program and the Rhode Island '
+                          'Department of Health , Providence, Rhode Island',
+                'contrib_type': 'author'
+            },
+            {
+                'contrib_name': {
+                    'given-names': 'FRANCISCO',
+                    'prefix': 'Prof',
+                    'suffix': 'Nieto',
+                    'surname': 'VENEGAS-MARTÍNEZ'
+                },
+                'contrib_type': 'author',
+                'contrib_xref': [
+                    {'ref_type': 'aff', 'rid': 'aff1', 'text': None},
+                    {'ref_type': 'aff', 'rid': 'aff2', 'text': None}
+                ]
+            }
+        ]
+
+        for i, item in enumerate(expected):
+            with self.subTest(i):
+                self.assertDictEqual(item, obtained[i])
+
+
+class ArticleContribTest(TestCase):
+    def setUp(self):
+        xml = """
+            <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
+               <front>
+                  <article-meta>
+                     <contrib-group>
+                        <contrib contrib-type="author">
+                           <contrib-id contrib-id-type="orcid">0000-0003-2243-0821</contrib-id>
+                           <name>
+                              <surname>Castro</surname>
+                              <given-names>Silvana de</given-names>
+                           </name>
+                           <xref ref-type="aff" rid="aff1">a</xref>
+                           <xref ref-type="corresp" rid="c1">*</xref>
+                        </contrib>
+                        <aff id="aff1">
+                           <label>a</label>
+                           <institution content-type="orgname">Universidade Federal do Rio de Janeiro (UFRJ)</institution>
+                           <addr-line>
+                              <named-content content-type="city">Rio de Janeiro</named-content>
+                              <named-content content-type="state">RJ</named-content>
+                           </addr-line>
+                           <country country="BR">Brazil</country>
+                           <institution content-type="original">Universidade Federal do Rio de Janeiro (UFRJ)</institution>
+                        </aff>
+                     </contrib-group>
+                  </article-meta>
+               </front>
+               <sub-article article-type="translation" id="SA1" xml:lang="pt">
+                  <front-stub>
+                     <article-id pub-id-type="doi">10.1016/j.bjan.2019.01.002</article-id>
+                     <contrib-group>
+                        <contrib contrib-type="author">
+                           <contrib-id contrib-id-type="orcid">0000-0003-2243-0821</contrib-id>
+                           <name>
+                              <surname>Castro</surname>
+                              <given-names>Silvana de</given-names>
+                           </name>
+                           <xref ref-type="aff" rid="aff4">a</xref>
+                           <xref ref-type="corresp" rid="c2">*</xref>
+                        </contrib>
+                     </contrib-group>
+                     <aff id="aff4">
+                        <label>a</label>
+                        <institution content-type="original">Universidade Federal do Rio de Janeiro (UFRJ)</institution>
+                     </aff>
+                  </front-stub>
+               </sub-article>
+            </article>
+            """
+        self.xmltree = etree.fromstring(xml)
+
+    def test_data(self):
+        self.maxDiff = None
+        obtained = list(ArticleContribs(self.xmltree).contribs)
+        expected = [
+            {
+                'parent': 'article',
+                'parent_article_type': 'research-article',
+                'parent_id': None,
+                'parent_lang': 'en',
+                'contrib_ids': {'orcid': '0000-0003-2243-0821'},
+                'contrib_name': {'given-names': 'Silvana de', 'surname': 'Castro'},
+                'contrib_type': 'author',
+                'contrib_xref': [
+                    {'ref_type': 'aff', 'rid': 'aff1', 'text': 'a'},
+                    {'ref_type': 'corresp', 'rid': 'c1', 'text': '*'}
+                ],
+                'affs': [
+                    {
+                        'city': 'Rio de Janeiro',
+                        'country_code': 'BR',
+                        'country_name': 'Brazil',
+                        'email': None,
+                        'id': 'aff1',
+                        'label': 'a',
+                        'orgdiv1': None,
+                        'orgdiv2': None,
+                        'orgname': 'Universidade Federal do Rio de Janeiro (UFRJ)',
+                        'original': 'Universidade Federal do Rio de Janeiro (UFRJ)',
+                        'state': 'RJ'
+                    }
+                ]
+            },
+            {
+                'parent': 'sub-article',
+                'parent_article_type': 'translation',
+                'parent_id': 'SA1',
+                'parent_lang': 'pt',
+                'contrib_ids': {'orcid': '0000-0003-2243-0821'},
+                'contrib_name': {'given-names': 'Silvana de', 'surname': 'Castro'},
+                'contrib_type': 'author',
+                'contrib_xref': [
+                    {'ref_type': 'aff', 'rid': 'aff4', 'text': 'a'},
+                    {'ref_type': 'corresp', 'rid': 'c2', 'text': '*'}
+                ],
+                'affs': [
+                    {
+                        'city': None,
+                        'country_name': None,
+                        'email': None,
+                        'id': 'aff4',
+                        'label': 'a',
+                        'orgdiv1': None,
+                        'orgdiv2': None,
+                        'orgname': None,
+                        'original': 'Universidade Federal do Rio de Janeiro (UFRJ)',
+                        'state': None
+                    }
+                ]
+            }
+
+        ]
+
+        for i, item in enumerate(expected):
+            with self.subTest(i):
+                self.assertDictEqual(item, obtained[i])
+
+
