654a31732025c7ab778b84db131e544007a5af72
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-05-21 11:37:34 -0300

    Adiciona 'ContribGroup' e 'ArticleContribs'

--- packtools/sps/models/article_contribs.py
+++ packtools/sps/models/article_contribs.py
@@ -1,3 +1,6 @@
+from packtools.sps.models.aff import Affiliation
+from packtools.sps.utils.xml_utils import node_plain_text
+
 """
 <contrib contrib-type="author">
     <contrib-id contrib-id-type="orcid">0000-0001-8528-2091</contrib-id>
@@ -41,7 +44,7 @@ class Contrib:
 
     @property
     def collab(self):
-        return self.node.findtext("collab")
+        return node_plain_text(self.node.find("collab"))
 
     @property
     def contrib_xref(self):
@@ -65,23 +68,81 @@ class Contrib:
     def data(self):
         data = {}
         for key, value in zip(
-            (
-                "contrib_type",
-                "contrib_ids",
-                "contrib_name",
-                "collab",
-                "contrib_xref",
-                "contrib_role"
-            ),
-            (
-                self.contrib_type,
-                self.contrib_ids,
-                self.contrib_name,
-                self.collab,
-                list(self.contrib_xref),
-                list(self.contrib_role)
-            )
+                (
+                        "contrib_type",
+                        "contrib_ids",
+                        "contrib_name",
+                        "collab",
+                        "contrib_xref",
+                        "contrib_role"
+                ),
+                (
+                        self.contrib_type,
+                        self.contrib_ids,
+                        self.contrib_name,
+                        self.collab,
+                        list(self.contrib_xref),
+                        list(self.contrib_role)
+                )
         ):
             if value:
                 data[key] = value
         return data
+
+
+class ContribGroup:
+    def __init__(self, node):
+        self.node = node
+
+    @property
+    def contribs(self):
+        for contribs in self.node.xpath(".//contrib"):
+            contrib = Contrib(contribs)
+            yield contrib.data
+
+
+class ArticleContribs:
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
+        self.aff = Affiliation(xmltree).affiliation_by_id
+
+    @property
+    def contribs(self):
+        for node, lang, article_type, parent, parent_id in _get_parent_context(self.xmltree):
+            for contrib_group in node.xpath(".//contrib-group"):
+                for contrib in ContribGroup(contrib_group).contribs:
+                    affs = list(_get_affs(self.aff, contrib))
+                    if affs:
+                        contrib['affs'] = affs
+                    yield _put_parent_context(contrib, lang, article_type, parent, parent_id)
+
+
+def _get_parent_context(xmltree):
+    main = xmltree.xpath(".")[0]
+    main_lang = main.get("{http://www.w3.org/XML/1998/namespace}lang")
+    main_article_type = main.get("article-type")
+    for node in xmltree.xpath(".//article-meta | .//sub-article"):
+        parent = "sub-article" if node.tag == "sub-article" else "article"
+        parent_id = node.get("id")
+        lang = node.get("{http://www.w3.org/XML/1998/namespace}lang") or main_lang
+        article_type = node.get("article-type") or main_article_type
+        yield node, lang, article_type, parent, parent_id
+
+
+def _put_parent_context(data, lang, article_type, parent, parent_id):
+    data.update(
+        {
+            "parent": parent,
+            "parent_id": parent_id,
+            "parent_lang": lang,
+            "parent_article_type": article_type,
+        }
+    )
+    return data
+
+
+def _get_affs(affs, contrib):
+    for xref in contrib.get('contrib_xref'):
+        aff = affs.get(xref.get('rid'))
+        if aff:
+            yield aff
