5ffe8abb467d0de6a69b4c2cf0aa9e8bed061b18
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-06-03 12:17:10 -0300

    Padroniza resposta

--- packtools/sps/validation/aff.py
+++ packtools/sps/validation/aff.py
@@ -1,5 +1,6 @@
 from packtools.sps.models.aff import Affiliation
 from packtools.sps.validation.exceptions import AffiliationValidationValidateCountryCodeException
+from packtools.sps.validation.utils import format_response
 
 from packtools.translator import _
 
@@ -29,43 +30,52 @@ class AffiliationValidation:
         self.country_codes_list = country_codes_list
 
     def validate_original(self):
-        original = self.affiliation.get('original')
-        yield {
-            'title': 'aff/institution element original attribute validation',
-            'xpath': './/aff/institution[@content-type="original"]',
-            'validation_type': 'exist',
-            'response': 'OK' if original else 'ERROR',
-            'expected_value': _('original affiliation'),
-            'got_value': original,
-            'message': _('Got {}, expected original affiliation').format(original),
-            'advice': None if original else _('provide the original affiliation')
-        }
+        original = self.affiliation.get("original")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="institution",
+            sub_item='@content-type="original"',
+            validation_type="exist",
+            is_valid=bool(original),
+            expected=_("original affiliation"),
+            obtained=original,
+            advice=_("provide the original affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_orgname(self):
-        orgname = self.affiliation.get('orgname')
-        yield {
-            'title': 'aff/institution element orgname attribute validation',
-            'xpath': './/aff/institution[@content-type="orgname"]',
-            'validation_type': 'exist',
-            'response': 'OK' if orgname else 'ERROR',
-            'expected_value': _('orgname affiliation'),
-            'got_value': orgname,
-            'message': _('Got {}, expected orgname affiliation').format(orgname),
-            'advice': None if orgname else _('provide the orgname affiliation')
-        }
+        orgname = self.affiliation.get("orgname")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="institution",
+            sub_item='@content-type="orgname"',
+            validation_type="exist",
+            is_valid=bool(orgname),
+            expected=_("orgname affiliation"),
+            obtained=orgname,
+            advice=_("provide the orgname affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_country(self):
-        country = self.affiliation.get('country_name')
-        yield {
-            'title': 'aff element country attribute validation',
-            'xpath': './/aff/country',
-            'validation_type': 'exist',
-            'response': 'OK' if country else 'ERROR',
-            'expected_value': _('country affiliation'),
-            'got_value': country,
-            'message': _('Got {}, expected country affiliation').format(country),
-            'advice': None if country else _('provide the country affiliation')
-        }
+        country = self.affiliation.get("country_name")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="aff",
+            sub_item="country",
+            validation_type="exist",
+            is_valid=bool(country),
+            expected=_("country affiliation"),
+            obtained=country,
+            advice=_("provide the country affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_country_code(self, country_codes_list=None):
         country_codes_list = country_codes_list or self.country_codes_list
@@ -73,43 +83,52 @@ class AffiliationValidation:
             raise AffiliationValidationValidateCountryCodeException(
                 "Function requires list of country codes"
             )
-        country_code = self.affiliation.get('country_code')
-        yield {
-            'title': 'aff element @country attribute validation',
-            'xpath': './/aff/@country',
-            'validation_type': 'value in list',
-            'response': 'OK' if country_code in country_codes_list else 'ERROR',
-            'expected_value': self.country_codes_list,
-            'got_value': country_code,
-            'message': _('Got {}, expected one item of this list: {}').format(country_code, country_codes_list),
-            'advice': None if country_code in country_codes_list else _('provide a valid @country affiliation')
-        }
+        country_code = self.affiliation.get("country_code")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="country",
+            sub_item="@country",
+            validation_type="value in list",
+            is_valid=country_code in country_codes_list,
+            expected=self.country_codes_list,
+            obtained=country_code,
+            advice=_("provide a valid @country affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_state(self):
-        state = self.affiliation.get('state')
-        yield {
-            'title': 'aff/addr-line element state attribute validation',
-            'xpath': './/aff/addr-line/named-content[@content-type="state"] or .//aff/addr-line/state',
-            'validation_type': 'exist',
-            'response': 'OK' if state else 'ERROR',
-            'expected_value': _('state affiliation'),
-            'got_value': state,
-            'message': _('Got {}, expected state affiliation').format(state),
-            'advice': None if state else _('provide the state affiliation')
-        }
+        state = self.affiliation.get("state")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="addr-line",
+            sub_item="state",
+            validation_type="exist",
+            is_valid=bool(state),
+            expected=_("state affiliation"),
+            obtained=state,
+            advice=_("provide the state affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_city(self):
-        city = self.affiliation.get('city')
-        yield {
-            'title': 'aff/addr-line element city attribute validation',
-            'xpath': './/aff/addr-line/named-content[@content-type="city"] or .//aff/addr-line/city',
-            'validation_type': 'exist',
-            'response': 'OK' if city else 'ERROR',
-            'expected_value': _('city affiliation'),
-            'got_value': city,
-            'message': _('Got {}, expected city affiliation').format(city),
-            'advice': None if city else _('provide the city affiliation')
-        }
+        city = self.affiliation.get("city")
+        yield format_response(
+            title="Affiliation validation",
+            parent=self.affiliation.get("parent"),
+            parent_id=self.affiliation.get("parent_id"),
+            item="addr-line",
+            sub_item="city",
+            validation_type="exist",
+            is_valid=bool(city),
+            expected=_("city affiliation"),
+            obtained=city,
+            advice=_("provide the city affiliation"),
+            data=self.affiliation,
+        )
 
     def validate_affiliation(self):
         yield from self.validate_original()
