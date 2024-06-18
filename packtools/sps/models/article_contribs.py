06437faa0f8871e77baeca6f35d6cef4203f8376
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-06-18 09:27:04 -0300

    Remove funções importadas

--- packtools/sps/models/article_contribs.py
+++ packtools/sps/models/article_contribs.py
@@ -1,5 +1,5 @@
 from packtools.sps.models.aff import Affiliation
-from packtools.sps.utils.xml_utils import node_plain_text
+from packtools.sps.utils.xml_utils import node_plain_text, get_parent_context, put_parent_context
 
 """
 <contrib contrib-type="author">
@@ -117,7 +117,7 @@ class ArticleContribs:
 
     @property
     def contribs(self):
-        for node, lang, article_type, parent, parent_id in _get_parent_context(
+        for node, lang, article_type, parent, parent_id in get_parent_context(
             self.xmltree
         ):
             for contrib_group in node.xpath(".//contrib-group"):
@@ -125,33 +125,9 @@ class ArticleContribs:
                     affs = list(_get_affs(self.aff, contrib))
                     if affs:
                         contrib["affs"] = affs
-                    yield _put_parent_context(
+                    yield put_parent_context(
                         contrib, lang, article_type, parent, parent_id
                     )
-
-
-def _get_parent_context(xmltree):
-    main = xmltree.xpath(".")[0]
-    main_lang = main.get("{http://www.w3.org/XML/1998/namespace}lang")
-    main_article_type = main.get("article-type")
-    for node in xmltree.xpath(".//article-meta | .//sub-article"):
-        parent = "sub-article" if node.tag == "sub-article" else "article"
-        parent_id = node.get("id")
-        lang = node.get("{http://www.w3.org/XML/1998/namespace}lang") or main_lang
-        article_type = node.get("article-type") or main_article_type
-        yield node, lang, article_type, parent, parent_id
-
-
-def _put_parent_context(data, lang, article_type, parent, parent_id):
-    data.update(
-        {
-            "parent": parent,
-            "parent_id": parent_id,
-            "parent_lang": lang,
-            "parent_article_type": article_type,
-        }
-    )
-    return data
 
 
 def _get_affs(affs, contrib):
