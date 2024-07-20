9a23a6fe38299af0e393faa32004d94a7012248a
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-07-20 19:26:57 -0300

    Adapta testes

--- tests/sps/models/test_article_doi_with_lang.py
+++ tests/sps/models/test_article_doi_with_lang.py
@@ -16,18 +16,18 @@ def _get_xmltree(doi=None, doi1=None, doi2=None):
         )
     if doi1:
         doi1 = (
-            """<sub-article xml:lang='es' article-type='translation'><front-stub>"""
+            """<sub-article xml:lang='es' id="01" article-type='translation'><front-stub>"""
             f"""<article-id pub-id-type="doi">{doi1}</article-id>"""
             """</front-stub></sub-article>"""
         )
     if doi2:
         doi2 = (
-            """<sub-article xml:lang='en' article-type='translation'><front-stub>"""
+            """<sub-article xml:lang='en' id="02" article-type='translation'><front-stub>"""
             f"""<article-id pub-id-type="doi">{doi2}</article-id>"""
             """</front-stub></sub-article>"""
         )
     s = (
-        "<article xml:lang='pt'>"
+        "<article xml:lang='pt' article-type='research-article'>"
         "<front>"
         "    <article-meta>"
         f"{doi}"
@@ -56,9 +56,27 @@ class TestDoiWithLang(TestCase):
         self.assertIsNone(article_doi_with_lang.main_doi)
 
     def test_data(self):
+        self.maxDiff = None
         expected = [
-            {"lang": "pt", "value": "10.1590/1678-69712003/administracao.v4n1p108-123"},
-            {"lang": "es", "value": "10.1590/1678-69712003/administracao.v4n1p108-123.es"},
-            {"lang": "en", "value": "10.1590/1678-69712003/administracao.v4n1p108-123.en"},
+            {
+                "lang": "pt",
+                "value": "10.1590/1678-69712003/administracao.v4n1p108-123",
+                "parent": "article",
+                "parent_article_type": "research-article",
+            },
+            {
+                "lang": "es",
+                "value": "10.1590/1678-69712003/administracao.v4n1p108-123.es",
+                "parent": "sub-article",
+                "parent_article_type": "translation",
+                "parent_id": "01",
+            },
+            {
+                "lang": "en",
+                "value": "10.1590/1678-69712003/administracao.v4n1p108-123.en",
+                "parent": "sub-article",
+                "parent_article_type": "translation",
+                "parent_id": "02",
+            },
         ]
         self.assertEqual(expected, self.article_doi_with_lang.data)
