14dd4712e6cf4de7b3c65dde180532352323feb8
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-08-02 10:22:34 -0300

    Adiciona 'docstring'

--- packtools/sps/validation/fig.py
+++ packtools/sps/validation/fig.py
@@ -3,11 +3,51 @@ from packtools.sps.validation.utils import format_response
 
 
 class FigValidation:
+    """
+    A class used to validate the existence of <fig> elements within an XML document.
+
+    Attributes
+    ----------
+    xmltree : lxml.etree._ElementTree
+        The parsed XML document representing the article.
+    figures_by_language : dict
+        A dictionary containing information about figures grouped by language.
+
+    Methods
+    -------
+    validate_fig_existence(error_level="WARNING")
+        Validates the existence of <fig> elements within the XML document and yields formatted responses.
+    """
+
     def __init__(self, xmltree):
+        """
+        Initializes a FigValidation object.
+
+        Parameters
+        ----------
+        xmltree : lxml.etree._ElementTree
+            The parsed XML document representing the article.
+        """
         self.xmltree = xmltree
         self.figures_by_language = ArticleFigs(xmltree).items_by_lang
 
     def validate_fig_existence(self, error_level="WARNING"):
+        """
+        Validates the existence of <fig> elements within the XML document.
+
+        If <fig> elements are found, yields a formatted response for each figure.
+        If no <fig> elements are found, yields a single formatted response indicating their absence.
+
+        Parameters
+        ----------
+        error_level : str, optional
+            The level of the error to be reported (default is "WARNING").
+
+        Yields
+        ------
+        dict
+            A dictionary containing the validation response.
+        """
         if self.figures_by_language:
             for lang, figure_data_list in self.figures_by_language.items():
                 for figure_data in figure_data_list:
