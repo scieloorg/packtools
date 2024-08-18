1dc8e1964b3380e746f235d2960f661007fc4f09
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-08-18 17:27:43 -0300

    Adapta a classe de validação

--- packtools/sps/validation/tablewrap.py
+++ packtools/sps/validation/tablewrap.py
@@ -1,35 +1,34 @@
 from lxml import etree
 
-from packtools.sps.models.tablewrap import ArticleTableWraps
+from packtools.sps.models.tablewrap import ArticleTableWrappers
 from packtools.sps.validation.utils import format_response
 
 
 class TableWrapValidation:
     def __init__(self, xmltree):
         self.xmltree = xmltree
-        self.table_wraps_by_language = ArticleTableWraps(xmltree).items_by_lang
+        self.table_wrappers = list(ArticleTableWrappers(xmltree).article_table_wrappers)
 
     def validate_tablewrap_existence(self, error_level="WARNING"):
-        if self.table_wraps_by_language:
-            for lang, table_wrap_data_list in self.table_wraps_by_language.items():
-                for table_wrap_data in table_wrap_data_list:
-                    table_wrap_node = table_wrap_data.get("node").element
-                    yield format_response(
-                        title="table-wrap presence",
-                        parent=table_wrap_data.get("parent"),
-                        parent_id=table_wrap_data.get("parent_id"),
-                        parent_article_type=table_wrap_data.get("parent_article_type"),
-                        parent_lang=table_wrap_data.get("parent_lang"),
-                        item="table-wrap",
-                        sub_item=None,
-                        validation_type="exist",
-                        is_valid=True,
-                        expected="<table-wrap> element",
-                        obtained=etree.tostring(table_wrap_node, encoding='unicode'),
-                        advice=None,
-                        data=table_wrap_data,
-                        error_level="OK",
-                    )
+        if self.table_wrappers:
+            for table_wrap_data in self.table_wrappers:
+                table_wrap_node = table_wrap_data.get("node").element
+                yield format_response(
+                    title="table-wrap presence",
+                    parent=table_wrap_data.get("parent"),
+                    parent_id=table_wrap_data.get("parent_id"),
+                    parent_article_type=table_wrap_data.get("parent_article_type"),
+                    parent_lang=table_wrap_data.get("parent_lang"),
+                    item="table-wrap",
+                    sub_item=None,
+                    validation_type="exist",
+                    is_valid=True,
+                    expected="<table-wrap> element",
+                    obtained=etree.tostring(table_wrap_node, encoding='unicode'),
+                    advice=None,
+                    data=table_wrap_data,
+                    error_level="OK",
+                )
         else:
             yield format_response(
                 title="table-wrap presence",
