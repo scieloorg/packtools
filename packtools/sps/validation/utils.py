2f303913c49a6bac87aeec5b6c2141bc9a38c1d0
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-08-19 12:45:49 -0300

    Adiciona função para extrair URLs de um nó

--- packtools/sps/validation/utils.py
+++ packtools/sps/validation/utils.py
@@ -79,3 +79,68 @@ def is_valid_url_format(text):
         return False
 
 
+def extract_urls_from_node(xml_node):
+    """
+    Extracts all URLs from the given XML node and its descendants.
+
+    This function searches for any attributes with the `xlink:href` namespace
+    in the provided XML node and its children. It returns a dictionary where
+    the keys are the tag names and the values are lists of URLs associated
+    with each tag.
+
+    Parameters:
+    -----------
+    xml_node : xml.etree.ElementTree.Element
+        The XML node from which URLs are to be extracted.
+
+    Returns:
+    --------
+    dict
+        A dictionary where each key is the name of a tag containing a URL,
+        and each value is a list of URLs found within that tag.
+        Example:
+        {
+            'ext-link': ['https://example.com', 'https://another-example.com'],
+            'graphic': ['https://images.example.com/image1.jpg']
+        }
+
+    Example:
+    --------
+    xml_data = '''
+    <article xmlns:xlink="http://www.w3.org/1999/xlink">
+        <ext-link xlink:href="https://example.com"/>
+        <sec>
+            <ext-link xlink:href="https://another-example.com"/>
+            <graphic xlink:href="https://images.example.com/image1.jpg"/>
+        </sec>
+    </article>
+    '''
+
+    xml_tree = etree.fromstring(xml_data)
+    links_dict = extract_urls_from_node(xml_tree)
+    print(links_dict)
+    # Output:
+    # {
+    #     'ext-link': ['https://example.com', 'https://another-example.com'],
+    #     'graphic': ['https://images.example.com/image1.jpg']
+    # }
+    """
+    links_dict = {}
+
+    # Verifica se o nó atual tem o atributo xlink:href considerando o namespace
+    xlink_href = xml_node.get('{http://www.w3.org/1999/xlink}href')
+    if xlink_href:
+        tag = xml_node.tag
+        links_dict.setdefault(tag, [])
+        links_dict[tag].append(xlink_href)
+
+    # Recursivamente aplica o mesmo processo aos filhos do nó atual
+    for child in xml_node:
+        child_links = extract_urls_from_node(child)
+        for tag, urls in child_links.items():
+            links_dict.setdefault(tag, [])
+            links_dict[tag].extend(urls)
+
+    return links_dict
+
+
