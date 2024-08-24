1ebb643322ef8bc36335c8b68474bdfa86d032c7
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-08-23 23:13:28 -0300

    Adapta testes para o modelo para referências cruzadas

--- tests/sps/models/v2/test_article_xref.py
+++ tests/sps/models/v2/test_article_xref.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from lxml import etree
 
-from packtools.sps.models.v2.article_xref import Xref, Id, Xrefs, Ids, ArticleXref
+from packtools.sps.models.v2.article_xref import Xref, Id, Ids, ArticleXref
 
 
 class XrefTest(TestCase):
@@ -27,14 +27,11 @@ class XrefTest(TestCase):
     def test_rid(self):
         self.assertEqual(self.xref.xref_rid, "aff1")
 
-    def test_parent(self):
-        self.assertEqual(self.xref.xref_parent, "p")
-
     def test_text(self):
         self.assertEqual(self.xref.xref_text, "1")
 
     def test_data(self):
-        obtained = {"ref-type": "aff", "rid": "aff1", "text": "1", "parent-tag": "p"}
+        obtained = {"ref-type": "aff", "rid": "aff1", "text": "1"}
         self.assertDictEqual(self.xref.data, obtained)
 
 
@@ -66,106 +63,38 @@ class IdTest(TestCase):
         self.assertDictEqual(self.node_id.data, obtained)
 
 
-class XrefsTest(TestCase):
-    def setUp(self):
-        self.xml_tree = etree.fromstring(
-            """
-            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" 
-            dtd-version="1.1" specific-use="sps-1.9" article-type="research-article" xml:lang="en">
-                <article-meta>
-                    <p><xref ref-type="aff" rid="aff1">1</xref></p>
-                    <aff id="aff1">
-                        <p>affiliation</p>
-                    </aff>
-
-                    <p><xref ref-type="fig" rid="fig1">1</xref></p>
-                    <fig id="fig1">
-                        <p>figure</p>
-                    </fig>
-
-                    <p><xref ref-type="table" rid="table1">1</xref></p>
-                    <table id="table1">
-                        <p>table</p>
-                    </table>
-                </article-meta>
-            </article>
-            """
-        )
-        self.xrefs = Xrefs(
-            self.xml_tree.xpath(".")[0], "en", "research-article", "article", None
-        )
-
-    def test_xrefs(self):
-        obtained = list(self.xrefs.xrefs())
-        expected = [
-            {
-                "parent": "article",
-                "parent-tag": "p",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "ref-type": "aff",
-                "rid": "aff1",
-                "text": "1",
-            },
-            {
-                "parent": "article",
-                "parent-tag": "p",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "ref-type": "fig",
-                "rid": "fig1",
-                "text": "1",
-            },
-            {
-                "parent": "article",
-                "parent-tag": "p",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "ref-type": "table",
-                "rid": "table1",
-                "text": "1",
-            },
-        ]
-        self.assertEqual(len(obtained), 3)
-        for i, item in enumerate(expected):
-            with self.subTest(i):
-                self.assertDictEqual(item, obtained[i])
-
-
 class IdsTest(TestCase):
     def setUp(self):
         self.xml_tree = etree.fromstring(
             """
-            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" 
+            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"
             dtd-version="1.1" specific-use="sps-1.9" article-type="research-article" xml:lang="en">
-                <article-meta>
-                    <p><xref ref-type="aff" rid="aff1">1</xref></p>
-                    <aff id="aff1">
-                        <p>affiliation</p>
-                    </aff>
-
-                    <p><xref ref-type="fig" rid="fig1">1</xref></p>
-                    <fig id="fig1">
-                        <p>figure</p>
-                    </fig>
-
-                    <p><xref ref-type="table" rid="table1">1</xref></p>
-                    <table id="table1">
-                        <p>table</p>
-                    </table>
-                </article-meta>
+                <front>
+                    <article-meta>
+                    
+                        <p><xref ref-type="aff" rid="aff1">1</xref></p>
+                        <aff id="aff1">
+                            <p>affiliation</p>
+                        </aff>
+    
+                        <p><xref ref-type="fig" rid="fig1">1</xref></p>
+                        <fig id="fig1">
+                            <p>figure</p>
+                        </fig>
+                        <p><xref ref-type="table" rid="table1">1</xref></p>
+                        <table id="table1">
+                            <p>table</p>
+                        </table>
+                    
+                    </article-meta>
+                </front>
             </article>
             """
         )
-        self.ids = Ids(
-            self.xml_tree.xpath(".")[0], "en", "research-article", "article", None
-        )
+        self.ids = Ids(self.xml_tree)
 
     def test_ids(self):
-        obtained = list(self.ids.ids())
+        obtained = list(self.ids.ids(element_name="aff"))
         expected = [
             {
                 "id": "aff1",
@@ -174,25 +103,9 @@ class IdsTest(TestCase):
                 "parent_id": None,
                 "parent_lang": "en",
                 "tag": "aff",
-            },
-            {
-                "id": "fig1",
-                "parent": "article",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "tag": "fig",
-            },
-            {
-                "id": "table1",
-                "parent": "article",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "tag": "table",
-            },
+            }
         ]
-        self.assertEqual(len(obtained), 3)
+        self.assertEqual(len(obtained), 1)
         for i, item in enumerate(expected):
             with self.subTest(i):
                 self.assertDictEqual(item, obtained[i])
@@ -202,7 +115,7 @@ class ArticleXrefTest(TestCase):
     def setUp(self):
         self.xml_tree = etree.fromstring(
             """
-            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" 
+            <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"
                      dtd-version="1.1" specific-use="sps-1.9" article-type="research-article" xml:lang="en">
                 <front>
                     <article-meta>
@@ -212,7 +125,7 @@ class ArticleXrefTest(TestCase):
                         </aff>
                     </article-meta>
                 </front>
-            
+
                 <sub-article article-type="translation" xml:lang="es">
                     <front-stub>
                         <contrib-group>
@@ -236,61 +149,61 @@ class ArticleXrefTest(TestCase):
         self.article_xref = ArticleXref(self.xml_tree)
 
     def test_all_ids(self):
-        obtained = list(self.article_xref.all_ids())
-        expected = [
-            {
-                "id": "aff1",
-                "parent": "article",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "tag": "aff",
-            },
-            {
-                "id": "aff2",
-                "parent": "sub-article",
-                "parent_article_type": "translation",
-                "parent_id": None,
-                "parent_lang": "es",
-                "tag": "aff",
-            },
-        ]
+        obtained = self.article_xref.all_ids(element_name="aff")
+        expected = {
+            "aff1": [
+                {
+                    "id": "aff1",
+                    "parent": "article",
+                    "parent_article_type": "research-article",
+                    "parent_id": None,
+                    "parent_lang": "en",
+                    "tag": "aff",
+                }
+            ],
+            "aff2": [
+                {
+                    "id": "aff2",
+                    "parent": "sub-article",
+                    "parent_article_type": "translation",
+                    "parent_id": None,
+                    "parent_lang": "es",
+                    "tag": "aff",
+                }
+            ]
+        }
         self.assertEqual(len(obtained), 2)
-        for i, item in enumerate(expected):
-            with self.subTest(i):
-                self.assertDictEqual(item, obtained[i])
+        for id, item in expected.items():
+            for i, subitem in enumerate(item):
+                with self.subTest(id):
+                    self.assertDictEqual(subitem, obtained[id][i])
 
     def test_all_xref_rids(self):
-        obtained = list(self.article_xref.all_xref_rids())
-        expected = [
-            {
-                "parent": "article",
-                "parent-tag": "p",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "ref-type": "aff",
-                "rid": "aff1",
-                "text": "1",
-            },
-            {
-                "parent": "sub-article",
-                "parent-tag": "contrib",
-                "parent_article_type": "translation",
-                "parent_id": None,
-                "parent_lang": "es",
-                "ref-type": "aff",
-                "rid": "aff2",
-                "text": "2",
-            },
-        ]
+        obtained = self.article_xref.all_xref_rids()
+        expected = {
+            "aff1": [
+                {
+                    "ref-type": "aff",
+                    "rid": "aff1",
+                    "text": "1",
+                }
+            ],
+            "aff2": [
+                {
+                    "ref-type": "aff",
+                    "rid": "aff2",
+                    "text": "2",
+                }
+            ]
+        }
         self.assertEqual(len(obtained), 2)
-        for i, item in enumerate(expected):
-            with self.subTest(i):
-                self.assertDictEqual(item, obtained[i])
+        for rid, item in expected.items():
+            for i, subitem in enumerate(item):
+                with self.subTest(rid):
+                    self.assertDictEqual(subitem, obtained[rid][i])
 
     def test_article_ids(self):
-        obtained = list(self.article_xref.article_ids())
+        obtained = list(self.article_xref.article_ids(element_name="aff"))
         expected = [
             {
                 "id": "aff1",
@@ -299,25 +212,6 @@ class ArticleXrefTest(TestCase):
                 "parent_id": None,
                 "parent_lang": "en",
                 "tag": "aff",
-            }
-        ]
-        self.assertEqual(len(obtained), 1)
-        for i, item in enumerate(expected):
-            with self.subTest(i):
-                self.assertDictEqual(item, obtained[i])
-
-    def test_article_xref_rids(self):
-        obtained = list(self.article_xref.article_xref_rids())
-        expected = [
-            {
-                "parent": "article",
-                "parent-tag": "p",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "en",
-                "ref-type": "aff",
-                "rid": "aff1",
-                "text": "1",
             }
         ]
         self.assertEqual(len(obtained), 1)
@@ -326,7 +220,7 @@ class ArticleXrefTest(TestCase):
                 self.assertDictEqual(item, obtained[i])
 
     def test_sub_article_translation_ids(self):
-        obtained = list(self.article_xref.sub_article_translation_ids())
+        obtained = list(self.article_xref.sub_article_translation_ids(element_name="aff"))
         expected = [
             {
                 "id": "aff2",
@@ -342,29 +236,6 @@ class ArticleXrefTest(TestCase):
             with self.subTest(i):
                 self.assertDictEqual(item, obtained[i])
 
-    def test_sub_article_translation_xref_rids(self):
-        obtained = list(self.article_xref.sub_article_translation_xref_rids())
-        expected = [
-            {
-                "parent": "sub-article",
-                "parent-tag": "contrib",
-                "parent_article_type": "translation",
-                "parent_id": None,
-                "parent_lang": "es",
-                "ref-type": "aff",
-                "rid": "aff2",
-                "text": "2",
-            }
-        ]
-        self.assertEqual(len(obtained), 1)
-        for i, item in enumerate(expected):
-            with self.subTest(i):
-                self.assertDictEqual(item, obtained[i])
-
     def test_sub_article_non_translation_ids(self):
-        obtained = list(self.article_xref.sub_article_non_translation_ids())
-        self.assertEqual(len(obtained), 0)
-
-    def test_sub_article_non_translation_xref_rids(self):
-        obtained = list(self.article_xref.sub_article_non_translation_xref_rids())
+        obtained = list(self.article_xref.sub_article_non_translation_ids(element_name="aff"))
         self.assertEqual(len(obtained), 0)
