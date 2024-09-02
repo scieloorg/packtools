2cea6f9033d8e722d2a3b60c732733631c008ec1
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-09-02 09:44:58 -0300

    Remove alterações do módulo 'aff.py'

--- packtools/sps/models/aff.py
+++ packtools/sps/models/aff.py
@@ -1,5 +1,3 @@
-import itertools
-
 from packtools.sps.utils.xml_utils import (
     node_text_without_xref,
     get_parent_context,
@@ -145,7 +143,7 @@ class Affiliation:
     @property
     def affiliation_list(self):
         """
-        Returns a list of affiliations.
+        Retorna lista de afiliações
 
         Returns
         -------
@@ -164,189 +162,74 @@ class Affiliation:
                 "email": email or None,
             }
         """
-        return list(self._get_affiliations_by_context())
 
-    def _get_affiliations_by_context(self):
-        """
-        Generator function to get affiliations based on different contexts.
-        """
-        for node, lang, article_type, parent, parent_id in get_parent_context(
-            self._xmltree
-        ):
-            for aff_node in node.xpath(".//aff"):
-                item = self._build_affiliation_item(aff_node, parent)
-                yield put_parent_context(item, lang, article_type, parent, parent_id)
-
-    def _build_affiliation_item(self, aff_node, parent):
-        """
-        Builds the affiliation item dictionary.
-
-        Parameters
-        ----------
-        aff_node : lxml.etree.Element
-            The XML element of the affiliation.
-        parent : str
-            The parent context.
-
-        Returns
-        -------
-        dict
-            The affiliation item.
-        """
         loc_types = ("state", "city")
         inst_types = ("orgname", "orgdiv1", "orgdiv2", "original")
 
-        affiliation_id = aff_node.get("id")
-        label = aff_node.findtext("label")
+        for node, lang, article_type, parent, parent_id in get_parent_context(
+            self._xmltree
+        ):
 
-        item = {
-            "id": affiliation_id,
-            "label": label,
+            for aff_node in node.xpath(".//aff"):
+
+                affiliation_id = aff_node.get("id")
+
+                label = aff_node.findtext("label")
+
+                institution = {}
+                for inst_type in inst_types:
+                    institution[inst_type] = aff_node.findtext(
+                        f'institution[@content-type="{inst_type}"]'
+                    )
+
+                address = {}
+                for loc_type in loc_types:
+                    address[loc_type] = aff_node.findtext(f"addr-line/{loc_type}")
+                    if not address[loc_type]:
+                        address[loc_type] = aff_node.findtext(
+                            f'addr-line/named-content[@content-type="{loc_type}"]'
+                        )
+                address["country_name"] = aff_node.findtext("country")
+
+                try:
+                    address["country_code"] = aff_node.find("country").get("country")
+                except AttributeError:
+                    pass
+                address["email"] = aff_node.findtext("email")
+
+                item = {
+                    "id": affiliation_id,
+                    "label": label,
+                }
+                item.update(institution)
+                item.update(address)
+                yield put_parent_context(item, lang, article_type, parent, parent_id)
+
+    @property
+    def affiliation_by_id(self):
+        """
+        Retorna as afiliações indexadas pelo seu id
+
+        Returns
+        -------
+        dict of dict, which key is id
+        {"aff1":
+            {
+                "id": affiliation_id or None,
+                "label": label or None,
+                "orgname": orgname or None,
+                "orgdiv1": orgdiv1 or None,
+                "orgdiv2": orgdiv2 or None,
+                "original": original or None,
+                "city": city or None,
+                "state": state or None,
+                "country_name": country_name or None,
+                "country_code": country_code or None,
+                "email": email or None,
+            }
         }
-
-        if parent == "sub-article":
-            item["original"] = self._get_institution_info(aff_node, ("original",))[
-                "original"
-            ]
-        else:
-            institution = self._get_institution_info(aff_node, inst_types)
-            address = self._get_address_info(aff_node, loc_types)
-            item.update(institution)
-            item.update(address)
-
-        return item
-
-    def _get_institution_info(self, aff_node, inst_types):
         """
-        Returns institution information.
-
-        Parameters
-        ----------
-        aff_node : lxml.etree.Element
-            The XML element of the affiliation.
-        inst_types : tuple
-            Types of institutions to be retrieved.
-
-        Returns
-        -------
-        dict
-            Institution information.
-        """
-        institution = {}
-        for inst_type in inst_types:
-            institution[inst_type] = aff_node.findtext(
-                f'institution[@content-type="{inst_type}"]'
-            )
-        return institution
-
-    def _get_address_info(self, aff_node, loc_types):
-        """
-        Returns address information.
-
-        Parameters
-        ----------
-        aff_node : lxml.etree.Element
-            The XML element of the affiliation.
-        loc_types : tuple
-            Types of locations to be retrieved.
-
-        Returns
-        -------
-        dict
-            Address information.
-        """
-        address = {}
-        for loc_type in loc_types:
-            address[loc_type] = aff_node.findtext(f"addr-line/{loc_type}")
-            if not address[loc_type]:
-                address[loc_type] = aff_node.findtext(
-                    f'addr-line/named-content[@content-type="{loc_type}"]'
-                )
-        address["country_name"] = aff_node.findtext("country")
-        address["country_code"] = self._get_country_code(aff_node)
-        address["email"] = aff_node.findtext("email")
-        return address
-
-    def _get_country_code(self, aff_node):
-        """
-        Returns the country code.
-
-        Parameters
-        ----------
-        aff_node : lxml.etree.Element
-            The XML element of the affiliation.
-
-        Returns
-        -------
-        str or None
-            Country code, or None if not found.
-        """
-        try:
-            return aff_node.find("country").get("country")
-        except AttributeError:
-            return None
-
-    def get_affiliations_article(self):
-        """
-        Returns affiliations for parent = article.
-
-        Returns
-        -------
-        dict of dict
-            A dictionary of affiliations keyed by affiliation ID.
-        """
-        affiliations = {}
-        for item in self._get_affiliations_by_context():
-            if item["parent"] == "article":
-                affiliations[item["id"]] = item
-        return affiliations
-
-    def get_affiliations_sub_article_translation(self):
-        """
-        Returns affiliations for parent = sub-article and article-type = translation.
-
-        Returns
-        -------
-        dict of dict
-            A dictionary of affiliations keyed by affiliation ID.
-        """
-        affiliations = {}
-        for item in self._get_affiliations_by_context():
-            if (
-                item["parent"] == "sub-article"
-                and item["parent_article_type"] == "translation"
-            ):
-                affiliations[item["id"]] = item
-        return affiliations
-
-    def get_affiliations_sub_article_non_translation(self):
-        """
-        Returns affiliations for parent = sub-article and article-type != translation.
-
-        Returns
-        -------
-        dict of dict
-            A dictionary of affiliations keyed by affiliation ID.
-        """
-        affiliations = {}
-        for item in self._get_affiliations_by_context():
-            if (
-                item["parent"] == "sub-article"
-                and item["parent_article_type"] != "translation"
-            ):
-                affiliations[item["id"]] = item
-        return affiliations
-
-    def get_affiliations_article_or_sub_article(self):
-        """
-        Returns affiliations for parent = article or parent = sub-article.
-
-        Returns
-        -------
-        dict of dict
-            A dictionary of affiliations keyed by affiliation ID.
-        """
-        return (
-            self.get_affiliations_article()
-            | self.get_affiliations_sub_article_translation()
-        )
+        data = {}
+        for item in self.affiliation_list:
+            data[item["id"]] = item
+        return data
