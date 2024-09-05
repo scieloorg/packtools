f2de32e4239e2eb97a52fe5307134c65bf1fc270
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-09-05 11:18:15 -0300

    Corrige "sections"

--- packtools/sps/models/v2/article_toc_sections.py
+++ packtools/sps/models/v2/article_toc_sections.py
@@ -10,14 +10,14 @@ class ArticleTocSections:
     def sections(self):
         for node, lang, article_type, parent, parent_id in get_parent_context(self.xmltree):
             for item in node.xpath(".//subj-group"):
+                section = node_text_without_xref(item.find("./subject")) or None
                 _section = {
                     "subj_group_type": item.get("subj-group-type"),
-                    "section": node_text_without_xref(section := item.find("./subject"))
+                    "section": section
                 }
                 subsections = []
                 for subsection in item.xpath(".//subject"):
-                    if subsection is not None and subsection != section:
-                        subsections.append(node_text_without_xref(subsection))
+                    subsections.append(node_text_without_xref(subsection) or None)
                 _section["subsections"] = subsections
                 yield put_parent_context(_section, lang, article_type, parent, parent_id)
 
