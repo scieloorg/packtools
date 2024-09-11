1129403057122feecee91fafa5417c9e4ef28db8
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-09-11 11:28:31 -0300

    Adapta os testes

--- tests/sps/models/v2/test_notes.py
+++ tests/sps/models/v2/test_notes.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from lxml import etree
 
-from packtools.sps.models.v2.notes import Fn, Fns, FnGroup, FnGroups, AuthorNote, AuthorNotes, ArticleNotes
+from packtools.sps.models.v2.notes import Fn, FnGroup, FnGroups, AuthorNote, AuthorNotes, ArticleNotes
 
 
 class FnTest(TestCase):
@@ -19,6 +19,7 @@ class FnTest(TestCase):
             '<email>roseanap@gmail.com</email>'
             '</corresp>'
             '<fn id="fn_01" fn-type="conflict">'
+            '<bold>*</bold>'
             '<label>1</label>'
             '<p>Os autores declaram não haver conflito de interesses.</p>'
             '</fn>'
@@ -30,80 +31,32 @@ class FnTest(TestCase):
         xmltree = etree.fromstring(xml)
         self.fn = Fn(xmltree.xpath(".//fn")[0])
 
-    def test_fn_id(self):
-        self.assertEqual(self.fn.fn_id, "fn_01")
+    def test_id(self):
+        self.assertEqual(self.fn.id, "fn_01")
 
-    def test_fn_type(self):
-        self.assertEqual(self.fn.fn_type, "conflict")
+    def test_type(self):
+        self.assertEqual(self.fn.type, "conflict")
 
-    def test_fn_label(self):
-        self.assertEqual(self.fn.fn_label, "1")
+    def test_label(self):
+        self.assertEqual(self.fn.label, "1")
 
-    def test_fn_text(self):
-        self.assertEqual(self.fn.fn_text, "1Os autores declaram não haver conflito de interesses.")
+    def test_text(self):
+        self.assertEqual(self.fn.text, "*1Os autores declaram não haver conflito de interesses.")
+
+    def test_has_bold(self):
+        self.assertTrue(self.fn.has_bold)
 
     def test_fn_data(self):
         self.assertDictEqual(
-            self.fn.data(),
+            self.fn.data,
             {
                 "fn_id": "fn_01",
                 "fn_type": "conflict",
                 "fn_label": "1",
-                "fn_text": "1Os autores declaram não haver conflito de interesses."
+                "fn_text": "*1Os autores declaram não haver conflito de interesses.",
+                "fn_has_bold": True
             }
         )
-
-
-class FnsTest(TestCase):
-    def setUp(self):
-        xml = (
-            '<article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'dtd-version="1.0" article-type="research-article" xml:lang="pt">'
-            '<back>'
-            '<fn-group>'
-            '<title>Highlights: </title>'
-            '<fn fn-type="other" id="fn2">'
-            '<p>Study presents design and production of an LED lamp for photovoltaic light traps.</p>'
-            '</fn>'
-            '<fn fn-type="other" id="fn3">'
-            '<p>The LED lamp switches on and off automatically, controls the battery charge and indicates the operating status of the system.</p>'
-            '</fn>'
-            '<fn fn-type="other" id="fn4">'
-            '<p>The LED lamp is a superior substitute for the standard fluorescent lamps used in conventional light traps.</p>'
-            '</fn>'
-            '</fn-group>'
-            '</back>'
-            '</article>'
-        )
-        self.xml_tree = etree.fromstring(xml)
-
-    def test_fns(self):
-        obtained = list(Fns(self.xml_tree).fns())
-        expected = [
-            {
-                'fn_id': 'fn2',
-                'fn_text': 'Study presents design and production of an LED lamp for photovoltaic light traps.',
-                'fn_label': None,
-                'fn_type': 'other'
-            },
-            {
-                'fn_id': 'fn3',
-                'fn_text': 'The LED lamp switches on and off automatically, controls the battery charge and indicates '
-                           'the operating status of the system.',
-                'fn_label': None,
-                'fn_type': 'other'
-            },
-            {
-                'fn_id': 'fn4',
-                'fn_text': 'The LED lamp is a superior substitute for the standard fluorescent lamps used in '
-                           'conventional light traps.',
-                'fn_label': None,
-                'fn_type': 'other'
-            }
-        ]
-        for i, item in enumerate(obtained):
-            with self.subTest(i):
-                self.assertDictEqual(item, expected[i])
 
 
 class FnGroupTest(TestCase):
@@ -130,14 +83,15 @@ class FnGroupTest(TestCase):
         self.xml_tree = etree.fromstring(xml)
 
     def test_fn_group(self):
-        obtained = list(FnGroup(self.xml_tree.xpath(".//fn-group")[0]).fn_group())
+        obtained = list(FnGroup(self.xml_tree.xpath(".//fn-group")[0]).fns)
         expected = [
             {
                 'fn_id': 'fn2',
                 'fn_text': 'Study presents design and production of an LED lamp for photovoltaic light traps.',
                 'fn_label': None,
                 'fn_type': 'other',
-                'fn_parent': 'fn-group'
+                'fn_parent': 'fn-group',
+                'fn_has_bold': False
             },
             {
                 'fn_id': 'fn3',
@@ -145,7 +99,8 @@ class FnGroupTest(TestCase):
                            'the operating status of the system.',
                 'fn_label': None,
                 'fn_type': 'other',
-                'fn_parent': 'fn-group'
+                'fn_parent': 'fn-group',
+                'fn_has_bold': False
             },
             {
                 'fn_id': 'fn4',
@@ -153,7 +108,8 @@ class FnGroupTest(TestCase):
                            'conventional light traps.',
                 'fn_label': None,
                 'fn_type': 'other',
-                'fn_parent': 'fn-group'
+                'fn_parent': 'fn-group',
+                'fn_has_bold': False
             }
         ]
         for i, item in enumerate(obtained):
@@ -186,20 +142,23 @@ class FnGroupsTest(TestCase):
 
     def test_fn_groups(self):
         self.maxDiff = None
-        obtained = list(FnGroups(self.xml_tree.find(".")).fn_groups())
+        obtained = list(FnGroups(self.xml_tree.find(".")).items)
         expected = [
             {
                 'parent': 'article',
                 'parent_article_type': 'research-article',
                 'parent_id': None,
                 'parent_lang': 'pt',
+                'label': None,
+                'title': 'Highlights: ',
                 'fns': [
                     {
                         'fn_id': 'fn2',
                         'fn_text': 'Study presents design and production of an LED lamp for photovoltaic light traps.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': False
                     },
                     {
                         'fn_id': 'fn3',
@@ -207,7 +166,8 @@ class FnGroupsTest(TestCase):
                                    'the operating status of the system.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': False
                     },
                     {
                         'fn_id': 'fn4',
@@ -215,7 +175,8 @@ class FnGroupsTest(TestCase):
                                    'conventional light traps.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': False
                     }
                 ]
             }
@@ -250,7 +211,7 @@ class AuthorNoteTest(TestCase):
         self.xml_tree = etree.fromstring(xml)
 
     def test_author_note(self):
-        obtained = list(AuthorNote(self.xml_tree.xpath(".//author-notes")[0]).author_note())
+        obtained = list(AuthorNote(self.xml_tree.xpath(".//author-notes")[0]).fns)
         expected = [
             {
                 'fn_id': 'fn_01',
@@ -258,6 +219,7 @@ class AuthorNoteTest(TestCase):
                 'fn_parent': 'author-notes',
                 'fn_text': '1Os autores declaram não haver conflito de interesses.',
                 'fn_type': 'conflict',
+                'fn_has_bold': False
             }
         ]
         for i, item in enumerate(obtained):
@@ -304,7 +266,7 @@ class AuthorNotesTest(TestCase):
 
     def test_author_notes(self):
         self.maxDiff = None
-        obtained = list(AuthorNotes(self.xml_tree.find(".")).author_notes())
+        obtained = list(AuthorNotes(self.xml_tree.find(".")).items)
         expected = [
             {
                 'parent': 'article',
@@ -323,6 +285,7 @@ class AuthorNotesTest(TestCase):
                         'fn_parent': 'author-notes',
                         'fn_text': '1Os autores declaram não haver conflito de interesses.',
                         'fn_type': 'conflict',
+                        'fn_has_bold': False
                     },
                 ]
             },
@@ -340,14 +303,16 @@ class AuthorNotesTest(TestCase):
                         'fn_label': None,
                         'fn_parent': 'author-notes',
                         'fn_text': 'Não há conflito de interesse entre os autores do artigo.',
-                        'fn_type': 'coi-statement'
+                        'fn_type': 'coi-statement',
+                        'fn_has_bold': False
                     },
                     {
                         'fn_id': None,
                         'fn_label': None,
                         'fn_parent': 'author-notes',
                         'fn_text': 'Todos os autores tiveram contribuição igualitária na criação do artigo.',
-                        'fn_type': 'equal'
+                        'fn_type': 'equal',
+                        'fn_has_bold': False
                     }
                 ]
             }
@@ -394,6 +359,7 @@ class ArticleNotesTest(TestCase):
             '<fn-group>'
             '<title>Highlights: </title>'
             '<fn fn-type="other" id="fn2">'
+            '<bold>A)</bold>'
             '<p>Study presents design and production of an LED lamp for photovoltaic light traps.</p>'
             '</fn>'
             '<fn fn-type="other" id="fn3">'
@@ -417,13 +383,16 @@ class ArticleNotesTest(TestCase):
                 'parent_article_type': 'research-article',
                 'parent_id': None,
                 'parent_lang': 'pt',
+                'label': None,
+                'title': 'Highlights: ',
                 'fns': [
                     {
                         'fn_id': 'fn2',
-                        'fn_text': 'Study presents design and production of an LED lamp for photovoltaic light traps.',
+                        'fn_text': 'A)Study presents design and production of an LED lamp for photovoltaic light traps.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': True
                     },
                     {
                         'fn_id': 'fn3',
@@ -431,7 +400,8 @@ class ArticleNotesTest(TestCase):
                                    'the operating status of the system.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': False
                     },
                     {
                         'fn_id': 'fn4',
@@ -439,7 +409,8 @@ class ArticleNotesTest(TestCase):
                                    'conventional light traps.',
                         'fn_label': None,
                         'fn_type': 'other',
-                        'fn_parent': 'fn-group'
+                        'fn_parent': 'fn-group',
+                        'fn_has_bold': False
                     }
                 ]
             },
@@ -460,6 +431,7 @@ class ArticleNotesTest(TestCase):
                         'fn_parent': 'author-notes',
                         'fn_text': '1Os autores declaram não haver conflito de interesses.',
                         'fn_type': 'conflict',
+                        'fn_has_bold': False
                     },
                 ]
             },
@@ -477,14 +449,16 @@ class ArticleNotesTest(TestCase):
                         'fn_label': None,
                         'fn_parent': 'author-notes',
                         'fn_text': 'Não há conflito de interesse entre os autores do artigo.',
-                        'fn_type': 'coi-statement'
+                        'fn_type': 'coi-statement',
+                        'fn_has_bold': False
                     },
                     {
                         'fn_id': None,
                         'fn_label': None,
                         'fn_parent': 'author-notes',
                         'fn_text': 'Todos os autores tiveram contribuição igualitária na criação do artigo.',
-                        'fn_type': 'equal'
+                        'fn_type': 'equal',
+                        'fn_has_bold': False
                     }
                 ]
             }
