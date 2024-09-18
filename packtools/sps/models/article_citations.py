26b25e5b4ec2434b37e2fb0ffa2fd57221a30244
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-09-18 11:57:17 -0300

    Adiciona 'ArticleReference'

--- packtools/sps/models/article_citations.py
+++ packtools/sps/models/article_citations.py
@@ -1,112 +1,80 @@
 from packtools.sps.utils.xml_utils import get_parent_context, put_parent_context, node_plain_text, process_subtags
 
 
-def get_label(node):
-    text = node_plain_text(node.find("./label"))
-    if text is not None and text.endswith("."):
-        text = text[:-1]
-    return text
+class ArticleReference:
+    def __init__(self, ref):
+        self.ref = ref
 
+    def get_label(self):
+        text = node_plain_text(self.ref.find("./label"))
+        if text is not None and text.endswith("."):
+            text = text[:-1]
+        return text
 
-def get_publication_type(node):
-    return node.find("./element-citation").get("publication-type")
+    def get_publication_type(self):
+        return self.ref.find("./element-citation").get("publication-type")
 
+    def get_source(self):
+        return node_plain_text(self.ref.find("./element-citation/source"))
 
-def get_source(node):
-    return node_plain_text(node.find("./element-citation/source"))
+    def get_main_author(self):
+        try:
+            return self.get_all_authors()[0]
+        except IndexError:
+            return
 
+    def get_all_authors(self):
+        tags = ["surname", "given-names", "prefix", "suffix"]
+        result = []
+        authors = self.ref.xpath("./element-citation/person-group//name")
+        for author in authors:
+            d = {}
+            for tag in tags:
+                if text := node_plain_text(author.find(tag)):
+                    d[tag] = text
+            result.append(d)
+        if collab := self.get_collab():
+            result.append({"collab": collab})
 
-def get_main_author(node):
-    try:
-        return get_all_authors(node)[0]
-    except IndexError:
-        return
+        return result
 
+    def get_collab(self):
+        collabs = self.ref.xpath("./element-citation/person-group//collab")
+        return [node_plain_text(collab) for collab in collabs]
 
-def get_all_authors(node):
-    tags = ["surname", "given-names", "prefix", "suffix"]
-    result = []
-    authors = node.xpath("./element-citation/person-group//name")
-    for author in authors:
-        d = {}
-        for tag in tags:
-            if text := node_plain_text(author.find(tag)):
-                d[tag] = text
-        result.append(d)
-    if collab := get_collab(node):
-        result.append({"collab": collab})
+    def get_volume(self):
+        return node_plain_text(self.ref.find("./element-citation/volume"))
 
-    return result
+    def get_issue(self):
+        return node_plain_text(self.ref.find("./element-citation/issue"))
 
+    def get_fpage(self):
+        return node_plain_text(self.ref.find("./element-citation/fpage"))
 
-def get_collab(node):
-    collabs = node.xpath("./element-citation/person-group//collab")
-    return [node_plain_text(collab) for collab in collabs]
+    def get_lpage(self):
+        return node_plain_text(self.ref.find("./element-citation/lpage"))
 
+    def get_year(self):
+        return node_plain_text(self.ref.find("./element-citation/year"))
 
-def get_volume(node):
-    return node_plain_text(node.find("./element-citation/volume"))
+    def get_article_title(self):
+        return node_plain_text(self.ref.find("./element-citation/article-title"))
 
+    def get_mixed_citation(self):
+        return node_plain_text(self.ref.find("./mixed-citation"))
 
-def get_issue(node):
-    return node_plain_text(node.find("./element-citation/issue"))
+    def get_citation_ids(self):
+        ids = {}
+        for pub_id in self.ref.xpath(".//pub-id"):
+            ids[pub_id.attrib["pub-id-type"]] = node_plain_text(pub_id)
+        return ids
 
+    def get_elocation_id(self):
+        return node_plain_text(self.ref.find("./element-citation/elocation-id"))
 
-def get_fpage(node):
-    return node_plain_text(node.find("./element-citation/fpage"))
+    def get_ref_id(self):
+        return self.ref.get("id")
 
-
-def get_lpage(node):
-    return node_plain_text(node.find("./element-citation/lpage"))
-
-
-def get_year(node):
-    return node_plain_text(node.find("./element-citation/year"))
-
-
-def get_article_title(node):
-    return node_plain_text(node.find("./element-citation/article-title"))
-
-
-def get_mixed_citation(node):
-    return node_plain_text(node.find("./mixed-citation"))
-
-
-def get_citation_ids(node):
-    ids = {}
-    for pub_id in node.xpath(".//pub-id"):
-        ids[pub_id.attrib["pub-id-type"]] = node_plain_text(pub_id)
-    return ids
-
-
-def get_elocation_id(node):
-    return node_plain_text(node.find("./element-citation/elocation-id"))
-
-
-def get_ref_id(node):
-    return node.get("id")
-
-def get_comment_text(node):
-    # localizar elementos <comment> que contêm <ext-link>
-    comments = node.xpath("//element-citation/comment[ext-link]")
-    results = []
-    for comment in comments:
-        # texto completo do <comment>
-        comment_text = comment.text
-
-        # extrai o primeiro elemento <ext-link> dentro do <comment>
-        ext_link = comment.find("ext-link")
-
-        # verifica se há um texto antes do <ext-link> e extrai esse texto
-        if comment_text and ext_link is not None:
-            text_between = comment_text.strip()
-            results.append({
-                'full_comment': process_subtags(comment),
-                'text_between': text_between,
-                'ext_link_text': process_subtags(ext_link)
-            })
-
-    return results
 
 
 class ArticleCitations:
@@ -119,24 +87,26 @@ class ArticleCitations:
         for node, lang, article_type, parent, parent_id in get_parent_context(
             self.xmltree
         ):
-            for item in node.xpath("./ref-list//ref"):
+            for item in node.xpath(".//ref-list/ref"):
+                ref = ArticleReference(item)
                 tags = [
-                    ("ref_id", get_ref_id(item)),
-                    ("label", get_label(item)),
-                    ("publication_type", get_publication_type(item)),
-                    ("source", get_source(item)),
-                    ("main_author", get_main_author(item)),
-                    ("all_authors", get_all_authors(item)),
-                    ("volume", get_volume(item)),
-                    ("issue", get_issue(item)),
-                    ("fpage", get_fpage(item)),
-                    ("lpage", get_lpage(item)),
-                    ("elocation_id", get_elocation_id(item)),
-                    ("year", get_year(item)),
-                    ("article_title", get_article_title(item)),
-                    ("citation_ids", get_citation_ids(item)),
-                    ("mixed_citation", get_mixed_citation(item)),
-                    ("comment_text", get_comment_text(item))
+                    ("ref_id", ref.get_ref_id()),
+                    ("label", ref.get_label()),
+                    ("publication_type", ref.get_publication_type()),
+                    ("source", ref.get_source()),
+                    ("main_author", ref.get_main_author()),
+                    ("all_authors", ref.get_all_authors()),
+                    ("volume", ref.get_volume()),
+                    ("issue", ref.get_issue()),
+                    ("fpage", ref.get_fpage()),
+                    ("lpage", ref.get_lpage()),
+                    ("elocation_id", ref.get_elocation_id()),
+                    ("year", ref.get_year()),
+                    ("article_title", ref.get_article_title()),
+                    ("citation_ids", ref.get_citation_ids()),
+                    ("mixed_citation", ref.get_mixed_citation()),
+                    ("comment_text", ref.get_extlink_and_comment_content()),
+                    ("text_before_extlink", ref.get_text_before_extlink())
                 ]
                 d = dict()
                 for name, value in tags:
@@ -145,5 +115,5 @@ class ArticleCitations:
                             d[name] = value.text
                         except AttributeError:
                             d[name] = value
-                d["author_type"] = "institutional" if get_collab(item) else "person"
+                d["author_type"] = "institutional" if ref.get_collab() else "person"
                 yield put_parent_context(d, lang, article_type, parent, parent_id)
