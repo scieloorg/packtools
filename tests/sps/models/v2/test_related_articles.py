626fdd3628e617bfa6f7641e518ac8baf3a72710
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-09 11:28:01 -0300

    Aplica formatação

--- tests/sps/models/v2/test_related_articles.py
+++ tests/sps/models/v2/test_related_articles.py
@@ -11,11 +11,16 @@ de Foucault.
 from unittest import TestCase
 
 from packtools.sps.utils import xml_utils
-from packtools.sps.models.v2.related_articles import RelatedArticle, RelatedArticlesByNode, RelatedArticles
+from packtools.sps.models.v2.related_articles import (
+    RelatedArticle,
+    RelatedArticlesParent,
+    RelatedArticles,
+)
 
 
 class RelatedArticleTest(TestCase):
     def test_related_article(self):
+        self.maxDiff = None
         xml = """
             <article xmlns:xlink="http://www.w3.org/1999/xlink">
             <front>
@@ -29,21 +34,35 @@ class RelatedArticleTest(TestCase):
                 </front>
             </article>
             """
-        obtained = RelatedArticle(xml_utils.get_xml_tree(xml).xpath(".//related-article")[0]).data()
+        obtained = RelatedArticle(
+            xml_utils.get_xml_tree(xml).xpath(".//related-article")[0]
+        ).data()
         expected = {
             "ext-link-type": "doi",
             "id": "A01",
             "related-article-type": "commentary-article",
             "href": "10.1590/0101-3173.2022.v45n1.p139",
             "text": "Referência do artigo comentado: FREITAS, J. H. de. Cinismo e indiferenciación: la huella de "
-                    "Glucksmann en <i>El coraje de la verdad</i> de Foucault. Trans/form/ação : revista de Filosofia "
-                    "da Unesp, v. 45, n. 1, p. 139-158, 2022."
+            "Glucksmann en <i>El coraje de la verdad</i> de Foucault. Trans/form/ação : revista de Filosofia "
+            "da Unesp, v. 45, n. 1, p. 139-158, 2022.",
+            "full_tag": '<related-article ext-link-type="doi" id="A01" '
+            'related-article-type="commentary-article" '
+            'xlink:href="10.1590/0101-3173.2022.v45n1.p139">\n'
+            "                Referência do artigo comentado: FREITAS, J. H. "
+            "de. Cinismo e indiferenciación: la huella de Glucksmann en\n"
+            "                <italic>El coraje de la verdad</italic>\n"
+            "                de Foucault.\n"
+            "                <bold>Trans/form/ação</bold>\n"
+            "                : revista de Filosofia da Unesp, v. 45, n. 1, p. "
+            "139-158, 2022.\n"
+            "                </related-article>",
         }
         self.assertDictEqual(obtained, expected)
 
 
 class RelatedArticlesByNodeTest(TestCase):
     def test_related_articles(self):
+        self.maxDiff = None
         xml = """
             <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink" 
             article-type="article-commentary" dtd-version="1.1" specific-use="sps-1.9" xml:lang="pt">
@@ -60,7 +79,11 @@ class RelatedArticlesByNodeTest(TestCase):
             </front>
             </article>
             """
-        obtained = list(RelatedArticlesByNode(xml_utils.get_xml_tree(xml).find(".")).related_articles())
+        obtained = list(
+            RelatedArticlesParent(
+                xml_utils.get_xml_tree(xml).find(".")
+            ).related_articles()
+        )
         expected = [
             {
                 "parent": "article",
@@ -72,8 +95,19 @@ class RelatedArticlesByNodeTest(TestCase):
                 "related-article-type": "commentary-article",
                 "href": "10.1590/0101-3173.2022.v45n1.p139",
                 "text": "Referência do artigo comentado: FREITAS, J. H. de. Cinismo e indiferenciación: la huella de "
-                        "Glucksmann en <i>El coraje de la verdad</i> de Foucault. Trans/form/ação : revista de Filosofia "
-                        "da Unesp, v. 45, n. 1, p. 139-158, 2022."
+                "Glucksmann en <i>El coraje de la verdad</i> de Foucault. Trans/form/ação : revista de Filosofia "
+                "da Unesp, v. 45, n. 1, p. 139-158, 2022.",
+                "full_tag": '<related-article ext-link-type="doi" id="A01" '
+                'related-article-type="commentary-article" '
+                'xlink:href="10.1590/0101-3173.2022.v45n1.p139">\n'
+                "            Referência do artigo comentado: FREITAS, J. H. de. "
+                "Cinismo e indiferenciación: la huella de Glucksmann en\n"
+                "            <italic>El coraje de la verdad</italic>\n"
+                "            de Foucault.\n"
+                "            <bold>Trans/form/ação</bold>\n"
+                "            : revista de Filosofia da Unesp, v. 45, n. 1, p. "
+                "139-158, 2022.\n"
+                "            </related-article>",
             }
         ]
         self.assertEqual(len(obtained), 1)
@@ -84,38 +118,39 @@ class RelatedArticlesByNodeTest(TestCase):
 
 class RelatedArticlesTest(TestCase):
     def setUp(self):
-        self.xml_tree = xml_utils.get_xml_tree("tests/fixtures/htmlgenerator/related-article/varias_erratas.xml")
+        self.xml_tree = xml_utils.get_xml_tree(
+            "tests/fixtures/htmlgenerator/related-article/varias_erratas.xml"
+        )
 
     def test_article(self):
         obtained = list(RelatedArticles(self.xml_tree).article())
         expected = [
             {
-                'ext-link-type': 'doi',
-                'href': '10.1590/s1413-65382620000100001',
-                'id': 'RA1',
-                'parent': 'article',
-                'parent_article_type': 'correction',
-                'parent_id': None,
-                'parent_lang': 'pt',
-                'related-article-type': 'corrected-article',
-                'text': ''
+                "ext-link-type": "doi",
+                "href": "10.1590/s1413-65382620000100001",
+                "id": "RA1",
+                "parent": "article",
+                "parent_article_type": "correction",
+                "parent_id": None,
+                "parent_lang": "pt",
+                "related-article-type": "corrected-article",
+                "text": "",
                 "full_tag": '<related-article id="RA1" related-article-type="corrected-article" ext-link-type="doi" '
                 'xlink:href="10.1590/s1413-65382620000100001"/>',
             },
             {
-                'ext-link-type': 'doi',
-                'href': '10.1590/s1413-65382620000100009',
-                'id': 'RA9',
-                'parent': 'article',
-                'parent_article_type': 'correction',
-                'parent_id': None,
-                'parent_lang': 'pt',
-                'related-article-type': 'corrected-article',
-                'text': ''
+                "ext-link-type": "doi",
+                "href": "10.1590/s1413-65382620000100009",
+                "id": "RA9",
+                "parent": "article",
+                "parent_article_type": "correction",
+                "parent_id": None,
+                "parent_lang": "pt",
+                "related-article-type": "corrected-article",
+                "text": "",
                 "full_tag": '<related-article id="RA9" related-article-type="corrected-article" ext-link-type="doi" '
                 'xlink:href="10.1590/s1413-65382620000100009"/>',
             },
-
         ]
         self.assertEqual(len(obtained), 9)
         self.assertDictEqual(obtained[0], expected[0])
@@ -125,32 +160,31 @@ class RelatedArticlesTest(TestCase):
         obtained = list(RelatedArticles(self.xml_tree).sub_articles())
         expected = [
             {
-                'ext-link-type': 'doi',
-                'href': '10.1590/s1413-65382620000100001',
-                'id': 'RA10',
-                'parent': 'sub-article',
-                'parent_article_type': 'translation',
-                'parent_id': "s1",
-                'parent_lang': 'en',
-                'related-article-type': 'corrected-article',
-                'text': ''
+                "ext-link-type": "doi",
+                "href": "10.1590/s1413-65382620000100001",
+                "id": "RA10",
+                "parent": "sub-article",
+                "parent_article_type": "translation",
+                "parent_id": "s1",
+                "parent_lang": "en",
+                "related-article-type": "corrected-article",
+                "text": "",
                 "full_tag": '<related-article id="RA10" related-article-type="corrected-article" ext-link-type="doi" '
                 'xlink:href="10.1590/s1413-65382620000100001"/>',
             },
             {
-                'ext-link-type': 'doi',
-                'href': '10.1590/s1413-65382620000100009',
-                'id': 'RA18',
-                'parent': 'sub-article',
-                'parent_article_type': 'translation',
-                'parent_id': "s1",
-                'parent_lang': 'en',
-                'related-article-type': 'corrected-article',
-                'text': ''
+                "ext-link-type": "doi",
+                "href": "10.1590/s1413-65382620000100009",
+                "id": "RA18",
+                "parent": "sub-article",
+                "parent_article_type": "translation",
+                "parent_id": "s1",
+                "parent_lang": "en",
+                "related-article-type": "corrected-article",
+                "text": "",
                 "full_tag": '<related-article id="RA18" related-article-type="corrected-article" ext-link-type="doi" '
                 'xlink:href="10.1590/s1413-65382620000100009"/>',
             },
-
         ]
         self.assertEqual(len(obtained), 9)
         self.assertDictEqual(obtained[0], expected[0])
@@ -173,22 +207,25 @@ class RelatedArticlesTest(TestCase):
             </front>
             </article>
             """
-        obtained = list(RelatedArticles(xml_utils.get_xml_tree(xml), related_article_type="commentary-article").related_articles())
+        obtained = list(
+            RelatedArticles(
+                xml_utils.get_xml_tree(xml), related_article_type="commentary-article"
+            ).related_articles()
+        )
         expected = [
             {
-                'ext-link-type': 'doi',
-                'href': '10.1590/0101-3173.2022.v45n1.p139',
-                'id': 'A01',
-                'parent': 'article',
-                'parent_article_type': 'article-commentary',
-                'parent_id': None,
-                'parent_lang': 'pt',
-                'related-article-type': 'commentary-article',
-                'text': ''
+                "ext-link-type": "doi",
+                "href": "10.1590/0101-3173.2022.v45n1.p139",
+                "id": "A01",
+                "parent": "article",
+                "parent_article_type": "article-commentary",
+                "parent_id": None,
+                "parent_lang": "pt",
+                "related-article-type": "commentary-article",
+                "text": "",
                 "full_tag": '<related-article ext-link-type="doi" id="A01" related-article-type="commentary-article" '
                 'xlink:href="10.1590/0101-3173.2022.v45n1.p139"/>',
             },
-
         ]
         self.assertEqual(len(obtained), 1)
         self.assertDictEqual(obtained[0], expected[0])
