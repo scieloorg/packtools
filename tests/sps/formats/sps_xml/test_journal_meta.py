1579e95becebc22defbfa129d4c62b078c4b9546
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-17 11:21:21 -0300

    Refatora 'journal-meta'

--- tests/sps/formats/sps_xml/test_journal_meta.py
+++ tests/sps/formats/sps_xml/test_journal_meta.py
@@ -20,91 +20,49 @@ class TestBuildJournalMeta(unittest.TestCase):
             },
             "publisher_name": "Associação Brasileira de Divulgação Científica"
         }
+        self.journal_meta_elem = build_journal_meta(self.data)
 
     def test_journal_meta_structure(self):
-        # Gera o XML a partir dos dados
-        journal_meta_elem = build_journal_meta(self.data)
-
         # Verifica se o elemento principal é <journal-meta>
-        self.assertEqual(journal_meta_elem.tag, "journal-meta")
+        self.assertEqual(self.journal_meta_elem.tag, "journal-meta")
 
-        # Verifica se <journal-id> elementos foram criados corretamente
-        journal_ids = journal_meta_elem.findall("journal-id")
+    def test_journal_meta_journal_ids(self):
+        journal_ids = self.journal_meta_elem.findall("journal-id")
         self.assertEqual(len(journal_ids), 2)
         self.assertEqual(journal_ids[0].get("journal-id-type"), "nlm-ta")
         self.assertEqual(journal_ids[0].text, "Braz J Med Biol Res")
         self.assertEqual(journal_ids[1].get("journal-id-type"), "publisher-id")
         self.assertEqual(journal_ids[1].text, "bjmbr")
 
-        # Verifica o grupo de títulos <journal-title-group>
-        journal_title_group = journal_meta_elem.find("journal-title-group")
+    def test_journal_meta_titles(self):
+        journal_title_group = self.journal_meta_elem.find("journal-title-group")
         self.assertIsNotNone(journal_title_group)
 
-        # Verifica se <journal-title> foi criado corretamente
         journal_title = journal_title_group.find("journal-title")
         self.assertEqual(journal_title.text, "Brazilian Journal of Medical and Biological Research")
 
-        # Verifica o título abreviado <abbrev-journal-title>
         abbrev_title = journal_title_group.find("abbrev-journal-title")
         self.assertEqual(abbrev_title.text, "Braz. J. Med. Biol. Res.")
         self.assertEqual(abbrev_title.get("abbrev-type"), "publisher")
 
-        # Verifica os elementos <issn>
-        issns = journal_meta_elem.findall("issn")
+    def test_journal_meta_issn(self):
+        issns = self.journal_meta_elem.findall("issn")
         self.assertEqual(len(issns), 2)
         self.assertEqual(issns[0].get("pub-type"), "epub")
         self.assertEqual(issns[0].text, "1414-431X")
         self.assertEqual(issns[1].get("pub-type"), "ppub")
         self.assertEqual(issns[1].text, "0100-879X")
 
-        # Verifica o elemento <publisher> e <publisher-name>
-        publisher = journal_meta_elem.find("publisher")
+    def test_journal_meta_publisher(self):
+        publisher = self.journal_meta_elem.find("publisher")
         self.assertIsNotNone(publisher)
 
         publisher_name = publisher.find("publisher-name")
         self.assertEqual(publisher_name.text, "Associação Brasileira de Divulgação Científica")
 
-    def test_journal_meta_empty_data(self):
-        # Testa com dados vazios
-        empty_data = {
-            "journal_ids": {},
-            "journal_title": "",
-            "abbrev_journal_title": "",
-            "issn": {},
-            "publisher_name": ""
-        }
-        journal_meta_elem = build_journal_meta(empty_data)
-
-        # Verifica se os elementos principais são criados, mas sem conteúdo
-        self.assertEqual(journal_meta_elem.tag, "journal-meta")
-        self.assertEqual(len(journal_meta_elem.findall("journal-id")), 0)
-        self.assertEqual(len(journal_meta_elem.findall("issn")), 0)
-
-        # Verifica se o <journal-title-group> está presente, mas vazio
-        journal_title_group = journal_meta_elem.find("journal-title-group")
-        self.assertIsNotNone(journal_title_group)
-
-        journal_title = journal_title_group.find("journal-title")
-        self.assertEqual(journal_title.text, "")
-
-        abbrev_title = journal_title_group.find("abbrev-journal-title")
-        self.assertEqual(abbrev_title.text, "")
-        self.assertEqual(abbrev_title.get("abbrev-type"), "publisher")
-
-        # Verifica se o <publisher> foi criado, mas sem nome
-        publisher = journal_meta_elem.find("publisher")
-        self.assertIsNotNone(publisher)
-
-        publisher_name = publisher.find("publisher-name")
-        self.assertEqual(publisher_name.text, "")
-
     def test_journal_meta_as_string(self):
-        self.maxDiff = None
-        # Gera o XML a partir dos dados
-        journal_meta_elem = build_journal_meta(self.data)
-
-        # Converte o elemento XML em uma string
-        generated_xml_str = ET.tostring(journal_meta_elem, encoding="unicode", method="xml")
+        # Gera o XML a partir dos dados válidos
+        generated_xml_str = ET.tostring(self.journal_meta_elem, encoding="unicode", method="xml")
 
         # String XML esperada
         expected_xml_str = (
@@ -124,7 +82,76 @@ class TestBuildJournalMeta(unittest.TestCase):
         )
 
         # Comparar o XML gerado com a string esperada
-        self.assertEqual(generated_xml_str, expected_xml_str)
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+
+class TestBuildJournalMetaNoneValues(unittest.TestCase):
+
+    def setUp(self):
+        self.empty_data = {
+            "journal_ids": {
+                "nlm-ta": None,
+            },
+            "journal_title": "",
+            "abbrev_journal_title": "",
+            "issn": {},
+            "publisher_name": ""
+        }
+
+    def _build_and_compare(self, expected_xml_str):
+        """Método auxiliar para construção e comparação do XML gerado"""
+        journal_meta_elem = build_journal_meta(self.empty_data)
+        generated_xml_str = ET.tostring(journal_meta_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_journal_meta_empty_data(self):
+        expected_xml_str = (
+            '<journal-meta>'
+            '<journal-id journal-id-type="nlm-ta" />'
+            '<journal-title-group>'
+            '<journal-title />'
+            '<abbrev-journal-title abbrev-type="publisher" />'
+            '</journal-title-group>'
+            '<publisher>'
+            '<publisher-name />'
+            '</publisher>'
+            '</journal-meta>'
+        )
+        self._build_and_compare(expected_xml_str)
+
+    def test_journal_meta_without_journal_ids(self):
+        self.maxDiff = None
+        # Exclui journal_ids para testar a ausência
+        self.empty_data.pop("journal_ids")
+        expected_xml_str = (
+            '<journal-meta>'
+            '<journal-title-group>'
+            '<journal-title />'
+            '<abbrev-journal-title abbrev-type="publisher" />'
+            '</journal-title-group>'
+            '<publisher>'
+            '<publisher-name />'
+            '</publisher>'
+            '</journal-meta>'
+        )
+        self._build_and_compare(expected_xml_str)
+
+    def test_journal_meta_without_issn(self):
+        # Exclui issn para testar a ausência
+        self.empty_data.pop("issn")
+        expected_xml_str = (
+            '<journal-meta>'
+            '<journal-id journal-id-type="nlm-ta" />'
+            '<journal-title-group>'
+            '<journal-title />'
+            '<abbrev-journal-title abbrev-type="publisher" />'
+            '</journal-title-group>'
+            '<publisher>'
+            '<publisher-name />'
+            '</publisher>'
+            '</journal-meta>'
+        )
+        self._build_and_compare(expected_xml_str)
 
 
 if __name__ == '__main__':
