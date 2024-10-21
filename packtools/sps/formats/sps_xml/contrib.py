39c382e909e231200b58b71cc24531ebf8955352
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-21 12:08:24 -0300

    Refatora 'build_contrib_author'

--- packtools/sps/formats/sps_xml/contrib.py
+++ packtools/sps/formats/sps_xml/contrib.py
@@ -1,10 +1,8 @@
 """
 data = {
     "contrib_type": "author",
-    "contrib_ids": {
-        "orcid": "0000-0001-8528-2091",
-        "scopus": "24771926600"
-    },
+    "orcid": "0000-0001-8528-2091",
+    "scopus": "24771926600",
     "surname": "Einstein",
     "given_names": "Albert",
     "affiliations": [
@@ -19,68 +17,61 @@ import xml.etree.ElementTree as ET
 def build_contrib_author(data):
     # Garantir que o contrib_type seja uma string válida
     contrib_type = data.get("contrib_type")
-    if contrib_type is None:
-        raise KeyError("contrib-type is required")
+    if not contrib_type:
+        raise NameError("contrib-type is required")
 
     # Criação do elemento principal <contrib> com o tipo de contribuição
     contrib_elem = ET.Element("contrib", attrib={"contrib-type": contrib_type})
 
-    # Adiciona os elementos <contrib-id> para ORCID e Scopus, se existirem no dicionário
-    if 'contrib_ids' in data:
-        if data.get('contrib_ids') is None:
-            raise KeyError("contrib-id-type is required")
-        else:
-            for contrib_id_type, contrib_id_value in (data['contrib_ids'] or {}).items():
-                contrib_id_elem = ET.Element("contrib-id", attrib={"contrib-id-type": contrib_id_type})
-                contrib_id_elem.text = contrib_id_value
-                contrib_elem.append(contrib_id_elem)
+    for contrib_id_type in ("lattes", "orcid", "researchid", "scopus"):
+        contrib_id_value = data.get(contrib_id_type)
+        if contrib_id_value:
+            contrib_id_elem = ET.Element("contrib-id", attrib={"contrib-id-type": contrib_id_type})
+            contrib_id_elem.text = contrib_id_value
+            contrib_elem.append(contrib_id_elem)
 
-    if "collab" in data:
+    if data.get("collab"):
         collab_elem = ET.Element("collab")
         collab_elem.text = data["collab"]
         contrib_elem.append(collab_elem)
 
     # Cria e adiciona o <name> com <surname> e <given-names> somente se existirem no dicionário
-    if any(key in data for key in ("surname", "given_names", "prefix", "suffix")):
+
+    if any(data.get(key) for key in ("surname", "given_names", "prefix", "suffix")):
         name_elem = ET.Element("name")
 
-        if "surname" in data:  # Adiciona <surname> somente se estiver presente no dicionário
+        if data.get("surname"):  # Adiciona <surname> somente se estiver presente no dicionário
             surname_elem = ET.Element("surname")
-            surname_elem.text = data["surname"]  # Mesmo se for vazio, cria o elemento
+            surname_elem.text = data["surname"]
             name_elem.append(surname_elem)
 
-        if "given_names" in data:  # Adiciona <given-names> somente se estiver presente no dicionário
+        if data.get("given_names"):  # Adiciona <given-names> somente se estiver presente no dicionário
             given_names_elem = ET.Element("given-names")
-            given_names_elem.text = data["given_names"]  # Mesmo se for vazio, cria o elemento
+            given_names_elem.text = data["given_names"]
             name_elem.append(given_names_elem)
 
-        if "prefix" in data:  # Adiciona <prefix> somente se estiver presente no dicionário
+        if data.get("prefix"):  # Adiciona <prefix> somente se estiver presente no dicionário
             prefix_elem = ET.Element("prefix")
-            prefix_elem.text = data["prefix"]  # Mesmo se for vazio, cria o elemento
+            prefix_elem.text = data["prefix"]
             name_elem.append(prefix_elem)
 
-        if "suffix" in data:  # Adiciona <suffix> somente se estiver presente no dicionário
+        if data.get("suffix"):  # Adiciona <suffix> somente se estiver presente no dicionário
             suffix_elem = ET.Element("suffix")
-            suffix_elem.text = data["suffix"]  # Mesmo se for vazio, cria o elemento
+            suffix_elem.text = data["suffix"]
             name_elem.append(suffix_elem)
 
         contrib_elem.append(name_elem)
 
     # Adiciona o <xref> para afiliação apenas se rid ou text forem fornecidos no dicionário
-    if "affiliations" in data:
-        if data.get("affiliations") is None:
-            xref_elem = ET.Element("xref")
-            contrib_elem.append(xref_elem)
-        else:
-            for xref_data in data.get("affiliations") or []:
-                # rid é obrigatório, a ausência da chave deveria levantar exceção
-                try:
-                    xref_elem = ET.Element("xref", attrib={"ref-type": "aff", "rid": xref_data["rid"]})
-                    xref_elem.text = xref_data["text"]
-                except KeyError as e:
-                    raise KeyError(f"{e} is required")
+    for xref_data in data.get("affiliations") or []:
+        # rid é obrigatório, a ausência da chave deveria levantar exceção
+        try:
+            xref_elem = ET.Element("xref", attrib={"ref-type": "aff", "rid": xref_data["rid"]})
+            xref_elem.text = xref_data["text"]
+        except KeyError as e:
+            raise KeyError(f"{e} is required")
 
-                contrib_elem.append(xref_elem)
+        contrib_elem.append(xref_elem)
 
     return contrib_elem
 
