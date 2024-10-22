b4a44b3ca1f8143802a72ba5d145a2f50aff2b0f
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-22 12:05:44 -0300

    Adiciona "build_aff"

---
+++ packtools/sps/formats/sps_xml/aff.py
@@ -0,0 +1,66 @@
+"""
+data = {
+    "aff_id": "aff01", (obrigatório)
+    "label": "1",
+    "orgname": "Fundação Oswaldo Cruz",
+    "orgdiv1": "Escola Nacional de Saúde Pública Sérgio Arouca",
+    "orgdiv2": "Centro de Estudos da Saúde do Trabalhador e Ecologia Humana",
+    "original": "Prof. da Fundação Oswaldo Cruz",
+    "country": {
+        (code: name)
+        "BR": "Brazil",
+    },
+    "city": "Manguinhos",
+    "state": "RJ",
+    "email": "maurosilva@foo.com"
+}
+"""
+
+import xml.etree.ElementTree as ET
+
+
+def build_aff(data):
+    aff_id = data.get("aff_id")
+    if aff_id:
+        aff_elem = ET.Element("aff", attrib={"id": aff_id})
+        for content_type in ("orgname", "orgdiv1", "orgdiv2", "original"):
+            instituion_text = data.get(content_type)
+            if instituion_text:
+                instituion_elem = ET.Element("institution", attrib={"content-type": content_type})
+                instituion_elem.text = instituion_text
+                aff_elem.append(instituion_elem)
+
+        label = data.get("label")
+        if label:
+            label_elem = ET.Element("label")
+            label_elem.text = label
+            aff_elem.append(label_elem)
+
+        city = data.get("city")
+        state = data.get("state")
+        if city or state:
+            addr_line_elem = ET.Element("addr-line")
+            if city:
+                city_elem = ET.Element("city")
+                city_elem.text = city
+                addr_line_elem.append(city_elem)
+            if state:
+                state_elem = ET.Element("state")
+                state_elem.text = state
+                addr_line_elem.append(state_elem)
+            aff_elem.append(addr_line_elem)
+
+        country_dict = data.get("country")
+        if country_dict and isinstance(country_dict, dict):
+            for code, name in country_dict.items():
+                country_elem = ET.Element("country", attrib={"country": code})
+                country_elem.text = name
+                aff_elem.append(country_elem)
+
+        email = data.get("email")
+        if email:
+            email_elem = ET.Element("email")
+            email_elem.text = email
+            aff_elem.append(email_elem)
+
+        return aff_elem
