a3a972e3191a765470dd667b857885af7940aa48
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-31 11:40:13 -0300

    Verifica a existência de atributo obrigatório

--- packtools/sps/formats/sps_xml/pub_date.py
+++ packtools/sps/formats/sps_xml/pub_date.py
@@ -17,11 +17,7 @@ import xml.etree.ElementTree as ET
 
 def build_pub_dates(data):
     for date_type, events_dict in data.items():
-        if isinstance(events_dict, dict) and any(events_dict.get(key) for key in ("day", "month", "year", "season")):
-            pub_date_elem = ET.Element("pub-date", attrib={"publication-format": "electronic", "date-type": date_type})
-            for event, event_text in events_dict.items():
-                if event_text:
-                    event_elem = ET.Element(event)
-                    event_elem.text = event_text
-                    pub_date_elem.append(event_elem)
-            yield pub_date_elem
+        # (((day?, month?) | season)?, year, era?)
+        # year is required
+        if not events_dict.get("year"):
+            raise ValueError("year is required")
