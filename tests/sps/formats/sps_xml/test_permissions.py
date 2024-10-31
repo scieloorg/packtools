4fdcbd957b740a39c32beb5cb585a8470b172f44
Luciano <41302084+Rossi-Luciano@users.noreply.github.com>
2024-10-31 09:40:24 -0300

    Feat: build fn group element (#737)
    * Adiciona 'full_tag'
    
    * Revert "Adiciona 'full_tag'"
    
    This reverts commit 1d0129c81e7dbcbb8e9c8dda27f2c6d3ff0c73af.
    
    * Adiciona "build_permissions"
    
    * Adiciona testes
    
    * Adiciona "build_fn_group"
    
    * Adiciona testes

---
+++ tests/sps/formats/sps_xml/test_permissions.py
@@ -0,0 +1,132 @@
+import unittest
+import xml.etree.ElementTree as ET
+from packtools.sps.formats.sps_xml.permissions import build_permissions
+
+
+class TestBuildPermissionsCopyrightStatement(unittest.TestCase):
+    def test_build_permissions_copyright_statement(self):
+        data = {
+            "copyright-statement": "Copyright © 2014 SciELO",
+        }
+        expected_xml_str = (
+            '<permissions>'
+            '<copyright-statement>Copyright © 2014 SciELO</copyright-statement>'
+            '</permissions>'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_build_permissions_copyright_statement_None(self):
+        data = {
+            "copyright-statement": None
+        }
+        expected_xml_str = (
+            '<permissions />'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+
+class TestBuildPermissionsCopyrightYear(unittest.TestCase):
+    def test_build_permissions_copyright_year(self):
+        data = {
+            "copyright-year": "2014",
+        }
+        expected_xml_str = (
+            '<permissions>'
+            '<copyright-year>2014</copyright-year>'
+            '</permissions>'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_build_permissions_copyright_year_None(self):
+        data = {
+            "copyright-year": None
+        }
+        expected_xml_str = (
+            '<permissions />'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+
+class TestBuildPermissionsCopyrightHolder(unittest.TestCase):
+    def test_build_permissions_copyright_holder(self):
+        data = {
+            "copyright-holder": "SciELO"
+        }
+        expected_xml_str = (
+            '<permissions>'
+            '<copyright-holder>SciELO</copyright-holder>'
+            '</permissions>'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_build_permissions_copyright_holder_None(self):
+        data = {
+            "copyright-holder": None
+        }
+        expected_xml_str = (
+            '<permissions />'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+
+class TestBuildPermissionsLicenses(unittest.TestCase):
+    def test_build_permissions_licenses(self):
+        data = {
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
+        }
+        expected_xml_str = (
+            '<permissions>'
+            '<license license-type="open-access" '
+            'xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" '
+            'xml:lang="pt">This is an article published in open access under a Creative Commons license'
+            '</license>'
+            '</permissions>'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_build_permissions_licenses_None(self):
+        data = {
+            "licenses": None
+        }
+        expected_xml_str = (
+            '<permissions />'
+        )
+        permissions_elem = build_permissions(data)
+        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
+        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+
+    def test_build_permissions_licenses_required_attributes(self):
+        # vale para os demais atributos, exceto "license-p"
+        data = {
+            "licenses": [
+                {
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
+        }
+        with self.assertRaises(ValueError) as e:
+            build_permissions(data)
+        self.assertEqual(str(e.exception), "'license-type' is required")
