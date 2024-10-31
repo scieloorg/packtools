aea37b6306a3705516d8488fc4ac108a660dec1e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-31 11:40:29 -0300

    Garante a ordem dos atributos

--- packtools/sps/formats/sps_xml/pub_date.py
+++ packtools/sps/formats/sps_xml/pub_date.py
@@ -21,3 +21,12 @@ def build_pub_dates(data):
         # year is required
         if not events_dict.get("year"):
             raise ValueError("year is required")
+
+        pub_date_elem = ET.Element("pub-date", attrib={"publication-format": "electronic", "date-type": date_type})
+
+        for event in ("day", "month", "season", "year", "era"):
+            if (event_text := events_dict.get(event)) is not None:
+                event_elem = ET.Element(event)
+                event_elem.text = event_text
+                pub_date_elem.append(event_elem)
+        yield pub_date_elem
