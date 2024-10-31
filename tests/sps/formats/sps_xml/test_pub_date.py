d94334fdd24e79d968af4355ec8c54336384fa67
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-10-31 11:40:53 -0300

    Adiciona teste para a ordem dos atributos

--- tests/sps/formats/sps_xml/test_pub_date.py
+++ tests/sps/formats/sps_xml/test_pub_date.py
@@ -46,5 +46,38 @@ class TestBuildPubDate(unittest.TestCase):
             "collection": None
         }
 
-        pub_date_elem = list(build_pub_dates(data))
-        self.assertEqual(len(pub_date_elem), 0)
+        with self.assertRaises(ValueError) as e:
+            list(build_pub_dates(data))
+        self.assertEqual(str(e.exception), "year is required")
+
+    def test_build_pub_dates_order(self):
+        data = {
+            "pub": {
+                "year": "2024",
+                "month": "01",
+                "day": "01"
+            },
+            "collection": {
+                "year": "2024",
+                "season": "Jan-Fev"
+            }
+        }
+        expected_xml_str = [
+            (
+                '<pub-date publication-format="electronic" date-type="pub">'
+                '<day>01</day>'
+                '<month>01</month>'
+                '<year>2024</year>'
+                '</pub-date>'
+            ),
+            (
+                '<pub-date publication-format="electronic" date-type="collection">'
+                '<season>Jan-Fev</season>'
+                '<year>2024</year>'
+                '</pub-date>'
+            )
+        ]
+        for item, pub_date_elem in enumerate(build_pub_dates(data)):
+            generated_xml_str = ET.tostring(pub_date_elem, encoding="unicode", method="xml")
+            with self.subTest(item):
+                self.assertEqual(generated_xml_str.strip(), expected_xml_str[item].strip())
