d64c0f8d7a53d6c5ee12961d4a3394416c22189e
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-02 16:56:11 -0300

    Modifica estrutura do parâmetro da função

--- packtools/sps/formats/sps_xml/person_group.py
+++ packtools/sps/formats/sps_xml/person_group.py
@@ -1,55 +1,51 @@
 """
-data = {
-    "author": [
-        {
-            "surname": "Einstein",
-            "given_names": "Albert",
-            "prefix": "Prof.",
-            "suffix": "Neto"
-        }
-    ],
-    "collab": ["Instituto Brasil Leitor"],
-    "role": ["Pesquisador"]
-
-}
+data = [
+    {
+        "person-group-type": "author",
+        "persons": [
+            {
+                "surname": "Einstein",
+                "given-names": "Albert",
+                "prefix": "Prof.",
+                "suffix": "Neto"
+            }
+        ],
+        "collab": ["Instituto Brasil Leitor"],
+        "role": ["Pesquisador"]
+    }
+]
 """
 
 import xml.etree.ElementTree as ET
 
 
 def build_person_group(data):
-    for person_group_type in ("author", "compiler"):
-        persons = data.get(person_group_type)
+    for person_group in data or []:
+        person_group_type = person_group.get("person-group-type")
+        if not person_group_type:
+            raise ValueError("person-group-type is required")
+
+        person_group_elem = ET.Element("person-group", attrib={"person-group-type": person_group_type})
+
+        persons = person_group.get("persons")
         if persons:
-            break
-    else:
-        raise ValueError("person group type is required")
-
-    person_group_elem = ET.Element("person-group", attrib={"person-group-type": person_group_type})
-
-    if isinstance(persons, list):
-        name_elem = ET.Element("name")
-        for person in persons:
-            if isinstance(person, dict):
+            name_elem = ET.Element("name")
+            for person in persons:
                 for key, value in person.items():
                     if value:
                         elem = ET.Element(key)
                         elem.text = value
                         name_elem.append(elem)
-        person_group_elem.append(name_elem)
+            person_group_elem.append(name_elem)
 
-    collab_list = data.get("collab")
-    if isinstance(collab_list, list):
-        for collab_text in collab_list:
+        for collab_text in person_group.get("collab") or []:
             collab_elem = ET.Element("collab")
             collab_elem.text = collab_text
             person_group_elem.append(collab_elem)
 
-    role_list = data.get("role")
-    if isinstance(role_list, list):
-        for role_text in role_list:
+        for role_text in person_group.get("role") or []:
             role_elem = ET.Element("role")
             role_elem.text = role_text
             person_group_elem.append(role_elem)
 
-    return person_group_elem
+        yield person_group_elem
