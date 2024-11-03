208bb3c93690e50b335cecb6797d3b02be4a1ab2
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-03 15:22:01 -0300

    Adapta os testes

--- tests/sps/formats/sps_xml/test_kwd_group.py
+++ tests/sps/formats/sps_xml/test_kwd_group.py
@@ -6,12 +6,16 @@ from packtools.sps.formats.sps_xml.kwd_group import build_kwd_group
 class TestBuildKwdGroup(unittest.TestCase):
     def test_build_kwd_group(self):
         data = {
-            "kwd_lang": "pt",
-            "kwd_title": "Palavra-chave",
+            "kwd-lang": "pt",
+            "kwd-title": "Palavra-chave",
+            "kwds": [
+                "Broncoscopia",
+            ]
         }
         expected_xml_str = (
             '<kwd-group xml:lang="pt">'
             '<title>Palavra-chave</title>'
+            '<kwd>Broncoscopia</kwd>'
             '</kwd-group>'
         )
         kwd_group_elem = build_kwd_group(data)
@@ -20,23 +24,30 @@ class TestBuildKwdGroup(unittest.TestCase):
 
     def test_build_kwd_group_None(self):
         data = {
-            "kwd_lang": None,
-            "kwd_title": "Palavra-chave",
+            "kwd-lang": None,
+            "kwd-title": "Palavra-chave",
+            "kwds": [
+                "Broncoscopia",
+            ]
         }
         with self.assertRaises(ValueError) as e:
             build_kwd_group(data)
-        self.assertEqual(str(e.exception), "kwd lang is required")
+        self.assertEqual(str(e.exception), "kwd-lang is required")
 
 
 class TestBuildKwdGroupTitle(unittest.TestCase):
     def test_build_kwd_group_title(self):
         data = {
-            "kwd_lang": "pt",
-            "kwd_title": "Palavra-chave",
+            "kwd-lang": "pt",
+            "kwd-title": "Palavra-chave",
+            "kwds": [
+                "Broncoscopia",
+            ]
         }
         expected_xml_str = (
             '<kwd-group xml:lang="pt">'
             '<title>Palavra-chave</title>'
+            '<kwd>Broncoscopia</kwd>'
             '</kwd-group>'
         )
         kwd_group_elem = build_kwd_group(data)
@@ -45,19 +56,22 @@ class TestBuildKwdGroupTitle(unittest.TestCase):
 
     def test_build_kwd_group_title_None(self):
         data = {
-            "kwd_lang": "pt",
-            "kwd_title": None,
+            "kwd-lang": "pt",
+            "kwd-title": None,
+            "kwds": [
+                "Broncoscopia",
+            ]
         }
         with self.assertRaises(ValueError) as e:
             build_kwd_group(data)
-        self.assertEqual(str(e.exception), "kwd title is required")
+        self.assertEqual(str(e.exception), "kwd-title is required")
 
 
 class TestBuildKwdGroupKwds(unittest.TestCase):
     def test_build_kwd_group_kwds(self):
         data = {
-            "kwd_lang": "pt",
-            "kwd_title": "Palavra-chave",
+            "kwd-lang": "pt",
+            "kwd-title": "Palavra-chave",
             "kwds": [
                 "Broncoscopia",
             ]
@@ -74,15 +88,10 @@ class TestBuildKwdGroupKwds(unittest.TestCase):
 
     def test_build_kwd_group_kwds_None(self):
         data = {
-            "kwd_lang": "pt",
-            "kwd_title": "Palavra-chave",
+            "kwd-lang": "pt",
+            "kwd-title": "Palavra-chave",
             "kwds": None
         }
-        expected_xml_str = (
-            '<kwd-group xml:lang="pt">'
-            '<title>Palavra-chave</title>'
-            '</kwd-group>'
-        )
-        kwd_group_elem = build_kwd_group(data)
-        generated_xml_str = ET.tostring(kwd_group_elem, encoding="unicode", method="xml")
-        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+        with self.assertRaises(ValueError) as e:
+            build_kwd_group(data)
+        self.assertEqual(str(e.exception), "kwds must not be an empty list")
