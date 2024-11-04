34ac87faa52b6939b1414a4bd4b4e810cb4128be
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-11-04 01:35:26 -0300

    Remove teste test_xml_pubmed_without_title_reference_list

--- tests/sps/formats/test_pubmed.py
+++ tests/sps/formats/test_pubmed.py
@@ -1191,28 +1191,6 @@ class PipelinePubmed(unittest.TestCase):
 
         self.assertEqual(obtained, expected)
 
-    def test_xml_pubmed_without_title_reference_list(self):
-        expected = (
-            '<Article/>'
-        )
-        xml_pubmed = ET.fromstring(
-            '<Article/>'
-        )
-        xml_tree = ET.fromstring(
-            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'xmlns:xlink="http://www.w3.org/1999/xlink" '
-            'article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">'
-            '<back>'
-            '<ref-list/>'
-            '</back>'
-            '</article>'
-        )
-
-        xml_pubmed_title_reference_list(xml_pubmed, xml_tree)
-
-        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
-
-        self.assertEqual(obtained, expected)
 
     def test_xml_pubmed_citations(self):
         self.maxDiff = None
