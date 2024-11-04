c7e4a9f5a29245949c7718868c42422c313edf13
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-11-04 00:47:13 -0300

    Remove testes desnecessarios

--- tests/sps/formats/test_pubmed.py
+++ tests/sps/formats/test_pubmed.py
@@ -530,34 +530,6 @@ class PipelinePubmed(unittest.TestCase):
 
         self.assertEqual(obtained, expected)
 
-    def test_xml_pubmed_article_title_pipe_without_title(self):
-        expected = (
-            '<ArticleSet>'
-            '<Article/>'
-            '</ArticleSet>'
-        )
-        xml_pubmed = ET.fromstring(
-            '<ArticleSet>'
-            '<Article/>'
-            '</ArticleSet>'
-        )
-        xml_tree = ET.fromstring(
-            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'xmlns:xlink="http://www.w3.org/1999/xlink" '
-            'article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">'
-            '<front>'
-            '<article-meta>'
-            '</article-meta>'
-            '</front>'
-            '</article>'
-        )
-
-        xml_pubmed_article_title_pipe(xml_pubmed, xml_tree)
-
-        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
-
-        self.assertEqual(obtained, expected)
-
     def test_xml_pubmed_vernacular_title_pipe(self):
         expected = (
             '<ArticleSet>'
@@ -591,34 +563,6 @@ class PipelinePubmed(unittest.TestCase):
             '</title-group>'
             '</front-stub>'
             '</sub-article>'
-            '</article>'
-        )
-
-        xml_pubmed_vernacular_title_pipe(xml_pubmed, xml_tree)
-
-        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
-
-        self.assertEqual(obtained, expected)
-
-    def test_xml_pubmed_vernacular_title_pipe_without_title(self):
-        expected = (
-            '<ArticleSet>'
-            '<Article/>'
-            '</ArticleSet>'            
-        )
-        xml_pubmed = ET.fromstring(
-            '<ArticleSet>'
-            '<Article/>'
-            '</ArticleSet>'
-        )
-        xml_tree = ET.fromstring(
-            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'xmlns:xlink="http://www.w3.org/1999/xlink" '
-            'article-type="research-article" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">'
-            '<front>'
-            '<article-meta>'
-            '</article-meta>'
-            '</front>'
             '</article>'
         )
 
