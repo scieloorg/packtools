eff7d56571b9d83508c3686daf1fbe8130e60efb
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-11-04 01:34:10 -0300

    Remove testes desnecessarios

--- tests/sps/formats/test_pubmed.py
+++ tests/sps/formats/test_pubmed.py
@@ -1010,68 +1010,6 @@ class PipelinePubmed(unittest.TestCase):
 
         self.assertEqual(obtained, expected)
 
-    def test_xml_pubmed_article_id_without_pii(self):
-        expected = (
-            '<Article>'
-            '<ArticleIdList>'
-            '<ArticleId IdType="doi">10.36660/abc.20210550</ArticleId>'
-            '</ArticleIdList>'
-            '</Article>'
-        )
-        xml_pubmed = ET.fromstring(
-            '<Article/>'
-        )
-        xml_tree = ET.fromstring(
-            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'xmlns:xlink="http://www.w3.org/1999/xlink" '
-            'article-type="letter" dtd-version="1.1" specific-use="sps-1.9" xml:lang="pt">'
-            '<front>'
-            '<article-meta>'
-            '<article-id specific-use="scielo-v3" pub-id-type="publisher-id">6LBrxmZwBqzgcwcCzgQQwzL</article-id>'
-            '<article-id pub-id-type="other">00501</article-id>'
-            '<article-id pub-id-type="doi">10.36660/abc.20210550</article-id>'
-            '</article-meta>'
-            '</front>'
-            '</article>'
-        )
-
-        xml_pubmed_article_id(xml_pubmed, xml_tree)
-
-        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
-
-        self.assertEqual(obtained, expected)
-
-    def test_xml_pubmed_article_id_without_doi(self):
-        expected = (
-            '<Article>'
-            '<ArticleIdList>'
-            '<ArticleId IdType="pii">S0066-782X2023000100501</ArticleId>'
-            '</ArticleIdList>'
-            '</Article>'
-        )
-        xml_pubmed = ET.fromstring(
-            '<Article/>'
-        )
-        xml_tree = ET.fromstring(
-            '<article xmlns:mml="http://www.w3.org/1998/Math/MathML" '
-            'xmlns:xlink="http://www.w3.org/1999/xlink" '
-            'article-type="letter" dtd-version="1.1" specific-use="sps-1.9" xml:lang="pt">'
-            '<front>'
-            '<article-meta>'
-            '<article-id specific-use="scielo-v3" pub-id-type="publisher-id">6LBrxmZwBqzgcwcCzgQQwzL</article-id>'
-            '<article-id specific-use="scielo-v2" pub-id-type="publisher-id">S0066-782X2023000100501</article-id>'
-            '<article-id pub-id-type="other">00501</article-id>'
-            '</article-meta>'
-            '</front>'
-            '</article>'
-        )
-
-        xml_pubmed_article_id(xml_pubmed, xml_tree)
-
-        obtained = ET.tostring(xml_pubmed, encoding="utf-8").decode("utf-8")
-
-        self.assertEqual(obtained, expected)
-
     def test_xml_pubmed_article_id_missing_article_id(self):
         expected_report = self.get_expected_report(
             missing_tag="ArticleIdList",
