67f2cab82162ff8dc2c42fabd6170805c96d6db9
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-07 11:21:05 -0300

    Adapta os testes

--- tests/sps/formats/sps_xml/test_disp_formula.py
+++ tests/sps/formats/sps_xml/test_disp_formula.py
@@ -8,13 +8,17 @@ class TestBuildDispFormulaId(unittest.TestCase):
     def test_build_disp_formula_id(self):
         data = {
             "formula-id": "e01",
-            "codification": "mml:math",
-            "codification-id": "m1",
-            "formula": "codificação da fórmula"
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": "fórmula no formato mml"
+                }
+            ]
         }
         expected_xml_str = (
             '<disp-formula id="e01">'
-            '<mml:math id="m1">codificação da fórmula</mml:math>'
+            '<mml:math id="m1">fórmula no formato mml</mml:math>'
             '</disp-formula>'
         )
         disp_formula_elem = build_disp_formula(data)
@@ -24,13 +28,17 @@ class TestBuildDispFormulaId(unittest.TestCase):
     def test_build_disp_formula_id_None(self):
         data = {
             "formula-id": None,
-            "codification": "mml:math",
-            "codification-id": "m1",
-            "formula": "codificação da fórmula"
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": "fórmula no formato mml"
+                }
+            ]
         }
         with self.assertRaises(ValueError) as e:
             build_disp_formula(data)
-        self.assertEqual(str(e.exception), "Missing required fields: formula-id")
+        self.assertEqual(str(e.exception), "formula-id is required")
 
 
 class TestBuildDispFormulaLabel(unittest.TestCase):
@@ -38,14 +46,18 @@ class TestBuildDispFormulaLabel(unittest.TestCase):
         data = {
             "formula-id": "e01",
             "label": "(1)",
-            "codification": "mml:math",
-            "codification-id": "m1",
-            "formula": "codificação da fórmula"
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": "fórmula no formato mml"
+                }
+            ]
         }
         expected_xml_str = (
             '<disp-formula id="e01">'
             '<label>(1)</label>'
-            '<mml:math id="m1">codificação da fórmula</mml:math>'
+            '<mml:math id="m1">fórmula no formato mml</mml:math>'
             '</disp-formula>'
         )
         disp_formula_elem = build_disp_formula(data)
@@ -56,13 +68,17 @@ class TestBuildDispFormulaLabel(unittest.TestCase):
         data = {
             "formula-id": "e01",
             "label": None,
-            "codification": "mml:math",
-            "codification-id": "m1",
-            "formula": "codificação da fórmula"
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": "fórmula no formato mml"
+                }
+            ]
         }
         expected_xml_str = (
             '<disp-formula id="e01">'
-            '<mml:math id="m1">codificação da fórmula</mml:math>'
+            '<mml:math id="m1">fórmula no formato mml</mml:math>'
             '</disp-formula>'
         )
         disp_formula_elem = build_disp_formula(data)
@@ -74,51 +90,76 @@ class TestBuildDispFormula(unittest.TestCase):
     def test_build_disp_formula_codification_None(self):
         data = {
             "formula-id": "e01",
-            "codification": None,
-            "codification-id": "m1",
-            "formula": "codificação da fórmula"
+            "label": "(1)",
+            "formulas": None
         }
         with self.assertRaises(ValueError) as e:
             build_disp_formula(data)
-        self.assertEqual(str(e.exception), "Missing required fields: codification")
+        self.assertEqual(str(e.exception), "At least one representation of the formula is required")
 
     def test_build_disp_formula_codification_id_None(self):
         data = {
             "formula-id": "e01",
-            "codification": "tex-math",
-            "codification-id": None,
-            "formula": "codificação da fórmula"
+            "label": "(1)",
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": None,
+                    "text": "fórmula no formato mml"
+                }
+            ]
         }
         with self.assertRaises(ValueError) as e:
             build_disp_formula(data)
-        self.assertEqual(str(e.exception), "Missing required fields: codification-id")
+        self.assertEqual(str(e.exception), "Missing required fields: id")
 
     def test_build_disp_formula_formula_None(self):
         data = {
             "formula-id": "e01",
-            "codification": "tex-math",
-            "codification-id":"m1",
-            "formula": None
+            "label": "(1)",
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": None
+                }
+            ]
         }
         with self.assertRaises(ValueError) as e:
             build_disp_formula(data)
-        self.assertEqual(str(e.exception), "Missing required fields: formula")
+        self.assertEqual(str(e.exception), "Missing required fields: text")
 
 
 class TestBuildDispFormulaAlternatives(unittest.TestCase):
     def test_build_disp_formula_alternatives(self):
         data = {
             "formula-id": "e01",
-            "codification": "tex-math",
-            "codification-id": "m1",
-            "formula": "codificação da fórmula",
-            "alternative-link": "0103-507X-rbti-26-02-0089-ee10.svg"
+            "label": "(1)",
+            "formulas": [
+                {
+                    "codification": "mml:math",
+                    "id": "m1",
+                    "text": "fórmula no formato mml"
+                },
+                {
+                    "codification": "tex-math",
+                    "id": "t1",
+                    "text": "fórmula no formato tex"
+                },
+                {
+                    "codification": "graphic",
+                    "id": "g1",
+                    "text": "0103-507X-rbti-26-02-0089-ee10.svg"
+                }
+            ]
         }
         expected_xml_str = (
             '<disp-formula id="e01">'
+            '<label>(1)</label>'
             '<alternatives>'
-            '<tex-math id="m1">codificação da fórmula</tex-math>'
-            '<graphic xlink:href="0103-507X-rbti-26-02-0089-ee10.svg" />'
+            '<mml:math id="m1">fórmula no formato mml</mml:math>'
+            '<tex-math id="t1">fórmula no formato tex</tex-math>'
+            '<graphic xlink:href="0103-507X-rbti-26-02-0089-ee10.svg" id="g1" />'
             '</alternatives>'
             '</disp-formula>'
         )
