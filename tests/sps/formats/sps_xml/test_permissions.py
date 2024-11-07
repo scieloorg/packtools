a4b42bd8ffdf2e32d5c9e5c6fc0fb1ac21afc6cd
Luciano <41302084+Rossi-Luciano@users.noreply.github.com>
2024-11-07 18:49:26 -0300

    Feat: build kwd group element (#733)
    * Adiciona 'full_tag'
    
    * Revert "Adiciona 'full_tag'"
    
    This reverts commit 1d0129c81e7dbcbb8e9c8dda27f2c6d3ff0c73af.
    
    * Adiciona "build_permissions"
    
    * Adiciona testes
    
    * Adiciona função para criar elemento "kwd-group"
    
    * Adiciona testes
    
    * Corrige "build_kwd_group"
    
    * Adapta os testes
    
    * Adiciona tratamento de exceção
    
    * Adapta os testes

--- tests/sps/formats/sps_xml/test_permissions.py
+++ tests/sps/formats/sps_xml/test_permissions.py
@@ -7,10 +7,21 @@ class TestBuildPermissionsCopyrightStatement(unittest.TestCase):
     def test_build_permissions_copyright_statement(self):
         data = {
             "copyright-statement": "Copyright © 2014 SciELO",
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
             '<permissions>'
             '<copyright-statement>Copyright © 2014 SciELO</copyright-statement>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
             '</permissions>'
         )
         permissions_elem = build_permissions(data)
@@ -19,10 +30,22 @@ class TestBuildPermissionsCopyrightStatement(unittest.TestCase):
 
     def test_build_permissions_copyright_statement_None(self):
         data = {
-            "copyright-statement": None
+            "copyright-statement": None,
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
-            '<permissions />'
+            '<permissions>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
+            '</permissions>'
         )
         permissions_elem = build_permissions(data)
         generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
@@ -33,10 +56,21 @@ class TestBuildPermissionsCopyrightYear(unittest.TestCase):
     def test_build_permissions_copyright_year(self):
         data = {
             "copyright-year": "2014",
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
             '<permissions>'
             '<copyright-year>2014</copyright-year>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
             '</permissions>'
         )
         permissions_elem = build_permissions(data)
@@ -45,10 +79,22 @@ class TestBuildPermissionsCopyrightYear(unittest.TestCase):
 
     def test_build_permissions_copyright_year_None(self):
         data = {
-            "copyright-year": None
+            "copyright-year": None,
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
-            '<permissions />'
+            '<permissions>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
+            '</permissions>'
         )
         permissions_elem = build_permissions(data)
         generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
@@ -58,12 +104,22 @@ class TestBuildPermissionsCopyrightYear(unittest.TestCase):
 class TestBuildPermissionsCopyrightHolder(unittest.TestCase):
     def test_build_permissions_copyright_holder(self):
         data = {
-            "copyright-holder": "SciELO"
+            "copyright-holder": "SciELO",
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
             '<permissions>'
             '<copyright-holder>SciELO</copyright-holder>'
-            '</permissions>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
         )
         permissions_elem = build_permissions(data)
         generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
@@ -71,10 +127,22 @@ class TestBuildPermissionsCopyrightHolder(unittest.TestCase):
 
     def test_build_permissions_copyright_holder_None(self):
         data = {
-            "copyright-holder": None
+            "copyright-holder": None,
+            "licenses": [
+                {
+                    "license-type": "open-access",
+                    "xlink:href": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+                    "xml:lang": "pt",
+                    "license-p": "This is an article published in open access under a Creative Commons license"
+                }
+            ]
         }
         expected_xml_str = (
-            '<permissions />'
+            '<permissions>'
+            '<license license-type="open-access" xlink:href="http://creativecommons.org/licenses/by-nc-sa/4.0/" xml:lang="pt">'
+            'This is an article published in open access under a Creative Commons license'
+            '</license>'
+            '</permissions>'
         )
         permissions_elem = build_permissions(data)
         generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
@@ -109,12 +177,10 @@ class TestBuildPermissionsLicenses(unittest.TestCase):
         data = {
             "licenses": None
         }
-        expected_xml_str = (
-            '<permissions />'
-        )
-        permissions_elem = build_permissions(data)
-        generated_xml_str = ET.tostring(permissions_elem, encoding="unicode", method="xml")
-        self.assertEqual(generated_xml_str.strip(), expected_xml_str.strip())
+        
+        with self.assertRaises(TypeError) as e:
+            build_permissions(data)
+        self.assertEqual(str(e.exception), "licenses must be a list")
 
     def test_build_permissions_licenses_required_attributes(self):
         # vale para os demais atributos, exceto "license-p"
