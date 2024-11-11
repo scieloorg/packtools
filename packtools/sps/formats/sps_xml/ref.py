1d0e1a699366d2ae859f8ee749d9ce3659dd51ad
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-11 10:58:35 -0300

    Corrige estrutura do XML resultante

--- packtools/sps/formats/sps_xml/ref.py
+++ packtools/sps/formats/sps_xml/ref.py
@@ -47,28 +47,44 @@ import xml.etree.ElementTree as ET
 
 
 def build_ref(data):
-    id_text = data.get("ref-id")
-    if not id_text:
+    if not (id_text := data.get("ref-id")):
         raise ValueError("attribute ref-id is required")
 
-    publication_type = data.get("publication-type")
-    if not publication_type:
+    ref_elem = ET.Element("ref", attrib={"id": id_text})
+
+    for key in ("label", "mixed-citation"):
+        if value := data.get(key):
+            ET.SubElement(ref_elem, key).text = value
+
+    if not (publication_type := data.get("publication-type")):
         raise ValueError("attribute publication-type is required")
 
-    ref_elem = ET.Element("ref", attrib={"id": id_text})
-    element_citation_elem = ET.Element("element-citation", attrib={"publication-type": publication_type})
-    ref_elem.append(element_citation_elem)
+    element_citation_elem = ET.SubElement(
+        ref_elem, "element-citation", attrib={"publication-type": publication_type}
+    )
 
-    basic_keys = ("label", "mixed-citation", "article-title", "chapter-title", "source", "edition", "publisher-loc",
-                  "publisher-name", "series", "day", "month", "year", "volume", "issue", "data-title", "version",
-                  "fpage", "lpage")
+    basic_keys = (
+        "article-title",
+        "chapter-title",
+        "source",
+        "edition",
+        "publisher-loc",
+        "publisher-name",
+        "series",
+        "day",
+        "month",
+        "year",
+        "volume",
+        "issue",
+        "data-title",
+        "version",
+        "fpage",
+        "lpage",
+    )
 
     for key in basic_keys:
-        key_text = data.get(key)
-        if key_text:
-            key_elem = ET.Element(key)
-            key_elem.text = key_text
-            ref_elem.append(key_elem)
+        if value := data.get(key):
+            ET.SubElement(element_citation_elem, key).text = value
 
     for ext_link in data.get("ext-link") or []:
         ext_link_type = ext_link.get("ext-link-type")
@@ -76,32 +92,33 @@ def build_ref(data):
         if not ext_link_type or not xlink_href:
             raise ValueError("ext-link-type and xlink:href are required")
 
-        ext_link_text = ext_link.get("text")
-        if ext_link_text:
-            ext_link_elem = ET.Element("ext-link", attrib={"ext-link-type": ext_link_type, "xlink:href": xlink_href})
-            ext_link_elem.text = ext_link_text
-            ref_elem.append(ext_link_elem)
+        if ext_link_text := ext_link.get("text"):
+            ET.SubElement(
+                element_citation_elem,
+                "ext-link",
+                attrib={"ext-link-type": ext_link_type, "xlink:href": xlink_href},
+            ).text = ext_link_text
 
     for pub_id in data.get("pub-ids") or []:
         pub_id_type = pub_id.get("pub-id-type")
         if not pub_id_type:
             raise ValueError("pub-id-type is required")
 
-        pub_id_text = pub_id.get("text")
-        if pub_id_text:
-            pub_id_elem = ET.Element("pub-id", attrib={"pub-id-type": pub_id_type})
-            pub_id_elem.text = pub_id_text
-            ref_elem.append(pub_id_elem)
+        if pub_id_text := pub_id.get("text"):
+            ET.SubElement(
+                element_citation_elem, "pub-id", attrib={"pub-id-type": pub_id_type}
+            ).text = pub_id_text
 
     for date_in_citation in data.get("dates-in-citation") or []:
         content_type = date_in_citation.get("content-type")
         if not content_type:
             raise ValueError("content-type is required")
 
-        date_in_citation_text = date_in_citation.get("text")
-        if date_in_citation_text:
-            date_in_citation_elem = ET.Element("date-in-citation", attrib={"content-type": content_type})
-            date_in_citation_elem.text = date_in_citation_text
-            ref_elem.append(date_in_citation_elem)
+        if date_in_citation_text := date_in_citation.get("text"):
+            ET.SubElement(
+                element_citation_elem,
+                "date-in-citation",
+                attrib={"content-type": content_type},
+            ).text = date_in_citation_text
 
     return ref_elem
