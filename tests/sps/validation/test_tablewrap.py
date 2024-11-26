fbe39d0fc4e56275c7e84f0be3e99a2a26fa723f
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-26 11:02:27 -0300

    Adiciona teste para validação de 'label'

--- tests/sps/validation/test_tablewrap.py
+++ tests/sps/validation/test_tablewrap.py
@@ -99,135 +99,63 @@ class TableWrapValidationTest(unittest.TestCase):
             with self.subTest(i):
                 self.assertDictEqual(item, obtained[i])
 
-    def test_tablewrap_validation_with_tablewrap_elements_fix_bug(self):
+    def test_validate_label(self):
         self.maxDiff = None
-        xml_tree = xml_utils.get_xml_tree(
-            "tests/fixtures/htmlgenerator/table_wrap_group_and_fig_group/2236-8906"
-            "-hoehnea-49-e1082020/2236-8906-hoehnea-49-e1082020.xml"
-        )
-        obtained = list(TableWrapValidation(xml_tree).validate_tablewrap_existence())
-
-        expected = [
-            {
-                "advice": None,
-                "data": {
-                    "alternative_elements": [],
-                    "alternative_parent": "table-wrap",
-                    "caption": "Classificação Sucessional adotada por alguns autores ao "
-                               "longo dos anos.",
-                    "footnote": "",
-                    "footnote_id": None,
-                    "footnote_label": None,
-                    "label": "Tabela 1",
-                    "parent": "article",
-                    "parent_article_type": "research-article",
-                    "parent_id": None,
-                    "parent_lang": "pt",
-                    "table_wrap_id": None,
-                },
-                "expected_value": "<table-wrap> element",
-                'got_value': '<table-wrap id="None">',
-                "item": "table-wrap",
-                "message": 'Got <table-wrap id="None">, expected <table-wrap> element',
-                "parent": "article",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "pt",
-                "response": "OK",
-                "sub_item": None,
-                "title": "table-wrap presence",
-                "validation_type": "exist",
-            },
-            {
-                "advice": None,
-                "data": {
-                    "alternative_elements": [],
-                    "alternative_parent": "table-wrap",
-                    "caption": "List of threatened species and their classification in "
-                               "the lists of São Paulo, 2016, Brazil, 2019c and IUCN, "
-                               "2019. VU categories: Vulnerable, EN: Endangered and EX: "
-                               "Extinct.",
-                    "footnote": "",
-                    "footnote_id": None,
-                    "footnote_label": None,
-                    "label": "Table 4",
-                    "parent": "article",
-                    "parent_article_type": "research-article",
-                    "parent_id": None,
-                    "parent_lang": "pt",
-                    "table_wrap_id": None,
-                },
-                "expected_value": "<table-wrap> element",
-                'got_value': '<table-wrap id="None">',
-                "item": "table-wrap",
-                'message': 'Got <table-wrap id="None">, expected <table-wrap> element',
-                "parent": "article",
-                "parent_article_type": "research-article",
-                "parent_id": None,
-                "parent_lang": "pt",
-                "response": "OK",
-                "sub_item": None,
-                "title": "table-wrap presence",
-                "validation_type": "exist",
-            }
-
-        ]
-
-        self.assertEqual(len(obtained), 8)
-        # showing only the first and last record for the table
-        self.assertDictEqual(expected[0], obtained[0])
-        self.assertDictEqual(expected[1], obtained[7])
-
-    def test_validate_tablewrap_elements(self):
-        self.maxDiff = None
-        xmltree = etree.fromstring(
+        xml_tree = etree.fromstring(
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" '
             'dtd-version="1.0" article-type="research-article" xml:lang="pt">'
             "<body>"
-            '<table-wrap>'
-            "<alternatives>"
-            '<graphic xlink:href="image1-lowres.png" mime-subtype="low-resolution"/>'
-            '<graphic xlink:href="image1-highres.png" mime-subtype="high-resolution"/>'
-            "</alternatives>"
-            "</table-wrap>"
-            "</body>"
-            '<sub-article article-type="translation" xml:lang="en">'
-            "<body>"
             '<table-wrap id="t01">'
-            "<label>Table 1:</label>"
             "<caption>Table caption</caption>"
-            "<alternatives>"
-            '<graphic xlink:href="image1-lowres.png" mime-subtype="low-resolution"/>'
-            '<graphic xlink:href="image1-highres.png" mime-subtype="high-resolution"/>'
-            "</alternatives>"
             "</table-wrap>"
             "</body>"
-            "</sub-article>"
             "</article>"
         )
-        obtained = list(TableWrapValidation(xmltree).validate_tablewrap_elements())
+        obtained = list(
+            ArticleTableWrapValidation(
+                xml_tree=xml_tree,
+                rules={
+                    "label_error_level": "CRITICAL",
+                },
+            ).validate()
+        )
 
         expected = [
             {
-                "advice": "provide table-wrap/@id or label or caption for table-wrap",
+                "title": "label",
+                "parent": "article",
+                "parent_id": None,
+                "parent_article_type": "research-article",
+                "parent_lang": "pt",
+                "item": "table-wrap",
+                "sub_item": "label",
+                "validation_type": "exist",
+                "response": "CRITICAL",
+                "expected_value": "label",
+                "got_value": None,
+                "message": "Got None, expected label",
+                "advice": "Identify the label",
                 "data": {
-                    "alternative_elements": ["graphic", "graphic"],
                     "alternative_parent": "table-wrap",
-                    "caption": "",
+                    "table_wrap_id": "t01",
+                    "label": None,
+                    "caption": "Table caption",
                     "footnote": "",
                     "footnote_id": None,
                     "footnote_label": None,
-                    "label": None,
+                    "alternative_elements": [],
                     "parent": "article",
-                    "parent_article_type": "research-article",
                     "parent_id": None,
+                    "parent_article_type": "research-article",
                     "parent_lang": "pt",
-                    "table_wrap_id": None,
                 },
-                "expected_value": "table-wrap/@id or label or caption elements",
-                "got_value": [],
-                "item": "table-wrap",
-                "message": "Got [], expected table-wrap/@id or label or caption elements",
+            }
+        ]
+
+        for i, item in enumerate(expected):
+            with self.subTest(i):
+                self.assertDictEqual(item, obtained[i])
+
                 "parent": "article",
                 "parent_article_type": "research-article",
                 "parent_id": None,
