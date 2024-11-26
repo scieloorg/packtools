471de20021b72d388784419803cce0a277c2c89c
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-11-26 11:01:51 -0300

    Adiciona teste para validação de 'id'

--- tests/sps/validation/test_tablewrap.py
+++ tests/sps/validation/test_tablewrap.py
@@ -44,61 +44,49 @@ class TableWrapValidationTest(unittest.TestCase):
             with self.subTest(i):
                 self.assertDictEqual(item, obtained[i])
 
-    def test_tablewrap_validation_with_tablewrap_elements(self):
+    def test_validate_id(self):
         self.maxDiff = None
-        xmltree = etree.fromstring(
+        xml_tree = etree.fromstring(
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML" '
             'dtd-version="1.0" article-type="research-article" xml:lang="pt">'
             "<body>"
-            '<table-wrap id="t01">'
+            "<table-wrap>"
             "<label>Table 1</label>"
-            '<caption>Table caption</caption>'
-            "<alternatives>"
-            '<graphic xlink:href="image1-lowres.png" mime-subtype="low-resolution"/>'
-            '<graphic xlink:href="image1-highres.png" mime-subtype="high-resolution"/>'
-            "</alternatives>"
-            "</table-wrap>"
-            "</body>"
-            '<sub-article article-type="translation" xml:lang="en">'
-            "<body>"
-            '<table-wrap id="t01">'
-            "<label>Table 1:</label>"
             "<caption>Table caption</caption>"
-            "<alternatives>"
-            '<graphic xlink:href="image1-lowres.png" mime-subtype="low-resolution"/>'
-            '<graphic xlink:href="image1-highres.png" mime-subtype="high-resolution"/>'
-            "</alternatives>"
             "</table-wrap>"
             "</body>"
-            "</sub-article>"
             "</article>"
         )
-        obtained = list(TableWrapValidation(xmltree).validate_tablewrap_existence())
+        obtained = list(
+            ArticleTableWrapValidation(
+                xml_tree=xml_tree, rules={"table_wrap_id_error_level": "CRITICAL"}
+            ).validate()
+        )
 
         expected = [
             {
-                "title": "table-wrap presence",
+                "title": "table_wrap_id",
                 "parent": "article",
                 "parent_id": None,
                 "parent_article_type": "research-article",
                 "parent_lang": "pt",
                 "item": "table-wrap",
-                "sub_item": None,
+                "sub_item": "table_wrap_id",
                 "validation_type": "exist",
-                "response": "OK",
-                'expected_value': '<table-wrap> element',
-                'got_value': '<table-wrap id="t01">',
-                'message': 'Got <table-wrap id="t01">, expected <table-wrap> element',
-                "advice": None,
+                "response": "CRITICAL",
+                "expected_value": "table_wrap_id",
+                "got_value": None,
+                "message": "Got None, expected table_wrap_id",
+                "advice": "Identify the table_wrap_id",
                 "data": {
                     "alternative_parent": "table-wrap",
-                    "table_wrap_id": "t01",
+                    "table_wrap_id": None,
                     "label": "Table 1",
                     "caption": "Table caption",
                     "footnote": "",
                     "footnote_id": None,
                     "footnote_label": None,
-                    "alternative_elements": ["graphic", "graphic"],
+                    "alternative_elements": [],
                     "parent": "article",
                     "parent_id": None,
                     "parent_article_type": "research-article",
