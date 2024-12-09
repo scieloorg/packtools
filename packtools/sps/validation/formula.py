ff7a237760e92f9fdeee4f3cbbb6273023434d40
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-12-09 14:46:13 -0300

    Complementa validações

--- packtools/sps/validation/formula.py
+++ packtools/sps/validation/formula.py
@@ -17,7 +17,7 @@ class ArticleFormulaValidation:
         if not isinstance(rules, dict):
             raise ValueError("rules must be a dictionary containing error levels.")
         try:
-            self.elements_by_language = ArticleFormulas(xml_tree).items_by_lang
+            self.elements = list(ArticleFormulas(xml_tree).items)
         except Exception as e:
             raise RuntimeError(f"Error processing formula: {e}")
         self.xml_tree = xml_tree
@@ -30,27 +30,27 @@ class ArticleFormulaValidation:
         Yields:
             dict: Validation results for each <formula> element or the absence of <formula> elements.
         """
-        if not self.elements_by_language:
+        if not self.elements:
             yield format_response(
-                title="formula",
+                title="disp-formula",
                 parent="article",
                 parent_id=None,
                 parent_article_type=self.xml_tree.get("article-type"),
                 parent_lang=self.xml_tree.get(
                     "{http://www.w3.org/XML/1998/namespace}lang"
                 ),
-                item="formula",
+                item="disp-formula",
                 sub_item=None,
                 validation_type="exist",
                 is_valid=False,
-                expected="formula",
+                expected="disp-formula",
                 obtained=None,
-                advice="Add <formula> elements to properly represent mathematical expressions in the content.",
+                advice="Add <disp-formula> elements to properly represent mathematical expressions in the content.",
                 data=None,
                 error_level=self.rules["absent_error_level"],
             )
         else:
-            for lang, data in self.elements_by_language.items():
+            for data in self.elements:
                 yield from FormulaValidation(data, self.rules).validate()
 
 
@@ -75,16 +75,22 @@ class FormulaValidation:
         Yields:
             dict: Validation results for specific <formula> attributes.
         """
-        yield self._validate_id()
+        validations = [
+            self.validate_id,
+            self.validate_label,
+            self.validate_codification,
+            self.validate_alternatives
+        ]
+        return [response for validate in validations if (response := validate())]
 
-    def _validate_id(self):
+    def validate_id(self):
         """
         Validates the presence of the '@id' attribute in a <formula> element.
 
         Returns:
             dict: Validation result indicating whether the '@id' attribute is present.
         """
-        if not self.data.get("formula_id"):
+        if not self.data.get("id"):
             return format_response(
                 title="@id",
                 parent=self.data.get("parent"),
@@ -101,3 +107,102 @@ class FormulaValidation:
                 data=self.data,
                 error_level=self.rules["id_error_level"],
             )
+
+    def validate_label(self):
+        """
+        Validates the presence of label in the <disp-formula>.
+
+        Returns:
+            The validation result in the expected format.
+        """
+        if not self.data.get("label"):
+            return format_response(
+                title="label",
+                parent=self.data.get("parent"),
+                parent_id=self.data.get("parent_id"),
+                parent_article_type=self.data.get("parent_article_type"),
+                parent_lang=self.data.get("parent_lang"),
+                item="label",
+                sub_item=None,
+                validation_type="exist",
+                is_valid=False,
+                expected="label",
+                obtained=None,
+                advice="Identify the label",
+                data=self.data,
+                error_level=self.rules["label_error_level"],
+            )
+
+    def validate_codification(self):
+        """
+        Validates the presence of codification (mml:math or tex-math) in the <disp-formula>.
+
+        Returns:
+            The validation result in the expected format.
+        """
+        mml = self.data.get("mml_math") or []
+        tex = self.data.get("tex_math") or []
+        if len(mml) + len(tex) == 0:
+            return format_response(
+                title="mml:math or tex-math",
+                parent=self.data.get("parent"),
+                parent_id=self.data.get("parent_id"),
+                parent_article_type=self.data.get("parent_article_type"),
+                parent_lang=self.data.get("parent_lang"),
+                item="mml:math or tex-math",
+                sub_item=None,
+                validation_type="exist",
+                is_valid=False,
+                expected="mml:math or tex-math",
+                obtained=None,
+                advice="Identify the mml:math or tex-math",
+                data=self.data,
+                error_level=self.rules["codification_error_level"],
+            )
+
+    def validate_alternatives(self):
+        """
+        Validates the presence of alternatives in the <disp-formula>.
+
+        Returns:
+            The validation result in the expected format.
+        """
+
+        graphic = self.data.get("graphic")
+        alternatives = self.data.get("alternative_elements")
+
+        # Define validation scenarios
+        validation_cases = [
+            {
+                "condition": graphic != [] and alternatives == [],
+                "expected": "alternatives",
+                "obtained": None,
+                "advice": "Identify the alternatives",
+            },
+            {
+                "condition": graphic == [] and alternatives != [],
+                "expected": None,
+                "obtained": "alternatives",
+                "advice": "Remove the alternatives",
+            },
+        ]
+
+        # Evaluate conditions and return formatted response if any validation fails
+        for case in validation_cases:
+            if case["condition"]:
+                return format_response(
+                    title="alternatives",
+                    parent=self.data.get("parent"),
+                    parent_id=self.data.get("parent_id"),
+                    parent_article_type=self.data.get("parent_article_type"),
+                    parent_lang=self.data.get("parent_lang"),
+                    item="alternatives",
+                    sub_item=None,
+                    validation_type="exist",
+                    is_valid=False,
+                    expected=case["expected"],
+                    obtained=case["obtained"],
+                    advice=case["advice"],
+                    data=self.data,
+                    error_level=self.rules["alternatives_error_level"],
+                )
