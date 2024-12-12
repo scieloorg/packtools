bb2762702de30be8ccfddc60d538446754bb1ebb
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2024-12-12 16:11:49 -0300

    Adiciona assinatura de 'collection'

--- packtools/sps/formats/am/am.py
+++ packtools/sps/formats/am/am.py
@@ -7,3 +7,10 @@ def code(xml_tree, am_dict):
     article_id_v2 = ArticleIds(xml_tree).v2
     am_dict.update({"code": article_id_v2})
     return am_dict
+
+
+def collection(xml_tree, am_dict):
+    # TODO: adicionar a lógica de obtenção do código da coleção
+    return am_dict
+
+
