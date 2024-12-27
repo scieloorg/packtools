54786fd4792095efcafd53b1ad7f60b7cf27ff63
Roberta Takenaka <505143+robertatakenaka@users.noreply.github.com>
2024-12-27 09:49:01 -0300

    Modifica packtools.sps.models.v2.aff e modifica validações de afiliações  (#722)
    * Reorganiza Affiliation.data para ficar mais legível para o usuário; Adiciona ArticleAffiliations.sub_article_translation_affs_by_lang para recuperar as aff das traduçõoes pelos idiomas
    
    * Corrige e melhora packtools.sps.validation.aff
    - Troca AffiliationValidationValidateCountryCodeException por AffiliationCountryCodeListNotProvidedException para ser mais preciso;
    - Troca AffiliationsListValidation por AffiliationsValidation
    - O parâmetro country_codes_list passa a ser obrigatório em AffiliationValidation e AffiliationsValidation e levanta exceção caso na instanciação não é passado com valor válido;
    - Troca AffiliationsValidation.affiliations_list por main_affs e translation_affs, adiciona translation_affs_by_lang
    - Elimina AffiliationsValidation.validate_affiliations_list que suprimia os parâmetros para os métodos validate_main_affiliations e validate_translated_affiliations, mas adicioná-los deixaria AffiliationsValidation.validate_affiliations_list com uma quantidade enorme de parâmetros
    - Elimina AffiliationsValidation.validate pelo mesmo motivo
    - Refeito AffiliationsValidation.validate_affiliation_count: foi renomeado para ficar mais simples; Corrige o método original que não considera que há artigos em sub-article e que não considera que pode haver mais de um sub-article
    
    * Corrige a importação de ArticleAffiliations e corrige alguns testes
    
    * Corrige Affiliation.data e corrige AffiliationsValidation.validate_affiliation_count
    
    * Ajusta a função validate_affiliations

--- packtools/sps/validation/exceptions.py
+++ packtools/sps/validation/exceptions.py
@@ -16,7 +16,7 @@ class ValidationArticleAndSubArticlesHasInvalidLanguage(ValidationArticleDataErr
     ...
 
 
-class AffiliationValidationValidateCountryCodeException(Exception):
+class AffiliationCountryCodeListNotProvidedException(Exception):
     ...
 
 
