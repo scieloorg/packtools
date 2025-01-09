135e101925877408e48c792f10454387e0b9ad03
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-01-09 08:57:28 -0300

    Cria Fulltext para representar os atributos de article e sub-article e respectivos translations e not_translations

--- packtools/sps/models/article_and_subarticles.py
+++ packtools/sps/models/article_and_subarticles.py
@@ -1,3 +1,88 @@
+class Fulltext:
+
+    def __init__(self, node):
+        """
+        node : article or sub-article
+        """
+        self.node = node
+        self.tag = node.tag
+        self.lang = node.get("{http://www.w3.org/XML/1998/namespace}lang")
+        self.article_type = node.get("article-type")
+        self.id = node.get("id")
+
+    @property
+    def front(self):
+        if not hasattr(self, '_front') or not self._front:
+            if self.tag == "article":
+                self._front = node.find("front")
+            else:
+                self._front = node.find("front-stub")
+        return self._front
+
+    @property
+    def body(self):
+        if not hasattr(self, '_body') or not self._body:
+            if self.tag == "article":
+                self._body = node.find("body")
+        return self._body
+
+    @property
+    def back(self):
+        if not hasattr(self, '_back') or not self._back:
+            if self.tag == "article":
+                self._back = node.find("back")
+        return self._back
+
+    @property
+    def sub_articles(self):
+        if not hasattr(self, '_sub_articles') or not self._sub_articles:
+            if self.tag == "article":
+                self._sub_articles = node.xpath("sub-article")
+        return self._sub_articles
+
+    @property
+    def translations(self):
+        if not hasattr(self, '_translations') or not self._translations:
+            if self.tag == "article":
+                self._translations = node.xpath("sub-article[@article-type='translation']")
+        return self._translations
+
+    @property
+    def not_translations(self):
+        if not hasattr(self, '_not_translations') or not self._not_translations:
+            if self.tag == "article":
+                self._not_translations = node.xpath("sub-article[@article-type!='translation']")
+        return self._not_translations
+
+    @property
+    def attribs(self):
+        return {
+            "tag": self.tag,
+            "id": self.id,
+            "lang": self.lang,
+            "article_type": self.article_type,
+        }
+
+    @property
+    def attribs_parent_prefixed(self):
+        return {
+            "parent": self.tag,
+            "parent_id": self.id,
+            "parent_lang": self.lang,
+            "parent_article_type": self.article_type,
+        }
+
+    @property
+    def fulltexts(self):
+        data = {}
+        data["attribs"] = self.attribs
+        data["attribs_parent_prefixed"] = self.attribs_parent_prefixed
+        data["translations"] = [Fulltext(node) for node in self.translations]
+        data["not_translations"] = [Fulltext(node) for node in self.not_translations]
+        data["sub_articles"] = [Fulltext(node) for node in self.sub_articles]
+        return data
+
+
 class ArticleAndSubArticles:
     def __init__(self, xmltree):
         self.xmltree = xmltree
