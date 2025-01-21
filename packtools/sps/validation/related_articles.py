e3c8fea33443625137680da1ddcd4603881a53c0
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-01-21 19:00:11 -0300

    Cria validação individual para related-article e seus testes

--- packtools/sps/validation/related_articles.py
+++ packtools/sps/validation/related_articles.py
@@ -1,7 +1,7 @@
 from packtools.sps.models.related_articles import RelatedItems
 from packtools.sps.models.article_and_subarticles import ArticleAndSubArticles
 from packtools.sps.validation.exceptions import ValidationRelatedArticleException
-from packtools.sps.validation.utils import build_response
+from packtools.sps.validation.utils import build_response, is_valid_url_format, validate_doi_format
 
 
 class RelatedArticlesValidation:
@@ -105,3 +105,227 @@ class RelatedArticlesValidation:
                 data=related_article,
                 error_level=self.error_level
             )
+
+
+class RelatedArticleValidation:
+    """Validates a single related-article element"""
+
+    def __init__(self, related_article, params=None):
+        """
+        Parameters
+        ----------
+        related_article : dict
+            Dictionary with related article data including parent_article_type
+        params : dict, optional
+            Dictionary with validation parameters:
+            {
+                'correspondence_list': [...],
+                'ext_link_types': ['doi', 'uri'],
+                'requires_related_article': ['correction', 'retraction', ...],
+                'requirement_error_level': 'ERROR',
+                'type_error_level': 'ERROR',
+                'ext_link_type_error_level': 'ERROR',
+                'uri_error_level': 'ERROR',
+                'uri_format_error_level': 'ERROR',
+                'doi_error_level': 'ERROR',
+                'doi_format_error_level': 'ERROR',
+                'id_error_level': 'ERROR'
+            }
+        """
+        self.related_article = related_article
+        self.article_type = related_article.get("original_article_type") or related_article.get('parent_article_type')
+        self.related_article_type = related_article.get("related-article-type")
+
+        self.params = params or {}
+        self.valid_ext_link_types = self.params.get('ext_link_types', ['doi', "uri"])
+        _related = (
+            self.params.get('article-types-and-related-article-types', {}).get(self.article_type, {})
+        )
+        self.required_related_article_types = _related.get("required_related_article_types") or []
+        self.acceptable_related_article_types = _related.get("acceptable_related_article_types") or []
+
+    def _get_error_level(self, validation_type):
+        """
+        Get error level for specific validation type from params
+        
+        Parameters
+        ----------
+        validation_type : str
+            Type of validation being performed
+            
+        Returns
+        -------
+        str
+            Error level for the validation type
+        """
+        error_level_key = f'{validation_type}_error_level'
+        return self.params.get(error_level_key, 'CRITICAL')
+
+    def validate_type(self):
+        """Validate if related article type matches main article type"""
+        expected_values = self.required_related_article_types + self.acceptable_related_article_types
+
+        obtained_type = self.related_article.get('related-article-type')
+
+        if not expected_values:
+            return build_response(
+                title='Related article type validation',
+                parent=self.related_article,
+                item='related-article',
+                sub_item='related-article-type',
+                validation_type='match',
+                is_valid=False,
+                expected=expected_values,
+                obtained=obtained_type,
+                advice=f"The article-type: {self.article_type} does not match the related-article-type: "
+                      f"{obtained_type}, provide one of the following items: {expected_values}",
+                data=self.related_article,
+                error_level=self._get_error_level('type')
+            )
+
+        is_valid = obtained_type in expected_values
+
+        return build_response(
+            title='Related article type validation',
+            parent=self.related_article,
+            item='related-article',
+            sub_item='related-article-type',
+            validation_type='match',
+            is_valid=is_valid,
+            expected=expected_values,
+            obtained=obtained_type,
+            advice=f"The article-type: {self.article_type} does not match the related-article-type: "
+                  f"{obtained_type}, provide one of the following items: {expected_values}",
+            data=self.related_article,
+            error_level=self._get_error_level('type')
+        )
+
+    def validate_ext_link_type(self):
+        """Validate if related article has valid ext-link-type"""
+        ext_link_type = self.related_article.get('ext-link-type')
+        is_valid = ext_link_type in self.valid_ext_link_types
+
+        return build_response(
+            title='Related article ext-link-type validation',
+            parent=self.related_article,
+            item='related-article',
+            sub_item='ext-link-type',
+            validation_type='match',
+            is_valid=is_valid,
+            expected=self.valid_ext_link_types,
+            obtained=ext_link_type,
+            advice=f'The ext-link-type should be one of {self.valid_ext_link_types} for related article with id="{self.related_article.get("id")}"',
+            data=self.related_article,
+            error_level=self._get_error_level('ext_link_type')
+        )
+
+    def validate_uri(self):
+        """Validate if related article has valid link (URI)"""
+        ext_link_type = self.related_article.get('ext-link-type')
+        if not ext_link_type == "uri":
+            return
+
+        link = self.related_article.get('href')
+        if not link:
+            return build_response(
+                title='Related article link validation',
+                parent=self.related_article,
+                item='related-article',
+                sub_item='xlink:href',
+                validation_type='exist',
+                is_valid=False,
+                expected=f'A valid {ext_link_type.upper() if ext_link_type else "link"}',
+                obtained=link,
+                advice=f'Provide a valid {ext_link_type.upper() if ext_link_type else "link"} for <related-article '
+                      f'id="{self.related_article.get("id")}" />',
+                data=self.related_article,
+                error_level=self._get_error_level('uri')
+            )
+
+        is_valid = is_valid_url_format(link)
+        expected = 'A valid URI format (e.g., http://example.com)'
+        
+        return build_response(
+            title='Related article link validation',
+            parent=self.related_article,
+            item='related-article',
+            sub_item='xlink:href',
+            validation_type='format',
+            is_valid=is_valid,
+            expected=expected,
+            obtained=link,
+            advice=None if is_valid else f'Invalid {ext_link_type.upper()} format for link: {link}',
+            data=self.related_article,
+            error_level=self._get_error_level('uri_format')
+        )
+
+    def validate_doi(self):
+        """Validate if related article has valid DOI"""
+        ext_link_type = self.related_article.get('ext-link-type')
+        if not ext_link_type == "doi":
+            return
+
+        link = self.related_article.get('href')
+        if not link:
+            return build_response(
+                title='Related article doi validation',
+                parent=self.related_article,
+                item='related-article',
+                sub_item='xlink:href',
+                validation_type='exist',
+                is_valid=False,
+                expected=f'A valid {ext_link_type.upper() if ext_link_type else "link"}',
+                obtained=link,
+                advice=f'Provide a valid {ext_link_type.upper() if ext_link_type else "link"} for <related-article '
+                      f'id="{self.related_article.get("id")}" />',
+                data=self.related_article,
+                error_level=self.params.get("doi_error_level")
+            )
+
+        valid = validate_doi_format(link)
+        is_valid = valid and valid.get("valido")
+        expected = 'A valid DOI'
+
+        return build_response(
+            title='Related article doi validation',
+            parent=self.related_article,
+            item='related-article',
+            sub_item='xlink:href',
+            validation_type='format',
+            is_valid=is_valid,
+            expected=expected,
+            obtained=link,
+            advice=None if is_valid else f'Invalid {ext_link_type.upper()} format for link: {link}',
+            data=self.related_article,
+            error_level=self.params.get("doi_format_error_level")
+        )
+
+    def validate_id_presence(self):
+        """Validate if related article has an ID"""
+        related_id = self.related_article.get('id')
+        is_valid = related_id is not None and related_id.strip() != ''
+        expected = 'A non-empty ID'
+
+        return build_response(
+            title='Related article id validation',
+            parent=self.related_article,
+            item='related-article',
+            sub_item='id',
+            validation_type='exist',
+            is_valid=is_valid,
+            expected=expected,
+            obtained=related_id,
+            advice='Each related-article element must have a unique id attribute',
+            data=self.related_article,
+            error_level=self._get_error_level('id')
+        )
+
+    def validate(self):
+        """Run all validations"""
+        validations = [
+            self.validate_type(),
+            self.validate_ext_link_type(),
+            self.validate_doi() or self.validate_uri(),
+            self.validate_id_presence()
+        ]
+        return [v for v in validations if v is not None]
--- tests/sps/models/test_related_articles.py
+++ tests/sps/models/test_related_articles.py
@@ -1,18 +1,17 @@
-import unittest
+from unittest import TestCase
 from io import BytesIO
 
 from lxml import etree
-from packtools.sps.models.related_articles import RelatedItems
+from packtools.sps.models.related_articles import RelatedItems, Fulltext
 
 
 def create_xml_tree(xml_content):
     return etree.parse(BytesIO(xml_content.encode("utf-8")))
 
 
-class TestArticleRelatedItems(unittest.TestCase):
+class TestArticleRelatedItems(TestCase):
     def setUp(self):
         xml = (
-            '<?xml version="1.0" encoding="utf-8"?>'
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en">'
             "<front>"
             "<article-meta>"
@@ -29,7 +28,6 @@ class TestArticleRelatedItems(unittest.TestCase):
         xml = '<related-article ext-link-type="doi" id="A01" related-article-type="commentary-article" xlink:href="10.1590/0101-3173.2022.v45n1.p139">Referência do artigo comentado: FREITAS, J. H. de. <italic>Some italic text</italic> Sample text <bold>Journal Name</bold>: additional details</related-article>'
         self.assertEqual(len(items), 1)
         item = items[0]
-        print(item)
         self.assertEqual(item["id"], "A01")
         self.assertEqual(item["ext-link-type"], "doi")
         self.assertEqual(item["related-article-type"], "commentary-article")
@@ -43,10 +41,9 @@ class TestArticleRelatedItems(unittest.TestCase):
         self.assertEqual(item.get("parent_article_type"), "research-article")
 
 
-class TestSubArticleRelatedItems(unittest.TestCase):
+class TestSubArticleRelatedItems(TestCase):
     def setUp(self):
         xml = (
-            '<?xml version="1.0" encoding="utf-8"?>'
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article">'
             '<sub-article article-type="translation" xml:lang="es" id="tr1">'
             "<front-stub>"
@@ -74,10 +71,9 @@ class TestSubArticleRelatedItems(unittest.TestCase):
         self.assertEqual(item.get("parent_article_type"), "translation")
 
 
-class TestMultipleRelatedItems(unittest.TestCase):
+class TestMultipleRelatedItems(TestCase):
     def setUp(self):
         xml = (
-            '<?xml version="1.0" encoding="utf-8"?>'
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en">'
             "<front>"
             "<article-meta>"
@@ -119,10 +115,9 @@ class TestMultipleRelatedItems(unittest.TestCase):
         self.assertEqual(items[2].get("parent_article_type"), "translation")
 
 
-class TestNoRelatedItems(unittest.TestCase):
+class TestNoRelatedItems(TestCase):
     def setUp(self):
         xml = (
-            '<?xml version="1.0" encoding="utf-8"?>'
             '<article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article">'
             "<front>"
             "<article-meta>"
@@ -138,5 +133,176 @@ class TestNoRelatedItems(unittest.TestCase):
         self.assertEqual(len(items), 0)
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestFulltextMainArticle(TestCase):
+    """Tests for basic article with related-articles and sub-articles"""
+    
+    def setUp(self):
+        xml = '''
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en" id="a1">
+            <front>
+                <article-meta>
+                    <related-article related-article-type="correction-forward" 
+                                   ext-link-type="doi" 
+                                   xlink:href="10.1590/123456789" 
+                                   id="ra1">Some text</related-article>
+                </article-meta>
+            </front>
+            <sub-article article-type="translation" xml:lang="pt" id="s1">
+                <front-stub>
+                    <related-article related-article-type="correction-forward"
+                                   ext-link-type="doi"
+                                   id="ra2">Translation related</related-article>
+                </front-stub>
+            </sub-article>
+            <sub-article article-type="reviewer-report" xml:lang="es" id="s2">
+                <front-stub>
+                    <related-article related-article-type="reviewer-report-of"
+                                   ext-link-type="doi"
+                                   id="ra3">Text related</related-article>
+                </front-stub>
+            </sub-article>
+        </article>'''
+        self.fulltext = Fulltext(etree.fromstring(xml).find("."))
+
+    def test_parent_data(self):
+        expected = {
+            'parent': 'article',
+            'parent_id': 'a1',
+            'parent_article_type': 'research-article',
+            'parent_lang': 'en',
+            'original_article_type': None
+        }
+        self.assertEqual(self.fulltext.parent_data, expected)
+
+    def test_related_articles(self):
+        related = list(self.fulltext.related_articles)
+        self.assertEqual(len(related), 1)
+        
+        article = related[0]
+        self.assertEqual(article['related-article-type'], 'correction-forward')
+        self.assertEqual(article['ext-link-type'], 'doi')
+        self.assertEqual(article['href'], '10.1590/123456789')
+        self.assertEqual(article['id'], 'ra1')
+        self.assertEqual(article['text'], 'Some text')
+        self.assertIn('xml', article)
+        self.assertIsNone(article['original_article_type'])
+
+    def test_fulltexts(self):
+        sub_articles = list(self.fulltext.fulltexts)
+        self.assertEqual(len(sub_articles), 2)
+        
+        # Test translation sub-article
+        translation = sub_articles[0]
+        self.assertEqual(translation.parent_data['parent_article_type'], 'translation')
+        self.assertEqual(translation.parent_data['parent_lang'], 'pt')
+        self.assertEqual(translation.parent_data['original_article_type'], 'research-article')
+        
+        # Test reviewer-report sub-article
+        reviewer_report = sub_articles[1]
+        self.assertEqual(reviewer_report.parent_data['parent_article_type'], 'reviewer-report')
+        self.assertEqual(reviewer_report.parent_data['parent_lang'], 'es')
+        self.assertIsNone(reviewer_report.parent_data['original_article_type'])
+
+
+class TestFulltextTranslation(TestCase):
+    """Tests for translation sub-article with original_article_type parameter"""
+    
+    def setUp(self):
+        xml = '''
+        <sub-article article-type="translation" xml:lang="es" id="s1">
+            <front-stub>
+                <related-article related-article-type="translation-of" 
+                               ext-link-type="doi" 
+                               id="ra1">Translation note</related-article>
+            </front-stub>
+            <body>
+                <related-article related-article-type="corrected-article"
+                               ext-link-type="doi"
+                               id="ra2">Body related</related-article>
+            </body>
+        </sub-article>'''
+        self.fulltext = Fulltext(etree.fromstring(xml), original_article_type="research-article")
+
+    def test_parent_data_with_translation(self):
+        expected = {
+            'parent': 'sub-article',
+            'parent_id': 's1',
+            'parent_article_type': 'translation',
+            'parent_lang': 'es',
+            'original_article_type': 'research-article'
+        }
+        self.assertEqual(self.fulltext.parent_data, expected)
+
+    def test_related_articles_in_translation(self):
+        related = list(self.fulltext.related_articles)
+        self.assertEqual(len(related), 2)
+        
+        # Map related articles by id
+        articles = {r['id']: r for r in related}
+        
+        # Check front-stub related article
+        front = articles['ra1']
+        self.assertEqual(front['related-article-type'], 'translation-of')
+        self.assertEqual(front['text'], 'Translation note')
+        self.assertEqual(front['original_article_type'], 'research-article')
+        
+        # Check body related article
+        body = articles['ra2']
+        self.assertEqual(body['related-article-type'], 'corrected-article')
+        self.assertEqual(body['text'], 'Body related')
+        self.assertEqual(body['original_article_type'], 'research-article')
+
+
+class TestFulltextNestedStructure(TestCase):
+    """Tests for complex nested structure with multiple sub-articles"""
+    
+    def setUp(self):
+        xml = '''
+        <article xmlns:xlink="http://www.w3.org/1999/xlink" article-type="research-article" xml:lang="en" id="main">
+            <front>
+                <related-article id="ra1">Main article</related-article>
+            </front>
+            <sub-article article-type="translation" xml:lang="pt" id="trans1">
+                <front-stub>
+                    <related-article id="ra2">Translation 1</related-article>
+                </front-stub>
+                <sub-article article-type="reviewer-report" id="abs1">
+                    <front-stub>
+                        <related-article id="ra3">Nested reviewer-report</related-article>
+                    </front-stub>
+                </sub-article>
+            </sub-article>
+        </article>'''
+        self.fulltext = Fulltext(etree.fromstring(xml))
+
+    def test_nested_structure(self):
+        # Test main article
+        main_related = list(self.fulltext.related_articles)
+        self.assertEqual(len(main_related), 1)
+        self.assertEqual(main_related[0]['text'], 'Main article')
+        
+        # Test first level sub-articles
+        level1_articles = list(self.fulltext.fulltexts)
+        self.assertEqual(len(level1_articles), 1)
+        
+        translation = level1_articles[0]
+        self.assertEqual(translation.parent_data['parent_article_type'], 'translation')
+        self.assertEqual(translation.parent_data['original_article_type'], 'research-article')
+        
+        # Test translation related articles
+        trans_related = list(translation.related_articles)
+        self.assertEqual(len(trans_related), 1)
+        self.assertEqual(trans_related[0]['text'], 'Translation 1')
+        
+        # Test nested reviewer-report
+        nested_articles = list(translation.fulltexts)
+        self.assertEqual(len(nested_articles), 1)
+        
+        reviewer_report = nested_articles[0]
+        self.assertEqual(reviewer_report.parent_data['parent_article_type'], 'reviewer-report')
+        self.assertIsNone(reviewer_report.parent_data['original_article_type'])
+        
+        # Test reviewer-report related articles
+        abs_related = list(reviewer_report.related_articles)
+        self.assertEqual(len(abs_related), 1)
+        self.assertEqual(abs_related[0]['text'], 'Nested reviewer-report')
