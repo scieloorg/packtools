dfd20196383c62915377f59d0031375c1de52e4d
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-01-21 11:47:24 -0300

    Melhora RelatedArticlesValidation: melhora os nomes dos métodos e substituir os parametros dos métodos por self.params

--- packtools/sps/validation/related_articles.py
+++ packtools/sps/validation/related_articles.py
@@ -1,138 +1,107 @@
-from packtools.sps.models import (
-    related_articles,
-    article_and_subarticles
-)
-
+from packtools.sps.models.related_articles import RelatedItems
+from packtools.sps.models.article_and_subarticles import ArticleAndSubArticles
 from packtools.sps.validation.exceptions import ValidationRelatedArticleException
-from packtools.sps.validation.utils import format_response
+from packtools.sps.validation.utils import build_response
 
 
 class RelatedArticlesValidation:
-    def __init__(self, xmltree):
-        self.related_articles = [related for related in related_articles.RelatedItems(xmltree).related_articles]
-        self.article_type = article_and_subarticles.ArticleAndSubArticles(xmltree).main_article_type
+    def __init__(self, xmltree, params=None):
+        """Initialize with xmltree and validation parameters
 
-    def related_articles_matches_article_type_validation(self, correspondence_list=None, error_level="ERROR"):
+        Parameters
+        ----------
+        xmltree : etree
+            XML tree to be validated
+        params : dict, optional
+            Dictionary containing validation parameters:
+            {
+                'correspondence_list': [
+                    {
+                        'article-type': 'correction',
+                        'related-article-types': ['corrected-article']
+                    },
+                    {
+                        'article-type': 'retraction',
+                        'related-article-types': ['retracted-article']
+                    }
+                ],
+                'error_level': 'ERROR'  # Default error level for validations
+            }
         """
-        Check whether the article type attribute of the article matches the options provided in a standard list.
+        self.related_articles = [
+            related for related in RelatedItems(xmltree).related_articles
+        ]
+        self.article_type = ArticleAndSubArticles(xmltree).main_article_type
+        self.params = params or {}
+        self.error_level = self.params.get('error_level', 'ERROR')
 
-        XML input
-        ---------
-        <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
-        article-type="correction-forward" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
-        <related-article ext-link-type="doi" id="ra1" related-article-type="corrected-article" xlink:href="10.1590/1808-057x202090350"/>
-        </article>
-
-        Params
-        ------
-        correspondence_list : list of dict, such as:
-            [
-                {
-                    'article-type': 'correction',
-                    'related-article-types': ['corrected-article']
-                },
-                {
-                    'article-type': 'retraction',
-                    'related-article-types': ['retracted-article']
-                }, ...
-            ]
+    def validate_related_article_types(self):
+        """
+        Validate if article type matches related article types from correspondence list.
 
         Returns
         -------
-        list of dict
-            A list of dictionaries, such as:
-            [
-                {
-                    'title': 'Related article type validation',
-                    'xpath': './article[@article-type] .//related-article[@related-article-type]',
-                    'validation_type': 'match',
-                    'response': 'OK',
-                    'expected_value': ['corrected-article'],
-                    'got_value': 'corrected-article',
-                    'message': 'Got corrected-article, expected one of the following items: ['corrected-article'],
-                    'advice': None
-                }, ...
-            ]
+        generator
+            Yields validation results for each related article
         """
+        correspondence_list = self.params.get('correspondence_list')
         if not correspondence_list:
-            raise ValidationRelatedArticleException("Function requires a list of dictionary with article type and related article types")
+            raise ValidationRelatedArticleException(
+                "Validation requires 'correspondence_list' parameter with article type and related article types"
+            )
 
-        expected_values_for_related_article_type = None
+        expected_values = None
         for item in correspondence_list:
             if isinstance(item, dict) and item.get('article-type') == self.article_type:
-                expected_values_for_related_article_type = item['related-article-types']
+                expected_values = item['related-article-types']
                 break
 
-        if expected_values_for_related_article_type:
+        if expected_values:
             for related_article in self.related_articles:
-                obtained_related_article = related_article.get('related-article-type')
-                is_valid = obtained_related_article in expected_values_for_related_article_type
-                yield format_response(
+                obtained_type = related_article.get('related-article-type')
+                is_valid = obtained_type in expected_values
+
+                yield build_response(
                     title='Related article type validation',
-                    parent=related_article.get("parent"),
-                    parent_id=related_article.get("parent_id"),
-                    parent_article_type=related_article.get("parent_article_type"),
-                    parent_lang=related_article.get("parent_lang"),
+                    parent=related_article,
                     item='related-article',
                     sub_item='related-article-type',
                     validation_type='match',
                     is_valid=is_valid,
-                    expected=expected_values_for_related_article_type,
-                    obtained=obtained_related_article,
+                    expected=expected_values,
+                    obtained=obtained_type,
                     advice=f"The article-type: {self.article_type} does not match the related-article-type: "
-                           f"{obtained_related_article}, provide one of the following items: "
-                           f"{expected_values_for_related_article_type}",
+                          f"{obtained_type}, provide one of the following items: {expected_values}",
                     data=related_article,
-                    error_level=error_level
+                    error_level=self.error_level
                 )
 
-    def related_articles_doi(self, error_level="ERROR"):
+    def validate_related_article_doi(self):
         """
-        Checks if there is a DOI for related articles.
-
-        XML input
-        ---------
-        <article xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink"
-        article-type="correction-forward" dtd-version="1.1" specific-use="sps-1.9" xml:lang="en">
-        <related-article ext-link-type="doi" id="ra1" related-article-type="corrected-article" xlink:href="10.1590/1808-057x202090350"/>
-        </article>
+        Validate if related articles have DOIs.
 
         Returns
         -------
-        list of dict
-            A list of dictionaries, such as:
-            [
-                {
-                    'title': 'Related article doi validation',
-                    'xpath': './/related-article/@xLink:href',
-                    'validation_type': 'exist',
-                    'response': 'OK',
-                    'expected_value': '10.1590/1808-057x202090350',
-                    'got_value': '10.1590/1808-057x202090350',
-                    'message': 'Got 10.1590/1808-057x202090350, expected 10.1590/1808-057x202090350',
-                    'advice': None
-                },...
-            ]
+        generator
+            Yields validation results for each related article's DOI
         """
-
         for related_article in self.related_articles:
             doi = related_article.get('href')
             is_valid = doi is not None
-            expected_value = doi if doi else 'A valid DOI or URI for related-article/@xlink:href'
-            yield format_response(
+            expected = doi if doi else 'A valid DOI or URI for related-article/@xlink:href'
+
+            yield build_response(
                 title='Related article doi validation',
-                parent=related_article.get("parent"),
-                parent_id=related_article.get("parent_id"),
-                parent_article_type=related_article.get("parent_article_type"),
-                parent_lang=related_article.get("parent_lang"),
+                parent=related_article,
                 item='related-article',
                 sub_item='xlink:href',
                 validation_type='exist',
                 is_valid=is_valid,
-                expected=expected_value,
+                expected=expected,
                 obtained=doi,
-                advice=f'Provide a valid DOI for <related-article ext-link-type="doi" id="{related_article.get("id")}" '
-                       f'related-article-type="{related_article.get("related-article-type")}" /> ',
+                advice=f'Provide a valid DOI for <related-article ext-link-type="doi" '
+                      f'id="{related_article.get("id")}" related-article-type='
+                      f'"{related_article.get("related-article-type")}" />',
                 data=related_article,
-                error_level=error_level
+                error_level=self.error_level
             )
