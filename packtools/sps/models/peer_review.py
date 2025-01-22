99d7f3e30df91ea1039fbb5c30f90e41d11de1ec
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-01-22 12:20:36 -0300

    Refaz o modelo PeerReview

--- packtools/sps/models/peer_review.py
+++ packtools/sps/models/peer_review.py
@@ -1,16 +1,8 @@
-from packtools.sps.utils.xml_utils import get_parent_context, put_parent_context
-
-
-class CustomMetaGroup:
-    def __init__(self, main_node):
-        self.main_node = main_node
-
-    @property
-    def data(self):
-        for node, lang, article_type, parent, parent_id in get_parent_context(self.main_node):
-            for item in node.xpath('.//custom-meta-group//custom-meta'):
-                custom_meta = CustomMeta(item).data
-                yield put_parent_context(custom_meta, lang, article_type, parent, parent_id)
+from packtools.sps.models.article_and_subarticles import Fulltext
+from packtools.sps.models.related_articles import Fulltext as FulltextRelatedArticles
+from packtools.sps.models.dates import FulltextDates
+from packtools.sps.models.article_contribs import Contrib
+from packtools.sps.models.article_license import License
 
 
 class CustomMeta:
@@ -19,13 +11,13 @@ class CustomMeta:
 
     @property
     def meta_name(self):
-        if name := self.custom_meta_node.findtext('.//meta-name'):
-            return name
+        return self.custom_meta_node.findtext('.//meta-name')
 
     @property
     def meta_value(self):
-        if value := self.custom_meta_node.findtext('.//meta-value'):
-            return value
+        # 'revision', 'major-revision', 'minor-revision', 'reject',
+        # 'reject-with-resubmit', 'accept', 'formal-accept', 'accept-in-principle'
+        return self.custom_meta_node.findtext('.//meta-value')
 
     @property
     def data(self):
@@ -35,5 +27,47 @@ class CustomMeta:
         }
 
 
-class PeerReview(CustomMetaGroup):
-    pass
+class PeerReview(Fulltext):
+    """
+    instanciado com node de article ou sub-article
+    """
+
+    @property
+    def related_articles(self):
+        if not hasattr(self, '_related_articles'):
+            fulltext_dates = FulltextRelatedArticles(self.node)
+            self._related_articles = fulltext_dates.related_articles
+        return self._related_articles
+
+    @property
+    def contribs(self):
+        if not hasattr(self, '_contribs'):
+            self._contribs = []
+            for contrib in self.front.xpath('.//contrib'):
+                self._contribs.append(contrib)
+        return self._contribs
+
+    @property
+    def history(self):
+        if not hasattr(self, '_history'):
+            fulltext_dates = FulltextDates(self.node)
+            self._history = fulltext_dates.history_dates_dict
+        return self._history
+
+    @property
+    def license_code(self):
+        if not hasattr(self, '_license_code'):
+            self._license_code = None
+            node = self.node.find(".//permission//license")
+            if node is not None:
+                fulltext_dates = License(self.node)
+                self._license_code = License(self.node).code
+        return self._license_code
+
+    @property
+    def custom_meta_items(self):
+        if not hasattr(self, '_custom_meta_items'):
+            self._custom_meta_items = []
+            for item in self.front.xpath('.//custom-meta'):
+                self._custom_meta_items.append(CustomMeta(item))
+        return self._custom_meta_items
--- tests/sps/models/test_peer_review.py
+++ tests/sps/models/test_peer_review.py
@@ -1,77 +1,257 @@
 import unittest
-from lxml import etree as ET
-from packtools.sps.models.peer_review import PeerReview, CustomMeta
 
-class PeerReviewArticleTest(unittest.TestCase):
+from lxml import etree
+
+from packtools.sps.models.peer_review import PeerReview
+
+
+class BasePeerReviewTestCase(unittest.TestCase):
+    """Base test case with helper methods"""
+    
+    def create_peer_review_xml(self, is_article=True):
+        """Helper method to create test XML"""
+        xml = """<?xml version="1.0" encoding="utf-8"?>
+        <{root_tag} article-type="reviewer-report" xml:lang="en" id="pr1">
+            <front{front_suffix}>
+                <article-meta>
+                    <contrib-group>
+                        <contrib contrib-type="author">
+                            <anonymous/>
+                            <role specific-use="reviewer"/>
+                            <xref ref-type="aff" rid="aff1"/>
+                        </contrib>
+                    </contrib-group>
+                    <permissions>
+                        <license license-type="open-access" 
+                                xlink:href="http://creativecommons.org/licenses/by/4.0/"
+                                xml:lang="en">
+                            <license-p>This is an open access article.</license-p>
+                        </license>
+                    </permissions>
+                    <custom-meta-group>
+                        <custom-meta>
+                            <meta-name>Review recommendation</meta-name>
+                            <meta-value>accept</meta-value>
+                        </custom-meta>
+                    </custom-meta-group>
+                    <related-article related-article-type="peer-reviewed-material"
+                                    xlink:href="10.1590/123456789"
+                                    ext-link-type="doi"/>
+                    <history>
+                        <date date-type="received">
+                            <day>15</day>
+                            <month>01</month>
+                            <year>2024</year>
+                        </date>
+                        <date date-type="accepted">
+                            <day>20</day>
+                            <month>01</month>
+                            <year>2024</year>
+                        </date>
+                    </history>
+                </article-meta>
+            </front{front_suffix}>
+            <body>
+                <p>Review content here.</p>
+            </body>
+        </{root_tag}>
+        """.format(
+            root_tag="article" if is_article else "sub-article",
+            front_suffix="" if is_article else "-stub"
+        )
+        return etree.fromstring(xml)
+
+
+class TestPeerReviewArticle(BasePeerReviewTestCase):
+    """Test PeerReview class with article-type peer reviews"""
+    
     def setUp(self):
-        xml_success = """
-            <article xmlns:xlink="http://www.w3.org/1999/xlink" 
-                     xmlns:mml="http://www.w3.org/1998/Math/MathML"
-                     dtdversion="1.3" specific-use="sps-1.10" article-type="reviewer-report" xml:lang="en">
-                <front>
-                    <article-meta>
-                        <custom-meta-group>
-                            <custom-meta>
-                                <meta-name>peer-review-recommendation</meta-name>
-                                <meta-value>accept</meta-value>
-                            </custom-meta>
-                        </custom-meta-group>
-                    </article-meta>
-                </front>
-            </article>
+        """Set up article-type peer review for testing"""
+        self.peer_review = PeerReview(self.create_peer_review_xml(is_article=True))
+
+    def test_related_articles(self):
+        """Test related_articles property"""
+        related = list(self.peer_review.related_articles)
+        self.assertEqual(len(related), 1)
+        self.assertEqual(related[0]['related-article-type'], 'peer-reviewed-material')
+        self.assertEqual(related[0]['href'], '10.1590/123456789')
+        self.assertEqual(related[0]['ext-link-type'], 'doi')
+
+    def test_contribs(self):
+        """Test contribs property"""
+        contribs = self.peer_review.contribs
+        self.assertEqual(len(contribs), 1)
+        self.assertEqual(contribs[0].get('contrib-type'), 'author')
+        self.assertIsNotNone(contribs[0].find('.//anonymous'))
+        self.assertEqual(contribs[0].find('.//role').get('specific-use'), 'reviewer')
+
+    def test_history(self):
+        """Test history property"""
+        history = self.peer_review.history
+        self.assertIn('received', history)
+        self.assertIn('accepted', history)
+        received = history['received']
+        self.assertEqual(received['year'], '2024')
+        self.assertEqual(received['month'], '01')
+        self.assertEqual(received['day'], '15')
+
+    def test_license_code(self):
+        """Test license_code property"""
+        self.assertEqual(self.peer_review.license_code, 'by')
+
+    def test_custom_meta_items(self):
+        """Test custom_meta_items property"""
+        items = self.peer_review.custom_meta_items
+        self.assertEqual(len(items), 1)
+        self.assertEqual(items[0].meta_name, 'Review recommendation')
+        self.assertEqual(items[0].meta_value, 'accept')
+        self.assertEqual(items[0].data, {
+            'meta_name': 'Review recommendation',
+            'meta_value': 'accept'
+        })
+
+
+class TestPeerReviewSubArticle(BasePeerReviewTestCase):
+    """Test PeerReview class with sub-article-type peer reviews"""
+    
+    def setUp(self):
+        """Set up sub-article-type peer review for testing"""
+        self.peer_review = PeerReview(self.create_peer_review_xml(is_article=False))
+
+    def test_structure(self):
+        """Test subarticle structure"""
+        self.assertEqual(self.peer_review.tag, 'sub-article')
+        self.assertEqual(self.peer_review.article_type, 'reviewer-report')
+        self.assertEqual(self.peer_review.front.tag, 'front-stub')
+
+
+class TestEmptyPeerReview(unittest.TestCase):
+    """Test PeerReview class with minimal content"""
+    
+    def setUp(self):
+        """Set up peer review with minimal content"""
+        xml = """
+        <article article-type="reviewer-report">
+            <front>
+                <article-meta/>
+            </front>
+        </article>
         """
-        xml_fail = """
-            <article xmlns:xlink="http://www.w3.org/1999/xlink" 
-                     xmlns:mml="http://www.w3.org/1998/Math/MathML"
-                     dtdversion="1.3" specific-use="sps-1.10" xml:lang="en">
-                <front>
-                    <article-meta>
-                        <custom-meta-group>
-                            <custom-meta>
-                                <meta-name></meta-name>
-                                <meta-value></meta-value>
-                            </custom-meta>
-                        </custom-meta-group>
-                    </article-meta>
-                </front>
-            </article>
+        self.review = PeerReview(etree.fromstring(xml))
+
+    def test_empty_content(self):
+        """Test behavior with minimal content"""
+        self.assertIsNone(self.review.license_code)
+        self.assertEqual(self.review.history, {})
+        self.assertEqual(list(self.review.custom_meta_items), [])
+        self.assertEqual(list(self.review.related_articles), [])
+
+
+class TestMultipleMetadata(unittest.TestCase):
+    """Test PeerReview class with multiple metadata items"""
+    
+    def setUp(self):
+        """Set up peer review with multiple metadata"""
+        xml = """
+        <article article-type="reviewer-report">
+            <front>
+                <article-meta>
+                    <custom-meta-group>
+                        <custom-meta>
+                            <meta-name>Review recommendation</meta-name>
+                            <meta-value>accept</meta-value>
+                        </custom-meta>
+                        <custom-meta>
+                            <meta-name>Review confidence</meta-name>
+                            <meta-value>high</meta-value>
+                        </custom-meta>
+                    </custom-meta-group>
+                </article-meta>
+            </front>
+        </article>
         """
+        self.review = PeerReview(etree.fromstring(xml))
 
-        self.custom_meta_success = CustomMeta(ET.fromstring(xml_success))
-        self.peer_review_success = PeerReview(ET.fromstring(xml_success))
-        self.custom_meta_fail = CustomMeta(ET.fromstring(xml_fail))
+    def test_multiple_custom_meta(self):
+        """Test multiple custom meta items"""
+        items = self.review.custom_meta_items
+        self.assertEqual(len(items), 2)
+        meta_names = {item.meta_name for item in items}
+        meta_values = {item.meta_value for item in items}
+        self.assertEqual(meta_names, {'Review recommendation', 'Review confidence'})
+        self.assertEqual(meta_values, {'accept', 'high'})
 
-    def test_meta_name_success(self):
-        expected = 'peer-review-recommendation'
-        obtained = self.custom_meta_success.meta_name
-        self.assertEqual(expected, obtained)
 
-    def test_meta_name_fail(self):
-        obtained = self.custom_meta_fail.meta_name
-        self.assertIsNone(obtained)
+class TestMultipleHistoryDates(unittest.TestCase):
+    """Test PeerReview class with multiple history dates"""
+    
+    def setUp(self):
+        """Set up peer review with multiple history dates"""
+        xml = """
+        <article article-type="reviewer-report">
+            <front>
+                <article-meta>
+                    <history>
+                        <date date-type="received">
+                            <day>15</day>
+                            <month>01</month>
+                            <year>2024</year>
+                        </date>
+                        <date date-type="accepted">
+                            <day>20</day>
+                            <month>01</month>
+                            <year>2024</year>
+                        </date>
+                        <date date-type="rev-recd">
+                            <day>18</day>
+                            <month>01</month>
+                            <year>2024</year>
+                        </date>
+                    </history>
+                </article-meta>
+            </front>
+        </article>
+        """
+        self.review = PeerReview(etree.fromstring(xml))
 
-    def test_meta_value_success(self):
-        expected = 'accept'
-        obtained = self.custom_meta_success.meta_value
-        self.assertEqual(expected, obtained)
+    def test_multiple_dates(self):
+        """Test multiple history dates"""
+        history = self.review.history
+        self.assertEqual(len(history), 3)
+        date_types = {'received', 'accepted', 'rev-recd'}
+        self.assertTrue(all(key in history for key in date_types))
+        self.assertTrue(all(
+            all(field in date for field in ['day', 'month', 'year'])
+            for date in history.values()
+        ))
 
-    def test_meta_value_fail(self):
-        obtained = self.custom_meta_fail.meta_value
-        self.assertIsNone(obtained)
 
-    def test_custom_meta_data(self):
-        obtained = list(self.peer_review_success.data)
-        expected = [
-            {
-                'parent': 'article',
-                'parent_article_type': 'reviewer-report',
-                'parent_id': None,
-                'parent_lang': 'en',
-                'meta_name': 'peer-review-recommendation',
-                'meta_value': 'accept'
-            }
-        ]
-        self.assertEqual(expected, obtained)
+class TestPeerReviewInheritance(unittest.TestCase):
+    """Test PeerReview class inheritance from Fulltext"""
+    
+    def setUp(self):
+        """Set up peer review for testing inheritance"""
+        xml = """
+        <article article-type="reviewer-report" xml:lang="en" id="pr1">
+            <front>
+                <article-meta/>
+            </front>
+        </article>
+        """
+        self.review = PeerReview(etree.fromstring(xml))
+
+    def test_inheritance(self):
+        """Test inheritance from Fulltext"""
+        self.assertEqual(self.review.tag, 'article')
+        self.assertEqual(self.review.lang, 'en')
+        self.assertEqual(self.review.id, 'pr1')
+        self.assertEqual(self.review.article_type, 'reviewer-report')
+        self.assertEqual(self.review.attribs, {
+            'tag': 'article',
+            'id': 'pr1',
+            'lang': 'en',
+            'article_type': 'reviewer-report'
+        })
 
 
 if __name__ == '__main__':
