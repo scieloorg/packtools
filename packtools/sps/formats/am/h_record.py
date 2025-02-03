24c2c8c7a68cdbd36aaaf0810f1b7fdbea24331f
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-02-03 14:21:06 -0300

    Adiciona estrutura da função para obtenção do valor de 'collection' teste pendente

--- packtools/sps/formats/am/h_record.py
+++ packtools/sps/formats/am/h_record.py
@@ -10,3 +10,8 @@ def code(xml_tree, h_record_dict):
         h_record_dict.update({"code": article_id_v2})
     return h_record_dict
 
+def collection(xml_tree, h_record_dict):
+    # TODO: adicionar a lógica de obtenção do código da coleção
+    return h_record_dict
+
+
