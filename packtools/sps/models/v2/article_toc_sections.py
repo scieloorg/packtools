4663657c74c886f2c4094ad428366da7bb8b06ce
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-02-06 11:22:14 -0300

    Cria ArticleTocSections.get_data para ser aplicado para qualquer nó subject

--- packtools/sps/models/v2/article_toc_sections.py
+++ packtools/sps/models/v2/article_toc_sections.py
@@ -10,17 +10,7 @@ class ArticleTocSections:
     def sections(self):
         for node, lang, article_type, parent, parent_id in get_parent_context(self.xmltree):
             for item in node.xpath(".//subj-group"):
-                subject = node_text_without_fn_xref(item.find("./subject")) or None
-                subject_parts = subject.split(':')
-                section = subject_parts[0]
-                _section = {
-                    "subject": subject,
-                    "subj_group_type": item.get("subj-group-type"),
-                    "section": section
-                }
-                if len(subject_parts) == 2:
-                    _section["subsec"] = subject_parts[-1]
-
+                _section = self.get_data(item.find("./subject"))
                 subsections = []
                 for subsection in item.xpath("./subj-group//subject"):
                     subsections.append(node_text_without_fn_xref(subsection) or None)
@@ -34,3 +24,17 @@ class ArticleTocSections:
             response.setdefault(item["parent_lang"], [])
             response[item["parent_lang"]].append(item)
         return response
+
+    def get_data(self, subject_node):
+        subject = node_text_without_fn_xref(subject_node) or ""
+        subject_parts = subject.split(':')
+        section = subject_parts[0]
+        data = {
+            "subject": subject,
+            "subj_group_type": item.get("subj-group-type"),
+            "section": section,
+        }
+        if len(subject_parts) == 2:
+            data["subsec"] = subject_parts[-1]
+        return data
+
