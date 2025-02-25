e83e87b67334eec6d0e13aac8e9e1ffccb48f0ee
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-02-25 12:03:07 -0300

    Complementa modelo

--- packtools/sps/models/supplementary_material.py
+++ packtools/sps/models/supplementary_material.py
@@ -1,53 +1,80 @@
 from packtools.sps.utils.xml_utils import get_parent_context, put_parent_context
 
-
 class SupplementaryMaterial:
     def __init__(self, node):
         self.node = node
+        self._parent_node = node.getparent()
 
     @property
-    def supplementary_material_id(self):
+    def id(self):
         return self.node.get("id")
 
     @property
-    def supplementary_material_label(self):
+    def parent(self):
+        return self._parent_node.tag if self._parent_node is not None else None
+
+    @property
+    def sec_type(self):
+        return self._parent_node.get("sec-type")
+
+    @property
+    def label(self):
         return self.node.findtext("label")
 
     @property
+    def caption_title(self):
+        return self.node.findtext("caption/title")
+
+    @property
+    def media_node(self):
+        node = self.node.findall("media") or self.node.findall("graphic")
+        return node[0] if node else None
+
+    @property
     def mimetype(self):
-        return self.node.get("mimetype")
+        return self.media_node.get("mimetype") if self.media_node is not None else None
 
     @property
     def mime_subtype(self):
-        return self.node.get("mime-subtype")
+        return self.media_node.get("mime-subtype") if self.media_node is not None else None
 
     @property
     def xlink_href(self):
-        return self.node.get("{http://www.w3.org/1999/xlink}href")
+        return self.media_node.get("{http://www.w3.org/1999/xlink}href") if self.media_node is not None else None
+
+    @property
+    def media_type(self):
+        return self.media_node.tag if self.media_node is not None else None
 
     @property
     def data(self):
         return {
-            "supplementary_material_id": self.supplementary_material_id,
-            "supplementary_material_label": self.supplementary_material_label,
+            "id": self.id,
+            "parent_tag": self.parent,
+            "parent_attrib_type": self.sec_type,
+            "label": self.label,
+            "caption_title": self.caption_title,
             "mimetype": self.mimetype,
             "mime_subtype": self.mime_subtype,
             "xlink_href": self.xlink_href,
+            "media_type": self.media_type,
+            "media_node": self.media_node
         }
 
 
 class ArticleSupplementaryMaterials:
     def __init__(self, xml_tree):
+        """
+        Extrai todos os <supplementary-material> e <sec sec-type="supplementary-material"> dentro do artigo.
+
+        Args:
+            xml_tree: Objeto XML representando o artigo
+        """
         self.xml_tree = xml_tree
 
     def data(self):
-        for node, lang, article_type, parent, parent_id in get_parent_context(
-            self.xml_tree
-        ):
-            for supp_node in node.xpath(
-                ".//supplementary-material | .//inline-supplementary-material"
-            ):
+        """Itera sobre os <supplementary-material> e retorna os dados extraídos."""
+        for node, lang, article_type, parent, parent_id in get_parent_context(self.xml_tree):
+            for supp_node in node.xpath(".//supplementary-material"):
                 supp_data = SupplementaryMaterial(supp_node).data
-                yield put_parent_context(
-                    supp_data, lang, article_type, parent, parent_id
-                )
+                yield put_parent_context(supp_data, lang, article_type, parent, parent_id)
