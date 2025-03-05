889558201751eb2ef1e8aaa5af8549d6fd120921
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-03-05 18:03:56 -0300

    Adiciona modelo para 'accessibility_data'

--- packtools/sps/models/accessibility_data.py
+++ packtools/sps/models/accessibility_data.py
@@ -1,53 +1,66 @@
 import re
+from xml.etree.ElementTree import Element
 
 class AccessibilityData:
-    def __init__(self, node):
+    def __init__(self, node: Element):
         self.node = node
 
-    def get_alt_text(self):
+    @property
+    def alt_text(self):
         """Obtém o texto alternativo (<alt-text>) do XML."""
         alt_text_node = self.node.find("alt-text")
-        return alt_text_node.text.strip() if alt_text_node is not None else None
+        return alt_text_node.text.strip() if alt_text_node is not None and alt_text_node.text else None
 
-    def get_long_desc(self):
-        """Obtém a descrição longa (<long-desc>) do XML."""
+    @property
+    def long_desc(self):
+        """Obtém a descrição longa (<long-desc>) do XML, removendo espaços extras."""
         long_desc_node = self.node.find("long-desc")
-        if long_desc_node is not None:
-            return " ".join(long_desc_node.text.split())  # Remove espaços extras e quebras de linha
+        if long_desc_node is not None and long_desc_node.text:
+            return " ".join(long_desc_node.text.split())  # Remove espaços extras
         return None
 
-    def get_transcript(self):
-        """Obtém todo o texto dentro de <sec sec-type='transcript'>, incluindo <speaker> e <speech>, removendo espaços extras."""
+    @property
+    def transcript(self):
+        """Obtém o texto dentro de <sec sec-type='transcript'>, removendo espaços extras."""
         transcript_node = self.node.find(".//sec[@sec-type='transcript']")
         if transcript_node is not None:
             text = " ".join(transcript_node.itertext()).strip()
-            return re.sub(r'\s+', ' ', text)
+            return re.sub(r'\s+', ' ', text) if text else None
         return None
 
-    def get_content_type(self):
+    @property
+    def content_type(self):
         """Obtém o atributo @content-type do elemento."""
         return self.node.get("content-type")
 
-    def get_speaker_data(self):
-        """Obtém o conteúdo de <speaker> e <speech> dentro da transcrição, se existir."""
-        speakers = self.node.findall(".//speaker")
-        speech_blocks = self.node.findall(".//speech")
+    @property
+    def speaker_data(self):
+        """Obtém os dados de <speaker> e <speech> dentro de transcrição."""
+        transcript_node = self.node.find(".//sec[@sec-type='transcript']")
+        if transcript_node is None:
+            return None
 
-        speaker_data = []
-        for speaker, speech in zip(speakers, speech_blocks):
-            speaker_data.append({
-                "speaker": speaker.text.strip() if speaker is not None else None,
-                "speech": speech.text.strip() if speech is not None else None
-            })
+        speakers = []
+        current_speaker = None
 
-        return speaker_data if speaker_data else None
+        for element in transcript_node:
+            if element.tag == "speaker" and element.text:
+                current_speaker = element.text.strip()
+            elif element.tag == "speech" and element.text:
+                speakers.append({
+                    "speaker": current_speaker,
+                    "speech": " ".join(element.text.split())
+                })
 
-    def extract_data(self):
+        return speakers if speakers else None
+
+    @property
+    def data(self):
         """Retorna um dicionário com todos os dados extraídos do XML."""
         return {
-            "alt_text": self.get_alt_text(),
-            "long_desc": self.get_long_desc(),
-            "transcript": self.get_transcript(),
-            "content_type": self.get_content_type(),
-            "speakers": self.get_speaker_data(),
+            "alt_text": self.alt_text,
+            "long_desc": self.long_desc,
+            "transcript": self.transcript,
+            "content_type": self.content_type,
+            "speakers": self.speaker_data,
         }
