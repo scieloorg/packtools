b4f0bfa0b3c31960aecd58441e7dd9fe8a8f8397
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-03-07 13:04:31 -0300

    Refatora modelo para <media>

--- packtools/sps/models/media.py
+++ packtools/sps/models/media.py
@@ -1,12 +1,8 @@
-from packtools.sps.models.article_and_subarticles import Fulltext
-from packtools.sps.models.accessibility_data import AccessibilityData
+from packtools.sps.models.label_and_caption import LabelAndCaption
+from packtools.sps.models.visual_resource_base import VisualResourceBase
 
 
-class Media:
-    def __init__(self, node):
-        self.node = node
-        self.accessibility = AccessibilityData(node)
-
+class BaseMedia(VisualResourceBase):
     @property
     def mimetype(self):
         return self.node.get("mimetype")
@@ -16,38 +12,31 @@ class Media:
         return self.node.get("mime-subtype")
 
     @property
-    def media_type(self):
-        return self.node.tag if self.node is not None else None
+    def data(self):
+        base_data = super().data
 
-    @property
-    def xlink_href(self):
-        return self.node.get("{http://www.w3.org/1999/xlink}href")
+        media_data = {
+            "mimetype": self.mimetype,
+            "mime_subtype": self.mime_subtype,
+        }
+
+        combined_data = {**base_data, **media_data}
+        return combined_data
+
+
+class Media(BaseMedia, LabelAndCaption):
+    def __init__(self, node):
+        BaseMedia.__init__(self, node)  # Chama o __init__ de BaseMedia
+        LabelAndCaption.__init__(self, node)  # Chama o __init__ de LabelAndCaption
 
     @property
     def data(self):
-        """Combina os dados de mídia com os dados de acessibilidade extraídos do XML."""
-        data = {
-            "mimetype": self.mimetype,
-            "mime_subtype": self.mime_subtype,
-            "media_type": self.media_type,
-            "xlink_href": self.xlink_href,
-        }
-        data.update(self.accessibility.data)  # Adiciona os dados de acessibilidade extraídos
-        return data
+        base_data = BaseMedia.data.fget(self)
+        label_caption_data = LabelAndCaption.data.fget(self)
+
+        return {**base_data, **label_caption_data}
 
 
-class XmlMedias:
-    def __init__(self, xml_tree):
-        self.xml_tree = xml_tree
 
-    @property
-    def items(self):
-        media_list = []
-        # TODO sub-article está sendo considerado em duplicidade
-        for node in self.xml_tree.xpath(". | sub-article"):
-            full_text = Fulltext(node)
-            for media_node in full_text.node.xpath(".//media | .//graphic"):
-                media_data = Media(media_node).data
-                media_data.update(full_text.attribs_parent_prefixed)
-                media_list.append(media_data)
-        return media_list
+class InlineMedia(BaseMedia):
+    pass
