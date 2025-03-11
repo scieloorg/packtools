a20f75d6d00b80ea47d301d066c9590b431560fd
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-03-11 13:57:47 -0300

    Adiciona XmlVisualResource

--- packtools/sps/models/visual_resource_base.py
+++ packtools/sps/models/visual_resource_base.py
@@ -1,4 +1,5 @@
 from packtools.sps.models.accessibility_data import AccessibilityData
+from packtools.sps.models.article_and_subarticles import ArticleAndSubArticles, Fulltext
 
 
 class VisualResourceBase:
@@ -22,3 +23,23 @@ class VisualResourceBase:
         }
         data.update(self.accessibility.data)  # Adiciona os dados de acessibilidade extraídos
         return data
+
+
+class XmlVisualResource:
+    RESOURCE_TYPES = []
+
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
+        self.article_and_subarticles = ArticleAndSubArticles(xmltree).article_and_sub_articles
+
+    def data(self):
+        """Gera dados dos recursos visuais de cada artigo e sub-artigo."""
+        for node in self.article_and_subarticles:
+            full_text = Fulltext(node)
+
+            for resource_type, resource_class in self.RESOURCE_TYPES:
+                resource_nodes = node.xpath(f".//{resource_type}") or []
+                for resource_node in resource_nodes:
+                    resource_instance = resource_class(resource_node)
+                    resource_data = resource_instance.data
+                    yield {**resource_data, **full_text.attribs_parent_prefixed}
