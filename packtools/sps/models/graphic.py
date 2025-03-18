2eff60ae02daf17e98444c44795057d754eaead9
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-03-18 09:27:23 -0300

    Adiciona modelo Graphic

---
+++ packtools/sps/models/graphic.py
@@ -0,0 +1,16 @@
+from packtools.sps.models.visual_resource_base import (
+    VisualResourceBase,
+    XmlVisualResource,
+)
+
+
+class Graphic(VisualResourceBase):
+    pass
+
+
+class InlineGraphic(Graphic):
+    pass
+
+
+class XmlGraphic(XmlVisualResource):
+    RESOURCE_TYPES = [("graphic", Graphic), ("inline-graphic", InlineGraphic)]
