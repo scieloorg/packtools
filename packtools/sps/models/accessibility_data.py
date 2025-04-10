adb17683ba05c96d1060357a916d77cf28a20106
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-04-10 18:27:33 -0300

    Corrige defeitos de AccessibilityData, cria XMLAccessibilityData e Transcript - AccessibilityData deve ser instanciado com elementos que contenham long-desc ou alt-text ou ser graphic, inline-graphic, media ou inline-media - AccessibilityData ainda deve considerar que existe o elemento xref com link para `sec[@sec-type="transcript"]` - Transcript deve ser instanciado com o elemento `sec[@sec-type="transcript"]` - XMLAccessibilityData retorna os itens instanciados com AccessibilityData

--- packtools/sps/models/accessibility_data.py
+++ packtools/sps/models/accessibility_data.py
@@ -1,78 +1,116 @@
 import re
 
 
+class XMLAccessibilityData:
+
+    def __init__(self, xmltree):
+        self.xmltree = xmltree
+
+    @property
+    def transcripts(self):
+        if not hasattr(self, '_transcripts') or not self._transcripts:
+            self._transcripts = {}
+            for item in self.xmltree.xpath(".//sec[@sec-type='transcript']"):
+                transcript = Transcript(item)
+                self._transcripts[transcript.transcript_id] = transcript.data
+        return self._transcripts
+
+    @property
+    def data(self):
+        xpaths = [
+            ".//*[alt-text]",
+            ".//*[long-desc]",
+            "graphic",
+            "inline-graphic",
+            "media",
+            "inline-media",
+        ]
+        for item in self.xmltree.xpath("|".join(xpaths)):
+            model = AccessibilityData(item)
+            model.transcript_data = self.transcripts.get(model.xref_sec_rid)
+            yield model.data
+
+
 class AccessibilityData:
     def __init__(self, node):
         self.node = node
+        self.transcript_data = None
 
+    @property
+    def xref_sec_rid(self):
+        """Obtém o texto alternativo (<alt-text>) do XML."""
+        try:
+            return self.node.xpath('xref[@ref-type="sec"]')[0].get("rid")
+        except:
+            return None
+            
     @property
     def alt_text(self):
         """Obtém o texto alternativo (<alt-text>) do XML."""
-        if self.node is not None:
+        try:
             alt_text_node = self.node.find("alt-text")
-            return (
-                alt_text_node.text.strip()
-                if alt_text_node is not None and alt_text_node.text
-                else None
-            )
+            return {
+                "alt_text": alt_text_node.text,
+                "alt_text_content_type": alt_text_node.get("content-type")
+            }
+        except:
+            return None
 
     @property
     def long_desc(self):
         """Obtém a descrição longa (<long-desc>) do XML, removendo espaços extras."""
-        if self.node is not None:
+        try:
             long_desc_node = self.node.find("long-desc")
-            if long_desc_node is not None and long_desc_node.text:
-                return " ".join(long_desc_node.text.split())  # Remove espaços extras
+            return {
+                "long_desc": long_desc_node.text,
+                "long_desc_content_type": long_desc_node.get("content-type")
+            }
+        except:
             return None
+
+    @property
+    def data(self):
+        """Retorna um dicionário com todos os dados extraídos do XML."""
+        d = {
+            "tag": self.node.tag,
+            "xref_sec_rid": self.xref_sec_rid,
+        }
+        d.update(self.long_desc)
+        d.update(self.alt_text)
+        d.update(self.transcript_data or {})
+        return d
+
+
+class Transcript:
+    def __init__(self, node):
+        self.node = node
+
+    @property
+    def transcript_id(self):
+        """Obtém o texto alternativo (<alt-text>) do XML."""
+        return self.node.get("id")
 
     @property
     def transcript(self):
         """Obtém o texto dentro de <sec sec-type='transcript'>, removendo espaços extras."""
-        if self.node is not None:
-            transcript_node = self.node.find(".//sec[@sec-type='transcript']")
-            if transcript_node is not None:
-                text = " ".join(transcript_node.itertext()).strip()
-                return re.sub(r"\s+", " ", text) if text else None
-            return None
-
-    @property
-    def content_type(self):
-        """Obtém o atributo @content-type do elemento."""
-        if self.node is not None:
-            return self.node.get("content-type")
+        text = " ".join(self.node.itertext()).strip()
+        return re.sub(r"\s+", " ", text) if text else None
 
     @property
     def speaker_data(self):
         """Obtém os dados de <speaker> e <speech> dentro de transcrição."""
-        if self.node is not None:
-            transcript_node = self.node.find(".//sec[@sec-type='transcript']")
-            if transcript_node is None:
-                return None
-
-            speakers = []
-            current_speaker = None
-
-            for element in transcript_node:
-                if element.tag == "speaker" and element.text:
-                    current_speaker = element.text.strip()
-                elif element.tag == "speech" and element.text:
-                    speakers.append(
-                        {
-                            "speaker": current_speaker,
-                            "speech": " ".join(element.text.split()),
-                        }
-                    )
-
-            return speakers if speakers else None
+        for speech in self.node.xpath("speech"):
+            yield {
+                "speaker": speech.findtext("speaker"),
+                "speech": " ".join(speech.xpath("p//text()"))
+            }
 
     @property
     def data(self):
         """Retorna um dicionário com todos os dados extraídos do XML."""
         return {
-            "alt_text": self.alt_text,
-            "long_desc": self.long_desc,
+            "id": self.id,
             "transcript": self.transcript,
-            "content_type": self.content_type,
             "speakers": self.speaker_data,
             "tag": self.node.tag,
         }
