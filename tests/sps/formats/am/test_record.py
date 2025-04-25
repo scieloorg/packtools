004d13160a4eac3373c1a3015e72c02abb05d2a4
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-04-25 14:41:49 -0300

    Complementa os testes

--- tests/sps/formats/am/test_record.py
+++ tests/sps/formats/am/test_record.py
@@ -32,7 +32,6 @@ class RecordTest(unittest.TestCase):
 
     def test_field_v10(self):
         obtained = am.get_contribs(self.xml_tree)
-        print(obtained)
         self.assertIsInstance(obtained, dict)
         self.assertIn("v10", obtained)
         self.assertGreater(len(obtained["v10"]), 0)
@@ -49,6 +48,29 @@ class RecordTest(unittest.TestCase):
         self.assertEqual(author["s"], "Begui")
         self.assertEqual(author["1"], "aff1")
         self.assertEqual(author["r"], "ND")
+
+    def test_field_v70(self):
+        obtained = am.get_affs(self.xml_tree)
+        self.assertIsInstance(obtained, dict)
+        self.assertIn("v70", obtained)
+        self.assertGreater(len(obtained["v70"]), 0)
+
+        aff = obtained["v70"][0]
+        expected_keys = {"c", "i", "l", "1", "p", "s", "_"}
+        self.assertTrue(expected_keys.issubset(aff.keys()))
+
+    def test_field_v72(self):
+        obtained = am.get_references(self.xml_tree)
+        self.assertIsInstance(obtained, dict)
+        self.assertIn("v72", obtained)
+        self.assertEqual(obtained["v72"][0]["_"], 35)
+
+    def test_field_v882(self):
+        obtained = am.get_articlemeta_issue(self.xml_tree)
+        self.assertIsInstance(obtained, dict)
+        self.assertIn("v882", obtained)
+        self.assertEqual(obtained["v882"][0]["v"], "33")
+        self.assertEqual(obtained["v882"][0]["_"], "")
 
 
 if __name__ == "__main__":
