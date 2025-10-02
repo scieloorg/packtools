adf04009497dbc2e22098c6a9973fb6bd314e2a1
Rafael Damaceno <rafael@pitangainnovare.com.br>
2025-10-02 13:37:54 -0300

    Cria módulo renderer.docx

---
+++ packtools/sps/formats/pdf/renderer/docx/__init__.py
@@ -0,0 +1,8 @@
+# docx/__init__.py
+
+from . import builder # noqa: F401
+from . import text # noqa: F401
+from . import section # noqa: F401
+from . import style # noqa: F401
+from . import table # noqa: F401
+from . import figure # noqa: F401
